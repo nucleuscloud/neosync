28fb53392a9f0be73c646c6b3edc1df4306422df
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2023-08-25 10:07:42 -0700

    Crd Stubs, go.work (#6)
    * adds Connection crd stub
    
    * adds Job CRD stub
    
    * adds job run crd
    
    * adds go.work

---
+++ k8s-operator/api/v1alpha1/jobrun_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// JobRunSpec defines the desired state of JobRun
+type JobRunSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of JobRun. Edit jobrun_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// JobRunStatus defines the observed state of JobRun
+type JobRunStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// JobRun is the Schema for the jobruns API
+type JobRun struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   JobRunSpec   `json:"spec,omitempty"`
+	Status JobRunStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// JobRunList contains a list of JobRun
+type JobRunList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []JobRun `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&JobRun{}, &JobRunList{})
+}
--- k8s-operator/go.mod
+++ k8s-operator/go.mod
@@ -3,6 +3,8 @@ module github.com/nucleuscloud/neosync/k8s-operator
 go 1.21
 
 require (
+	github.com/onsi/ginkgo/v2 v2.6.0
+	github.com/onsi/gomega v1.24.1
 	k8s.io/apimachinery v0.26.0
 	k8s.io/client-go v0.26.0
 	sigs.k8s.io/controller-runtime v0.14.1
