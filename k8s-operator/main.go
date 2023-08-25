9e11e5818c29aa4da397b85283da616406c57abf
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-08-25 10:03:58 -0700

    adds job run crd

--- k8s-operator/main.go
+++ k8s-operator/main.go
@@ -103,6 +103,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Job")
 		os.Exit(1)
 	}
+	if err = (&controllers.JobRunReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "JobRun")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
