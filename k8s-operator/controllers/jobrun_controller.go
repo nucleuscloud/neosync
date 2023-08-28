c0fecb22b4e9a0b40c49886c25907a02f6a79efa
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-08-28 11:24:52 -0700

    adds checks for benthos config secret

--- k8s-operator/controllers/jobrun_controller.go
+++ k8s-operator/controllers/jobrun_controller.go
@@ -19,16 +19,19 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"time"
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	neosyncdevv1alpha1 "github.com/nucleuscloud/neosync/k8s-operator/api/v1alpha1"
 )
 
@@ -43,6 +46,7 @@ type JobRunReconciler struct {
 //+kubebuilder:rbac:groups=neosync.dev,resources=jobruns/finalizers,verbs=update
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -68,7 +72,7 @@ func (r *JobRunReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	}
 
 	if jobrun.Status.JobStatus == nil || jobrun.Status.JobStatus.CompletionTime == nil {
-		logger.Info("creating job")
+		logger.Info("reconciling job")
 
 		job := &batchv1.Job{}
 		err = r.Get(ctx, req.NamespacedName, job)
@@ -76,6 +80,15 @@ func (r *JobRunReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 			logger.Error(err, "failed to get job")
 			return ctrl.Result{}, err
 		} else if err != nil && apierrors.IsNotFound(err) {
+			isConfigPresent, err := isBenthosConfigPresent(ctx, r.Client, req.Namespace, jobrun.Spec.RunConfig, logger)
+			if err != nil {
+				logger.Error(err, "unable to check if benthos config is present prior to creating job")
+				return ctrl.Result{RequeueAfter: 30 * time.Second}, err
+			}
+			if !isConfigPresent {
+				logger.Info("benthos config not present in job spec, or corresponding secret is not found or in correct format")
+				return ctrl.Result{RequeueAfter: 30 * time.Second}, nil
+			}
 			job = &batchv1.Job{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace: req.Namespace,
@@ -130,6 +143,7 @@ func (r *JobRunReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 				logger.Error(err, "unable to set ownership reference on job")
 				return ctrl.Result{}, err
 			}
+			logger.Info("attempting to create job")
 			if err = r.Create(ctx, job); err != nil {
 				logger.Error(err, "unable to create job")
 				return ctrl.Result{}, err
@@ -146,6 +160,48 @@ func (r *JobRunReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	logger.Info(fmt.Sprintf("reconciliation of pipeline %s finished", req.Name))
 
 	return ctrl.Result{}, nil
+}
+
+func isBenthosConfigPresent(
+	ctx context.Context,
+	k8sclient client.Client,
+	namespace string,
+	rc *neosyncdevv1alpha1.RunConfig,
+	logger logr.Logger,
+) (bool, error) {
+	if rc == nil {
+		logger.Info("runconfig was nil")
+		return false, nil
+	}
+	if rc.Benthos == nil || rc.Benthos.ConfigFrom == nil || rc.Benthos.ConfigFrom.SecretKeyRef == nil {
+		logger.Info("benthos, benthos.configfrom, or benthos.configfrom.secretkeyref was nil")
+		return false, nil
+	}
+	if rc.Benthos.ConfigFrom.SecretKeyRef.Name == "" || rc.Benthos.ConfigFrom.SecretKeyRef.Key == "" {
+		logger.Info("benthos secret key ref contains empty strings for name and/or key")
+		return false, nil
+	}
+	secret := &corev1.Secret{}
+	err := k8sclient.Get(ctx, types.NamespacedName{
+		Namespace: namespace,
+		Name:      rc.Benthos.ConfigFrom.SecretKeyRef.Name,
+	}, secret)
+	if err != nil && !apierrors.IsNotFound(err) {
+		return false, fmt.Errorf("isBenthosConfigPresent: %w", err)
+	} else if err != nil && apierrors.IsNotFound(err) {
+		logger.Info("benthos secret was not found", "error", err)
+		return false, nil
+	}
+	if secret.Data == nil {
+		logger.Info("benthos data was nil")
+		return false, nil
+	}
+	if _, ok := secret.Data[rc.Benthos.ConfigFrom.SecretKeyRef.Key]; !ok {
+		logger.Info("benthos secret did not have referenced key")
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
