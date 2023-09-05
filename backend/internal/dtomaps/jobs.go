04c6f65a8f4eb52e2d80625d566abab9026435b9
Alisha <alisha@nucleuscloud.com>
2023-09-05 15:53:55 -0700

    fix lint

--- backend/internal/dtomaps/jobs.go
+++ backend/internal/dtomaps/jobs.go
@@ -29,8 +29,8 @@ func ToJobDto(
 		Id:                       inputJob.Labels[k8s_utils.NeosyncUuidLabel],
 		Name:                     inputJob.Name,
 		CreatedAt:                timestamppb.New(inputJob.CreationTimestamp.Time),
-		UpdatedAt:                timestamppb.New(inputJob.CreationTimestamp.Time), // TODO
-		Status:                   mgmtv1alpha1.JobStatus(0),                        // TODO
+		UpdatedAt:                timestamppb.New(inputJob.CreationTimestamp.Time), // TODO @alisha implement
+		Status:                   mgmtv1alpha1.JobStatus(0),                        // TODO @alisha implement
 		ConnectionSourceId:       inputSourceConnId,
 		CronSchedule:             inputJob.Spec.CronSchedule,
 		ConnectionDestinationIds: inputDestConnIds,
--- frontend/app/api/jobs/route.ts
+++ frontend/app/api/jobs/route.ts
@@ -7,10 +7,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
   return withNeosyncContext(async (ctx) => {
-    return ctx.jobsClient.getJobs(
-      new GetJobsRequest({
-      })
-    );
+    return ctx.jobsClient.getJobs(new GetJobsRequest({}));
   })(req);
 }
 
