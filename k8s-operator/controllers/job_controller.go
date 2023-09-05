aee92c55d213b803b9f4f3fd7937aef94c1c2adf
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-09-05 12:59:45 -0700

    updates job controller to have correct rbac

--- k8s-operator/controllers/job_controller.go
+++ k8s-operator/controllers/job_controller.go
@@ -43,6 +43,11 @@ type JobReconciler struct {
 //+kubebuilder:rbac:groups=neosync.dev,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=neosync.dev,resources=jobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=neosync.dev,resources=jobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac,resources=roles,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=batch,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
