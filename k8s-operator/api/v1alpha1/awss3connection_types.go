f9ef470108720ec7ee2b2cf3a2eedccee96d1db0
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-09-05 17:01:30 -0700

    adds stub aws s3 connection types

---
+++ k8s-operator/api/v1alpha1/awss3connection_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AwsS3ConnectionSpec defines the desired state of AwsS3Connection
+type AwsS3ConnectionSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of AwsS3Connection. Edit awss3connection_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// AwsS3ConnectionStatus defines the observed state of AwsS3Connection
+type AwsS3ConnectionStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// AwsS3Connection is the Schema for the awss3connections API
+type AwsS3Connection struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AwsS3ConnectionSpec   `json:"spec,omitempty"`
+	Status AwsS3ConnectionStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// AwsS3ConnectionList contains a list of AwsS3Connection
+type AwsS3ConnectionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AwsS3Connection `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AwsS3Connection{}, &AwsS3ConnectionList{})
+}
--- k8s-operator/main.go
+++ k8s-operator/main.go
@@ -133,6 +133,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "TaskRun")
 		os.Exit(1)
 	}
+	if err = (&controllers.AwsS3ConnectionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AwsS3Connection")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
