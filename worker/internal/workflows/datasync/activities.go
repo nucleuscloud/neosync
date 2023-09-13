7383aa9529bd949cf7cc430361c1c4d39c29c388
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-09-13 14:19:47 -0700

    split out workflow and activites into sep files

--- worker/internal/workflows/datasync/activities.go
+++ worker/internal/workflows/datasync/activities.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
 
 	"connectrpc.com/connect"
 
@@ -18,49 +17,7 @@ import (
 	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
 	"github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1/mgmtv1alpha1connect"
 	neosync_benthos "github.com/nucleuscloud/neosync/worker/internal/benthos"
-	"go.temporal.io/sdk/workflow"
 )
-
-type WorkflowRequest struct {
-	JobId string
-}
-
-type WorkflowResponse struct{}
-
-func Workflow(ctx workflow.Context, req *WorkflowRequest) (*WorkflowResponse, error) {
-	ao := workflow.ActivityOptions{
-		StartToCloseTimeout: 10 * time.Second,
-	}
-	ctx = workflow.WithActivityOptions(ctx, ao)
-	var bcResp *GenerateBenthosConfigsResponse
-
-	var wfActivites *Activities
-	err := workflow.ExecuteActivity(ctx, wfActivites.GenerateBenthosConfigs, &GenerateBenthosConfigsRequest{
-		JobId:      req.JobId,
-		BackendUrl: "http://localhost:8080",
-	}).Get(ctx, bcResp)
-	if err != nil {
-		return nil, err
-	}
-
-	futures := make([]workflow.Future, len(bcResp.BenthosConfigs))
-	for idx := range bcResp.BenthosConfigs {
-		bc := bcResp.BenthosConfigs[idx]
-		future := workflow.ExecuteActivity(ctx, nil, bc)
-		futures = append(futures, future)
-	}
-
-	// todo: this should be heavily optimized
-	for _, future := range futures {
-		var resp any
-		err := future.Get(ctx, resp)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return &WorkflowResponse{}, nil
-}
 
 type GenerateBenthosConfigsRequest struct {
 	JobId      string
