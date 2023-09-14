7a02f03d9499c3816b3b77304140fca2df18a7ce
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-09-14 16:05:45 -0700

    wiring up rough authentication

---
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import type { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
+import Auth0 from 'next-auth/providers/auth0';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    Auth0({
+      clientId: process.env.AUTH0_CLIENT_ID ?? '',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
+      issuer: process.env.AUTH0_ISSUER,
+      authorization: {
+        params: {
+          audience: process.env.AUTH0_AUDIENCE,
+          scope: process.env.AUTH0_SCOPE,
+        },
+      },
+    }),
+  ],
+  session: { strategy: 'jwt' },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,23 @@
 import '@/app/globals.css';
 import SiteFooter from '@/components/SiteFooter';
 import SiteHeader from '@/components/SiteHeader';
+import { SessionProvider } from '@/components/session-provider';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { fontSans } from '@/libs/fonts';
 import { cn } from '@/libs/utils';
 import { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
 export const metadata: Metadata = {};
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
@@ -29,12 +33,14 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="relative flex min-h-screen flex-col">
-            <SiteHeader />
-            <div className="flex-1 container">{children}</div>
-            <SiteFooter />
-            <Toaster />
-          </div>
+          <SessionProvider session={session}>
+            <div className="relative flex min-h-screen flex-col">
+              <SiteHeader />
+              <div className="flex-1 container">{children}</div>
+              <SiteFooter />
+              <Toaster />
+            </div>
+          </SessionProvider>
         </ThemeProvider>
       </body>
     </html>
