32921bf22a4692fc4c7f10acc54c9891a9c7c6c2
Alisha <alisha@nucleuscloud.com>
2023-09-15 12:52:01 -0700

    fix job-run job id conversion

--- backend/internal/dtomaps/job-runs.go
+++ backend/internal/dtomaps/job-runs.go
@@ -1,6 +1,9 @@
 package dtomaps
 
 import (
+	"fmt"
+	"strings"
+
 	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
 	workflowpb "go.temporal.io/api/workflow/v1"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -11,10 +14,11 @@ func ToJobRunDto(
 ) *mgmtv1alpha1.JobRun {
 
 	attributes := input.GetSearchAttributes().IndexedFields
+	fmt.Println(string(attributes["TemporalScheduledById"].Data))
 
 	return &mgmtv1alpha1.JobRun{
 		Id:        input.Execution.WorkflowId,
-		JobId:     attributes["TemporalScheduledById"].String(),
+		JobId:     strings.Trim(string(attributes["TemporalScheduledById"].Data), "\""),
 		Name:      input.Type.Name,
 		Status:    mgmtv1alpha1.JobRunStatus(0), // TODO @alisha implement
 		CreatedAt: timestamppb.New(*input.StartTime),
