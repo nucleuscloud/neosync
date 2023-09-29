ab7e6fb03f3a228244a3df984ed743d462be9298
Evis Drenova <evis@usenucleus.cloud>
2023-09-29 13:03:38 -0700

    added custom cards and custom card lists and stylings

---
+++ docs/src/components/HomepageFeatures/CustomCard.tsx
@@ -0,0 +1,24 @@
+import Link from "@docusaurus/Link";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import React, { ReactElement } from "react";
+import { Card } from "./CustomCardList";
+
+export function CustomCard(props: Card): ReactElement {
+  const { title, description, icon, link } = props;
+  return (
+    <Link href={link} className="custom-card hover:no-underline">
+      <div className="flex flex-col space-y-4">
+        <div className="flex flex-row gap-2">
+          <div className="border-2 border-blue-200 rounded-lg bg-blue-100 p-2">
+            {icon}
+          </div>
+          <div className="card-title">{title}</div>
+        </div>
+        <div className="card-text">{description}</div>
+      </div>
+      <div className="flex justify-end">
+        <ArrowRightIcon />
+      </div>
+    </Link>
+  );
+}
--- docs/src/theme/NavbarItem/Gitlink.tsx
+++ docs/src/theme/NavbarItem/Gitlink.tsx
@@ -1,16 +1,15 @@
-import Link from "@docusaurus/Link";
-import { ExternalLinkIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
-import { ReactElement } from "react";
-import React from "react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import React, { ReactElement } from "react";
 
 export default function Gitlink(): ReactElement {
   return (
-    <a href="https://github.com/nucleuscloud/neosync" className="no-underline">
-      <div className="flex flex-row items-center gap-2 pr-10">
-        <div>
-          <GitHubLogoIcon />
-        </div>
-        <div>Github</div>
+    <a
+      href="https://github.com/nucleuscloud/neosync"
+      className="hover:no-underline"
+    >
+      <div className="flex flex-row items-center gap-2 mr-10 p-2 rounded-full hover:bg-gray-100 hover:no-underline">
+        <GitHubLogoIcon />
+        <div className="text-sm">Support us on Github</div>
       </div>
     </a>
   );
