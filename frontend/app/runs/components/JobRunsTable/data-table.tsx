9957f40dffb86382461041540b8e98107496013b
Alisha <alisha@nucleuscloud.com>
2023-10-02 12:30:22 -0700

    clean up

--- frontend/app/runs/components/JobRunsTable/data-table.tsx
+++ frontend/app/runs/components/JobRunsTable/data-table.tsx
@@ -43,7 +43,6 @@ export function DataTable<TData, TValue>({
     []
   );
   const [sorting, setSorting] = React.useState<SortingState>([]);
-  console.log('data', data);
 
   const table = useReactTable({
     data,
