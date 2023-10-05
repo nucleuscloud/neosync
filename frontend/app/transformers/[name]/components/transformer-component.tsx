d3ef994c36da95213f32ca20bab2f52f90bcf939
Evis Drenova <evis@usenucleus.cloud>
2023-10-05 16:25:11 -0700

    base email transformer form component

--- frontend/app/transformers/[name]/components/transformer-component.tsx
+++ frontend/app/transformers/[name]/components/transformer-component.tsx
@@ -2,6 +2,7 @@
 import PageHeader from '@/components/headers/PageHeader';
 import { Transformer } from '@/neosync-api-client/mgmt/v1alpha1/transformer_pb';
 import { ReactElement } from 'react';
+import EmailTransformerForm from './EmailTransformerForm';
 
 interface TransformerComponent {
   name: string;
@@ -37,7 +38,7 @@ export function getTransformerComponentDetails(
             description="This transformer is a passthrough."
           />
         ),
-        body: <div>{transformer?.name} transformer for right now</div>,
+        body: <div>{handleTransformerForm(transformer)}</div>,
       };
     default:
       return {
@@ -61,4 +62,14 @@ export function getTransformerComponentDetails(
         ),
       };
   }
+}
+
+function handleTransformerForm(transformer: Transformer): ReactElement {
+  switch (transformer.name) {
+    case 'email':
+      return <EmailTransformerForm transformer={transformer} />;
+    default:
+      <div>No tranformer component found</div>;
+  }
+  return <div></div>;
 }
