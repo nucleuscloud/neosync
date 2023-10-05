1d2d6263fafa4b4a4fa1febd2ed8c6ffd7f92a9a
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-05 13:16:17 -0700

    cleaning up workflow

--- worker/pkg/workflows/datasync/workflow.go
+++ worker/pkg/workflows/datasync/workflow.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/spf13/viper"
+	"go.temporal.io/sdk/log"
+
 	"go.temporal.io/sdk/workflow"
 	"gopkg.in/yaml.v3"
 )
@@ -42,72 +44,10 @@ func Workflow(wfctx workflow.Context, req *WorkflowRequest) (*WorkflowResponse, 
 		return nil, err
 	}
 
-	// benthos jobs that have been completed
-	completed := map[string]struct{}{}
 	started := map[string]struct{}{}
-	// syncChan := workflow.NewChannel(wfctx)
+	completed := map[string]struct{}{}
+
 	workselector := workflow.NewSelector(ctx)
-
-	wg := workflow.NewWaitGroup(ctx)
-	wg.Add(len(bcResp.BenthosConfigs))
-
-	// workflow.GoNamed(ctx, "ConfigSync", func(ctx workflow.Context) {
-	// 	var bcName string
-	// 	syncChan.Receive(ctx, &bcName)
-
-	// 	if bcName == "" {
-	// 		wg.Done()
-	// 	}
-
-	// 	logger.Info("benthos config has completed", "name", bcName)
-	// 	completed[bcName] = struct{}{}
-
-	// 	logger.Info(fmt.Sprintf("all completed configs: %s", strings.Join(getMapKeys(completed), ",")))
-
-	// 	for len(completed) != len(bcResp.BenthosConfigs) {
-	// 		for _, bc := range bcResp.BenthosConfigs {
-	// 			bc := bc
-	// 			if _, ok := completed[bc.Name]; ok {
-	// 				continue
-	// 			}
-	// 			isready := true
-	// 			for _, dep := range bc.DependsOn {
-	// 				if _, ok := completed[dep]; !ok {
-	// 					isready = false
-	// 					break
-	// 				}
-	// 			}
-	// 			if !isready {
-	// 				continue
-	// 			}
-	// 			configbits, _ := yaml.Marshal(bc.Config)
-	// 			if err != nil {
-	// 				logger.Error("unable to marshal benthos config", "err", err)
-	// 				// return nil, fmt.Errorf("unable to marshal benthos config: %w", err)
-	// 			}
-	// 			future, settable := workflow.NewFuture(ctx)
-	// 			workflow.Go(ctx, func(ctx workflow.Context) {
-	// 				var result SyncResponse
-	// 				err := workflow.ExecuteActivity(ctx, wfActivites.Sync, &SyncRequest{BenthosConfig: string(configbits)}).Get(ctx, &result)
-	// 				if err != nil {
-	// 					settable.SetError(err)
-	// 				} else {
-	// 					settable.SetValue(result)
-	// 					syncChan.Send(ctx, bc.Name)
-	// 				}
-	// 			})
-	// 			workselector.AddFuture(future, func(f workflow.Future) {
-	// 				var result SyncResponse
-	// 				err := f.Get(ctx, &result)
-	// 				if err != nil {
-	// 					logger.Error("activity did not complete", "err", err)
-	// 					// todo: cancel workflow
-	// 				}
-	// 			})
-	// 		}
-	// 	}
-	// 	wg.Done()
-	// })
 
 	splitConfigs := splitBenthosConfigs(bcResp.BenthosConfigs)
 	var activityErr error
@@ -115,23 +55,9 @@ func Workflow(wfctx workflow.Context, req *WorkflowRequest) (*WorkflowResponse, 
 
 	for _, bc := range splitConfigs.Root {
 		bc := bc
-		configbits, err := yaml.Marshal(bc.Config)
-		if err != nil {
-			logger.Error("unable to marshal benthos config", "err", err)
-			return nil, fmt.Errorf("unable to marshal benthos config: %w", err)
-		}
-		future, settable := workflow.NewFuture(childctx)
-		logger.Info("triggering config sync", "name", bc.Name)
-		started[bc.Name] = struct{}{}
-		workflow.GoNamed(childctx, bc.Name, func(ctx workflow.Context) {
-			defer wg.Done()
-			var result SyncResponse
-			err := workflow.ExecuteActivity(ctx, wfActivites.Sync, &SyncRequest{BenthosConfig: string(configbits)}).Get(ctx, &result)
-			completed[bc.Name] = struct{}{}
-			settable.Set(result, err)
-		})
+		future := invokeSync(bc, childctx, started, completed, logger)
 		workselector.AddFuture(future, func(f workflow.Future) {
-			logger.Info("config sync completed (future)", "name", bc.Name)
+			logger.Info("config sync completed", "name", bc.Name)
 			var result SyncResponse
 			err := f.Get(childctx, &result)
 			if err != nil {
@@ -143,12 +69,12 @@ func Workflow(wfctx workflow.Context, req *WorkflowRequest) (*WorkflowResponse, 
 	}
 
 	for i := 0; i < len(bcResp.BenthosConfigs); i++ {
-		logger.Info("*** blocking select ***", "i", i)
+		logger.Debug("*** blocking select ***", "i", i)
 		workselector.Select(ctx)
 		if activityErr != nil {
 			return nil, fmt.Errorf("activity failed: %w", activityErr)
 		}
-		logger.Info("*** post select ***", "i", i)
+		logger.Debug("*** post select ***", "i", i)
 
 		for _, bc := range splitConfigs.Dependents {
 			bc := bc
@@ -158,24 +84,9 @@ func Workflow(wfctx workflow.Context, req *WorkflowRequest) (*WorkflowResponse, 
 			if !isConfigReady(bc, completed) {
 				continue
 			}
-			started[bc.Name] = struct{}{}
-
-			configbits, err := yaml.Marshal(bc.Config)
-			if err != nil {
-				logger.Error("unable to marshal benthos config", "err", err)
-				return nil, fmt.Errorf("unable to marshal benthos config: %w", err)
-			}
-			future, settable := workflow.NewFuture(childctx)
-			logger.Info("triggering config sync", "name", bc.Name)
-			workflow.GoNamed(childctx, bc.Name, func(ctx workflow.Context) {
-				defer wg.Done()
-				completed[bc.Name] = struct{}{}
-				var result SyncResponse
-				err := workflow.ExecuteActivity(ctx, wfActivites.Sync, &SyncRequest{BenthosConfig: string(configbits)}).Get(ctx, &result)
-				settable.Set(result, err)
-			})
+			future := invokeSync(bc, childctx, started, completed, logger)
 			workselector.AddFuture(future, func(f workflow.Future) {
-				logger.Info("config sync completed (future)", "name", bc.Name)
+				logger.Info("config sync completed", "name", bc.Name)
 				var result SyncResponse
 				err := f.Get(childctx, &result)
 				if err != nil {
@@ -187,12 +98,35 @@ func Workflow(wfctx workflow.Context, req *WorkflowRequest) (*WorkflowResponse, 
 		}
 	}
 
-	logger.Info("waiting for wg to finish")
-	wg.Wait(ctx)
-
 	logger.Info("workflow completed")
 
 	return &WorkflowResponse{}, nil
+}
+
+func invokeSync(
+	config *benthosConfigResponse,
+	ctx workflow.Context,
+	started map[string]struct{},
+	completed map[string]struct{},
+	logger log.Logger,
+) workflow.Future {
+	future, settable := workflow.NewFuture(ctx)
+	logger.Info("triggering config sync", "name", config.Name)
+	started[config.Name] = struct{}{}
+	var wfActivites *Activities
+	workflow.GoNamed(ctx, config.Name, func(ctx workflow.Context) {
+		configbits, err := yaml.Marshal(config.Config)
+		if err != nil {
+			logger.Error("unable to marshal benthos config", "err", err)
+			settable.SetError(fmt.Errorf("unable to marshal benthos config: %w", err))
+			return
+		}
+		var result SyncResponse
+		err = workflow.ExecuteActivity(ctx, wfActivites.Sync, &SyncRequest{BenthosConfig: string(configbits)}).Get(ctx, &result)
+		completed[config.Name] = struct{}{}
+		settable.Set(result, err)
+	})
+	return future
 }
 
 func isConfigReady(config *benthosConfigResponse, completed map[string]struct{}) bool {
@@ -210,14 +144,6 @@ func isConfigReady(config *benthosConfigResponse, completed map[string]struct{})
 	}
 	return true
 }
-
-// func getMapKeys[T any](val map[string]T) []string {
-// 	keys := make([]string, 0, len(val))
-// 	for key := range val {
-// 		keys = append(keys, key)
-// 	}
-// 	return keys
-// }
 
 type SplitConfigs struct {
 	Root       []*benthosConfigResponse
