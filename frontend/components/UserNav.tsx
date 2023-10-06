7e3049f53825e4ed68e9a9386c139acc49a2779a
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-06 11:39:04 -0700

    adds padding to run table

--- frontend/components/UserNav.tsx
+++ frontend/components/UserNav.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -17,7 +17,7 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            {/* <AvatarImage src="/avatars/01.png" alt="@shadcn" /> */}
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
         </Button>
