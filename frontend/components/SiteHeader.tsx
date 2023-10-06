fd596f0d039ea705ec5770f03b8f04cf3008639e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-06 12:08:12 -0700

    removes unused svgs

--- frontend/components/SiteHeader.tsx
+++ frontend/components/SiteHeader.tsx
@@ -1,35 +1,34 @@
 import { siteConfig } from '@/app/config/site';
 import { cn } from '@/libs/utils';
+import { GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 import { MainNav } from './MainNav';
 import { MobileNav } from './MobileNav';
 import { ModeToggle } from './ModeToggle';
 import { UserNav } from './UserNav';
-import { Icons } from './icons';
 import { buttonVariants } from './ui/button';
 
 export default function SiteHeader() {
+  const iconButtonClassNames = cn(
+    buttonVariants({
+      variant: 'ghost',
+    }),
+    'p-0 px-2'
+  );
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center">
         <MainNav />
         <MobileNav />
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <nav className="flex items-center space-x-1">
+        <div className="flex flex-1 md:justify-end">
+          <nav className="flex items-center space-x-2">
             <Link
               href={siteConfig.links.github}
               target="_blank"
               rel="noreferrer"
             >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: 'ghost',
-                  }),
-                  'w-9 px-0'
-                )}
-              >
-                <Icons.gitHub className="h-4 w-4" />
+              <div className={iconButtonClassNames}>
+                <GitHubLogoIcon />
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
@@ -38,15 +37,8 @@ export default function SiteHeader() {
               target="_blank"
               rel="noreferrer"
             >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: 'ghost',
-                  }),
-                  'w-9 px-0'
-                )}
-              >
-                <Icons.twitter className="h-4 w-4 fill-current" />
+              <div className={iconButtonClassNames}>
+                <TwitterLogoIcon />
                 <span className="sr-only">Twitter</span>
               </div>
             </Link>
