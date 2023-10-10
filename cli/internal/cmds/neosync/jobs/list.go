43620931e2d4bae8925db6294f1f60595858ecac
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-09 17:06:47 -0700

    adds crappy list command

--- cli/internal/cmds/neosync/jobs/list.go
+++ cli/internal/cmds/neosync/jobs/list.go
@@ -1,8 +1,13 @@
 package jobs_cmd
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 
+	"connectrpc.com/connect"
+	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
+	"github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1/mgmtv1alpha1connect"
 	"github.com/spf13/cobra"
 )
 
@@ -12,12 +17,27 @@ func newListCmd() *cobra.Command {
 		Short: "list jobs",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cmd.SilenceUsage = true
-			return listJobs()
+			return listJobs(cmd.Context())
 		},
 	}
 }
 
-func listJobs() error {
-	fmt.Println("todo")
+func listJobs(
+	ctx context.Context,
+) error {
+	jobclient := mgmtv1alpha1connect.NewJobServiceClient(
+		http.DefaultClient,
+		"http://localhost:8080",
+	)
+	res, err := jobclient.GetJobs(ctx, connect.NewRequest[mgmtv1alpha1.GetJobsRequest](&mgmtv1alpha1.GetJobsRequest{
+		AccountId: "4f45a5ff-b1ff-47f5-9f89-f576ebd1d03c", // todo: pull from context
+	}))
+	if err != nil {
+		return err
+	}
+	// todo: use table library from nucleus cli
+	for _, job := range res.Msg.Jobs {
+		fmt.Println(job.Id, job.Name)
+	}
 	return nil
 }
