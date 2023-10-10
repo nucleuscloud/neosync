fc629ef4999ed3797cf9cfea8f9acc5c749de8ab
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-10 11:46:19 -0700

    fixes build metadata in cli

--- cli/internal/version/version.go
+++ cli/internal/version/version.go
@@ -14,16 +14,16 @@ type VersionInfo struct {
 	Platform   string `json:"platform" yaml:"platform"`
 }
 
-func (info VersionInfo) String() string {
+func (info *VersionInfo) String() string {
 	return info.GitVersion
 }
 
 // Get returns the overall codebase version. It's for detecting
 // what code a binary was built from.
-func Get() VersionInfo {
+func Get() *VersionInfo {
 	// These variables typically come from -ldflags settings and in
 	// their absence fallback to the settings in ./base.go
-	return VersionInfo{
+	return &VersionInfo{
 		GitVersion: gitVersion,
 		GitCommit:  gitCommit,
 		BuildDate:  buildDate,
