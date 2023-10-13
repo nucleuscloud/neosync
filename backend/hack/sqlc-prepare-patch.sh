c9aa570a11578a956260a8aa81e496c74ba95e17
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-13 11:56:37 -0700

    updates script to specify sqlc filepath

--- backend/hack/sqlc-prepare-patch.sh
+++ backend/hack/sqlc-prepare-patch.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 databaseUri=$1
+FILEPATH=$2
 
-yq -i ".sql[0].database.uri = \"$databaseUri\"" sqlc.yaml
+if [ "$FILEPATH" == "" ]; then
+  FILEPATH="sqlc.yaml"
+fi
+
+yq -i ".sql[0].database.uri = \"$databaseUri\"" "$FILEPATH"
 
 # creates array if rules doesn't exist, concats rules + the sqlc/db-prepare
 # shouldn't need to de-dupe here because by default we don't have sqlc/db-prepare in the standard config
-yq -i ".sql[0].rules = [] + .sql[0].rules + \"sqlc/db-prepare\"" sqlc.yaml
+yq -i ".sql[0].rules = [] + .sql[0].rules + \"sqlc/db-prepare\"" "$FILEPATH"
