a4eea55800bf74574177066df8de0253d415a3a1
Evis Drenova <evis@usenucleus.cloud>
2023-10-13 14:15:48 -0700

    tranformer metadata handling

--- frontend/app/transformers/EditTransformerOptions.tsx
+++ frontend/app/transformers/EditTransformerOptions.tsx
@@ -3,6 +3,7 @@ import { Separator } from '@/components/ui/separator';
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -16,6 +17,12 @@ interface Props {
   transformer: Transformer | undefined;
   index: number;
 }
+
+interface TransformerMetadata {
+  name: string;
+  description: string;
+}
+
 export default function EditTransformerOptions(props: Props): ReactElement {
   const { transformer, index } = props;
 
@@ -37,7 +44,20 @@ export default function EditTransformerOptions(props: Props): ReactElement {
       <SheetContent className="w-[800px]">
         <SheetHeader>
           <div className="flex flex-row justify-between w-full">
-            <SheetTitle>{transformer?.value}</SheetTitle>
+            <div className="flex flex-col space-y-2">
+              <SheetTitle>
+                {
+                  handleTransformerMetadata(transformer ?? new Transformer())
+                    .name
+                }
+              </SheetTitle>
+              <SheetDescription>
+                {
+                  handleTransformerMetadata(transformer ?? new Transformer())
+                    .description
+                }
+              </SheetDescription>
+            </div>
             <Button variant="ghost" onClick={() => setIsSheetOpen(false)}>
               <Cross2Icon className="h-4 w-4" />
             </Button>
@@ -68,3 +88,44 @@ function handleTransformerForm(
   }
   return <div></div>;
 }
+
+const handleTransformerMetadata = (t: Transformer): TransformerMetadata => {
+  const tEntries: Record<string, TransformerMetadata>[] = [
+    {
+      email: {
+        name: 'Email',
+        description: 'Anonymizes or generates a new email.',
+      },
+    },
+    {
+      phone_number: {
+        name: 'Phone Number',
+        description: 'Anonymizes or generates a new phone number.',
+      },
+    },
+    {
+      first_name: {
+        name: 'First Name',
+        description: 'Anonymizes or generates a new first name.',
+      },
+    },
+    { uuidv4: { name: 'UUIDv4', description: 'Generates a new UUIDv4 id.' } },
+    {
+      passthrough: {
+        name: 'Passthrough',
+        description:
+          'Passes the input value through to the desination with no changes.',
+      },
+    },
+  ];
+  const res = tEntries.find((item) => item[t.value]);
+
+  const def = {
+    default: {
+      name: 'Undefined',
+      description: 'Undefined Transformer',
+    },
+  };
+
+  return res ? res[t.value] : def.default;
+};
