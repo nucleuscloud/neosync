048e2bb03c2ec8c8aa5fcedc4aa3dd6feb518ba2
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2023-10-16 15:28:37 -0700

    Nick/neos 73 add web server to worker for better orchestration (#314)

--- worker/go.mod
+++ worker/go.mod
@@ -4,6 +4,8 @@ go 1.21.1
 
 require (
 	connectrpc.com/connect v1.11.1
+	connectrpc.com/grpchealth v1.3.0
+	connectrpc.com/grpcreflect v1.2.0
 	github.com/benthosdev/benthos/v4 v4.22.0
 	github.com/bxcodec/faker/v4 v4.0.0-beta.3
 	github.com/google/uuid v1.3.1
@@ -13,6 +15,8 @@ require (
 	github.com/spf13/viper v1.17.0
 	github.com/stretchr/testify v1.8.4
 	go.temporal.io/sdk v1.25.0
+	go.uber.org/atomic v1.11.0
+	golang.org/x/net v0.17.0
 	golang.org/x/sync v0.4.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -208,12 +212,10 @@ require (
 	go.opentelemetry.io/otel/metric v1.19.0 // indirect
 	go.opentelemetry.io/otel/trace v1.19.0 // indirect
 	go.temporal.io/api v1.24.0 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.12.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/term v0.13.0 // indirect
--- worker/internal/cmds/worker/serve/serve.go
+++ worker/internal/cmds/worker/serve/serve.go
@@ -1,15 +1,24 @@
 package serve_connect
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"log"
 	"log/slog"
+	"net/http"
 	"os"
+	"time"
 
+	"connectrpc.com/grpchealth"
+	"connectrpc.com/grpcreflect"
 	"github.com/nucleuscloud/neosync/worker/pkg/workflows/datasync"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func NewCmd() *cobra.Command {
@@ -38,9 +47,12 @@ func serve() error {
 	if taskQueue == "" {
 		return errors.New("must provide TEMPORAL_TASK_QUEUE environment variable")
 	}
+	jsonloghandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})
+	logger := slog.New(jsonloghandler)
+	loglogger := slog.NewLogLogger(jsonloghandler, slog.LevelInfo)
 
 	temporalClient, err := client.Dial(client.Options{
-		Logger:    slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})),
+		Logger:    logger,
 		HostPort:  temporalUrl,
 		Namespace: temporalNamespace,
 		// Interceptors: ,
@@ -57,9 +69,56 @@ func serve() error {
 	w.RegisterWorkflow(datasync.Workflow)
 	w.RegisterActivity(&datasync.Activities{})
 
-	err = w.Run(worker.InterruptCh())
-	if err != nil {
+	if err := w.Start(); err != nil {
+		return err
+	}
+
+	httpServer := getHttpServer(loglogger)
+
+	go func() {
+		logger.Info(fmt.Sprintf("listening on %s", httpServer.Addr))
+		err := httpServer.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logger.Error(err.Error())
+		}
+	}()
+
+	<-worker.InterruptCh()
+
+	w.Stop()
+
+	ctx, cancelHandler := context.WithDeadline(context.Background(), time.Now().Add(2*time.Second))
+	defer cancelHandler()
+	if err := httpServer.Shutdown(ctx); err != nil {
 		return err
 	}
 	return nil
+}
+
+func getHttpServer(logger *log.Logger) *http.Server {
+	port := viper.GetInt32("PORT")
+	if port == 0 {
+		port = 8080
+	}
+	host := viper.GetString("HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+	mux := http.NewServeMux()
+	mux.Handle(grpchealth.NewHandler(grpchealth.NewStaticChecker()))
+
+	reflector := grpcreflect.NewStaticReflector()
+	mux.Handle(grpcreflect.NewHandlerV1(reflector))
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(reflector))
+
+	api := http.NewServeMux()
+	mux.Handle("/", api)
+
+	httpServer := http.Server{
+		Addr:              fmt.Sprintf("%s:%d", host, port),
+		Handler:           h2c.NewHandler(mux, &http2.Server{}),
+		ErrorLog:          logger,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+	return &httpServer
 }
