c4ea83be82382d0493e3ac4d27bfe22d7f95c588
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-17 10:12:30 -0700

    removes empty class name

--- frontend/app/jobs/[id]/layout.tsx
+++ frontend/app/jobs/[id]/layout.tsx
@@ -78,7 +78,7 @@ export default function SettingsLayout({ children, params }: PageProps) {
         />
       </div>
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="">
+        <aside>
           <SidebarNav buttonClassName="px-8" items={sidebarNavItems} />
         </aside>
         <div className="flex-1 lg:max-w-8xl">{children}</div>
