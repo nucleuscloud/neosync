435c32dc72c6778a957891f9b402f66c9accefff
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-19 16:04:43 -0700

    less margin padding in overview

--- frontend/components/headers/PageHeader.tsx
+++ frontend/components/headers/PageHeader.tsx
@@ -3,7 +3,7 @@ import { Separator } from '../ui/separator';
 
 interface Props {
   header: string;
-  description: string;
+  description?: string;
 
   extraHeading?: ReactNode;
   leftIcon?: ReactNode;
@@ -20,7 +20,9 @@ export default function PageHeader(props: Props) {
         </div>
         {extraHeading ? <div>{extraHeading}</div> : null}
       </div>
-      <h3 className="text-muted-foreground">{description}</h3>
+      {description ? (
+        <h3 className="text-muted-foreground">{description}</h3>
+      ) : null}
       <div className="my-4">
         <Separator />
       </div>
