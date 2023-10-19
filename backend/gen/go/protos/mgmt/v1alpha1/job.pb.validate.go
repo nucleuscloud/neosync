ec97ebb75da254746ac69b7aa092d9ce042f8d57
Evis Drenova <evis@usenucleus.cloud>
2023-10-19 13:10:30 -0700

    base random int transformer and tests

--- backend/gen/go/protos/mgmt/v1alpha1/job.pb.validate.go
+++ backend/gen/go/protos/mgmt/v1alpha1/job.pb.validate.go
@@ -9569,6 +9569,47 @@ func (m *TransformerConfig) validate(all bool) error {
 			}
 		}
 
+	case *TransformerConfig_RandomIntConfig:
+		if v == nil {
+			err := TransformerConfigValidationError{
+				field:  "Config",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetRandomIntConfig()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, TransformerConfigValidationError{
+						field:  "RandomIntConfig",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, TransformerConfigValidationError{
+						field:  "RandomIntConfig",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetRandomIntConfig()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return TransformerConfigValidationError{
+					field:  "RandomIntConfig",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	case *TransformerConfig_NullConfig:
 		if v == nil {
 			err := TransformerConfigValidationError{
@@ -10807,3 +10848,106 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RandomBoolValidationError{}
+
+// Validate checks the field values on RandomInt with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *RandomInt) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on RandomInt with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in RandomIntMultiError, or nil
+// if none found.
+func (m *RandomInt) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *RandomInt) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for PreserveLength
+
+	// no validation rules for IntLength
+
+	if len(errors) > 0 {
+		return RandomIntMultiError(errors)
+	}
+
+	return nil
+}
+
+// RandomIntMultiError is an error wrapping multiple validation errors returned
+// by RandomInt.ValidateAll() if the designated constraints aren't met.
+type RandomIntMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m RandomIntMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m RandomIntMultiError) AllErrors() []error { return m }
+
+// RandomIntValidationError is the validation error returned by
+// RandomInt.Validate if the designated constraints aren't met.
+type RandomIntValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e RandomIntValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e RandomIntValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e RandomIntValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e RandomIntValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e RandomIntValidationError) ErrorName() string { return "RandomIntValidationError" }
+
+// Error satisfies the builtin error interface
+func (e RandomIntValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sRandomInt.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = RandomIntValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = RandomIntValidationError{}
