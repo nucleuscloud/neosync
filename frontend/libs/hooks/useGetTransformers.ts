60d1572265a498d3d51c88545f2e0d5296ed75ea
Evis Drenova <evis@usenucleus.cloud>
2023-10-24 10:34:43 -0700

    updated hook

--- frontend/libs/hooks/useGetTransformers.ts
+++ frontend/libs/hooks/useGetTransformers.ts
@@ -1,4 +1,4 @@
-import { GetTransformersResponse } from '@/neosync-api-client/mgmt/v1alpha1/job_pb';
+import { GetTransformersResponse } from '@/neosync-api-client/mgmt/v1alpha1/transformer_pb';
 import { JsonValue } from '@bufbuild/protobuf';
 import { HookReply } from './types';
 import { useNucleusAuthenticatedFetch } from './useNucleusAuthenticatedFetch';
