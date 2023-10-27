306f953286688bfbb9432dff5850005dffa39af5
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-27 21:59:06 +0000

    fix avatar referrer

--- frontend/components/ui/avatar.tsx
+++ frontend/components/ui/avatar.tsx
@@ -27,6 +27,7 @@ const AvatarImage = React.forwardRef<
   <AvatarPrimitive.Image
     ref={ref}
     className={cn('aspect-square h-full w-full', className)}
+    referrerPolicy="no-referrer"
     {...props}
   />
 ));
