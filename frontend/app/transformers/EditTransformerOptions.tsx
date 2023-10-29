0f016e658e7af0cf93bb2a61b8063ef8e94cafa1
Evis Drenova <evis@usenucleus.cloud>
2023-10-28 23:48:20 -0700

    updated the form routing in job definitoin

--- frontend/app/transformers/EditTransformerOptions.tsx
+++ frontend/app/transformers/EditTransformerOptions.tsx
@@ -9,7 +9,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
-import { Transformer } from '@/neosync-api-client/mgmt/v1alpha1/transformer_pb';
+import { CustomTransformer } from '@/neosync-api-client/mgmt/v1alpha1/transformer_pb';
 import { Cross2Icon, Pencil1Icon } from '@radix-ui/react-icons';
 import { ReactElement, useEffect, useRef, useState } from 'react';
 import EmailTransformerForm from './Sheetforms/EmailTransformerForm';
@@ -25,14 +25,8 @@ import RandomStringTransformerForm from './Sheetforms/RandomStringTransformerFor
 import UuidTransformerForm from './Sheetforms/UuidTransformerForm';
 
 interface Props {
-  transformer: Transformer | undefined;
+  transformer: CustomTransformer | undefined;
   index: number;
-}
-
-interface TransformerMetadata {
-  name: string;
-  description: string;
-  type: string;
 }
 
 export default function EditTransformerOptions(props: Props): ReactElement {
@@ -81,6 +75,7 @@ export default function EditTransformerOptions(props: Props): ReactElement {
         <Button
           variant="outline"
           size="sm"
+          disabled={!transformer}
           onClick={() => setIsSheetOpen(true)}
           className="ml-auto hidden h-[36px] lg:flex"
         >
@@ -92,16 +87,10 @@ export default function EditTransformerOptions(props: Props): ReactElement {
           <div className="flex flex-row justify-between w-full">
             <div className="flex flex-col space-y-2">
               <div className="flex flex-row gap-2">
-                <SheetTitle>
-                  {handleTransformerMetadata(transformer?.value).name}
-                </SheetTitle>
-                <Badge variant="outline">
-                  {handleTransformerMetadata(transformer?.value).type}
-                </Badge>
+                <SheetTitle>{transformer?.name}</SheetTitle>
+                <Badge variant="outline">{transformer?.type}</Badge>
               </div>
-              <SheetDescription>
-                {handleTransformerMetadata(transformer?.value).description}
-              </SheetDescription>
+              <SheetDescription>{transformer?.description}</SheetDescription>
             </div>
             <Button variant="ghost" onClick={() => setIsSheetOpen(false)}>
               <Cross2Icon className="h-4 w-4" />
@@ -111,7 +100,7 @@ export default function EditTransformerOptions(props: Props): ReactElement {
         </SheetHeader>
         <div className="pt-8">
           {transformer &&
-            handleTransformerForm(transformer.value, index, setIsSheetOpen)}
+            handleTransformerForm(transformer, index, setIsSheetOpen)}
         </div>
       </SheetContent>
     </Sheet>
@@ -119,11 +108,11 @@ export default function EditTransformerOptions(props: Props): ReactElement {
 }
 
 function handleTransformerForm(
-  value: string,
+  t: CustomTransformer,
   index?: number,
   setIsSheetOpen?: (val: boolean) => void
 ): ReactElement {
-  switch (value) {
+  switch (t.source) {
     case 'email':
       return (
         <EmailTransformerForm index={index} setIsSheetOpen={setIsSheetOpen} />
@@ -196,186 +185,4 @@ function handleTransformerForm(
       <div>No transformer component found</div>;
   }
   return <div></div>;
-}
-
-export function handleTransformerMetadata(
-  value: string | undefined
-): TransformerMetadata {
-  const tEntries: Record<string, TransformerMetadata>[] = [
-    {
-      email: {
-        name: 'Email',
-        description: 'Anonymizes or generates a new email.',
-        type: 'string',
-      },
-    },
-    {
-      phone_number: {
-        name: 'Phone Number',
-        description:
-          'Anonymizes or generates a new phone number. The default format is <XXX-XXX-XXXX>.',
-        type: 'string',
-      },
-    },
-    {
-      int_phone_number: {
-        name: 'Int64 Phone Number',
-        description:
-          'Anonymizes or generates a new phone number of type int64 with a default length of 10.',
-        type: 'int64',
-      },
-    },
-    {
-      first_name: {
-        name: 'First Name',
-        description: 'Anonymizes or generates a new first name.',
-        type: 'string',
-      },
-    },
-    {
-      last_name: {
-        name: 'Last Name',
-        description: 'Anonymizes or generates a new last name.',
-        type: 'string',
-      },
-    },
-    {
-      full_name: {
-        name: 'Full Name',
-        description:
-          'Anonymizes or generates a new full name consisting of a first and last name.',
-        type: 'string',
-      },
-    },
-    {
-      uuid: {
-        name: 'UUID',
-        description: 'Generates a new UUIDv4 id.',
-        type: 'uuid',
-      },
-    },
-    {
-      passthrough: {
-        name: 'Passthrough',
-        description:
-          'Passes the input value through to the desination with no changes.',
-        type: 'passthrough',
-      },
-    },
-    {
-      null: {
-        name: 'Null',
-        description: 'Inserts a <null> string instead of the source value.',
-        type: 'null',
-      },
-    },
-    {
-      random_string: {
-        name: 'Random String',
-        description:
-          'Creates a randomly ordered alphanumeric string with a default length of 10 unless the String Length or Preserve Length parameters are defined.',
-        type: 'string',
-      },
-    },
-    {
-      random_bool: {
-        name: 'Random Bool',
-        description: 'Generates a boolean value at random.',
-        type: 'bool',
-      },
-    },
-    {
-      random_int: {
-        name: 'Random Integer',
-        description:
-          'Generates a random integer value with a default length of 4 unless the Integer Length or Preserve Length paramters are defined. .',
-        type: 'int64',
-      },
-    },
-    {
-      random_float: {
-        name: 'Random Float',
-        description:
-          'Generates a random float value with a default length of <XX.XXX>.',
-        type: 'float',
-      },
-    },
-    {
-      gender: {
-        name: 'Gender',
-        description:
-          'Randomly generates one of the following genders: female, male, undefined, nonbinary.',
-        type: 'string',
-      },
-    },
-    {
-      utc_timestamp: {
-        name: 'UTC Timestamp',
-        description: 'Randomly generates a UTC timestamp.',
-        type: 'time',
-      },
-    },
-    {
-      unix_timestamp: {
-        name: 'Unix Timestamp',
-        description: 'Randomly generates a Unix timestamp.',
-        type: 'int64',
-      },
-    },
-    {
-      street_address: {
-        name: 'Street Address',
-        description:
-          'Randomly generates a street address in the format: {street_num} {street_addresss} {street_descriptor}. For example, 123 Main Street.',
-        type: 'string',
-      },
-    },
-    {
-      city: {
-        name: 'City',
-        description:
-          'Randomly selects a city from a list of predefined US cities.',
-        type: 'string',
-      },
-    },
-    {
-      zipcode: {
-        name: 'Zip Code',
-        description:
-          'Randomly selects a zip code from a list of predefined US cities.',
-        type: 'string',
-      },
-    },
-    {
-      state: {
-        name: 'State',
-        description:
-          'Randomly selects a US state and returns the two-character state code.',
-        type: 'string',
-      },
-    },
-    {
-      full_address: {
-        name: 'Full Address',
-        description:
-          'Randomly generates a street address in the format: {street_num} {street_addresss} {street_descriptor} {city}, {state} {zipcode}. For example, 123 Main Street Boston, Massachusetts 02169. ',
-        type: 'string',
-      },
-    },
-  ];
-
-  const def = {
-    default: {
-      name: 'Undefined',
-      description: 'Undefined Transformer',
-      type: 'undefined',
-    },
-  };
-
-  if (!value) {
-    return def.default;
-  }
-  const res = tEntries.find((item) => item[value]);
-
-  return res ? res[value] : def.default;
 }
--- frontend/components/jobs/SchemaTable/column.tsx
+++ frontend/components/jobs/SchemaTable/column.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import EditTransformerOptions, {
-  handleTransformerMetadata,
-} from '@/app/transformers/EditTransformerOptions';
+import EditTransformerOptions from '@/app/transformers/EditTransformerOptions';
 import { Checkbox } from '@/components/ui/checkbox';
 
 import { Button } from '@/components/ui/button';
@@ -47,6 +45,11 @@ export function getColumns(
   props: GetColumnsProps
 ): ColumnDef<PlainMessage<DatabaseColumn>>[] {
   const { systemTransformers, customTransformers } = props;
+
+  const mergedTransformers = MergeSystemAndCustomTransformers(
+    systemTransformers,
+    customTransformers
+  );
 
   return [
     {
@@ -147,14 +150,13 @@ export function getColumns(
                   <FormControl>
                     <div className="flex flex-row space-x-2">
                       <TransformerSelect
-                        systemTransformers={systemTransformers}
-                        customTransformers={customTransformers}
+                        mergedTransformers={mergedTransformers}
                         value={field.value}
                         onSelect={field.onChange}
                       />
                       <EditTransformerOptions
-                        transformer={systemTransformers?.find(
-                          (item) => item.value == field.value
+                        transformer={mergedTransformers?.find(
+                          (item) => item.name == field.value
                         )}
                         index={row.index}
                       />
@@ -177,36 +179,14 @@ export function getColumns(
 }
 
 interface TransformersSelectProps {
-  systemTransformers: Transformer[];
-  customTransformers: CustomTransformer[];
+  mergedTransformers: CustomTransformer[];
   value: string;
   onSelect: (value: string) => void;
 }
 
 function TransformerSelect(props: TransformersSelectProps) {
-  const { systemTransformers, customTransformers, value, onSelect } = props;
+  const { mergedTransformers, value, onSelect } = props;
   const [open, setOpen] = useState(false);
-
-  let merged: CustomTransformer[] = [...customTransformers];
-
-  systemTransformers.map((st) => {
-    const cf = {
-      config: {
-        case: st.config?.config.case,
-        value: st.config?.config.value,
-      },
-    };
-
-    const newCt = new CustomTransformer({
-      name: handleTransformerMetadata(st.value).name,
-      description: handleTransformerMetadata(st.value).description,
-      type: handleTransformerMetadata(st.value).type,
-      source: st.value,
-      config: cf as TransformerConfig,
-    });
-
-    merged.push(newCt);
-  });
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -228,7 +208,7 @@ function TransformerSelect(props: TransformersSelectProps) {
           <CommandInput placeholder="Search transformers..." />
           <CommandEmpty>No transformers found.</CommandEmpty>
           <CommandGroup className="overflow-auto h-[400px]">
-            {merged.map((t, index) => (
+            {mergedTransformers.map((t, index) => (
               <CommandItem
                 key={`${t.name}-${index}`}
                 onSelect={() => {
@@ -251,4 +231,222 @@ function TransformerSelect(props: TransformersSelectProps) {
       </PopoverContent>
     </Popover>
   );
+}
+
+// merge system into custom and add in additional metadata fields for system transformers
+// to fit into the custom transformers interface
+export function MergeSystemAndCustomTransformers(
+  system: Transformer[],
+  custom: CustomTransformer[]
+): CustomTransformer[] {
+  let merged: CustomTransformer[] = [...custom];
+
+  system.map((st) => {
+    const cf = {
+      config: {
+        case: st.config?.config.case,
+        value: st.config?.config.value,
+      },
+    };
+
+    const newCt = new CustomTransformer({
+      name: handleTransformerMetadata(st.value).name,
+      description: handleTransformerMetadata(st.value).description,
+      type: handleTransformerMetadata(st.value).type,
+      source: st.value,
+      config: cf as TransformerConfig,
+    });
+
+    merged.push(newCt);
+  });
+
+  return merged;
+}
+
+interface TransformerMetadata {
+  name: string;
+  description: string;
+  type: string;
+}
+
+export function handleTransformerMetadata(
+  value: string | undefined
+): TransformerMetadata {
+  const tEntries: Record<string, TransformerMetadata>[] = [
+    {
+      email: {
+        name: 'Email',
+        description: 'Anonymizes or generates a new email.',
+        type: 'string',
+      },
+    },
+    {
+      phone_number: {
+        name: 'Phone Number',
+        description:
+          'Anonymizes or generates a new phone number. The default format is <XXX-XXX-XXXX>.',
+        type: 'string',
+      },
+    },
+    {
+      int_phone_number: {
+        name: 'Int64 Phone Number',
+        description:
+          'Anonymizes or generates a new phone number of type int64 with a default length of 10.',
+        type: 'int64',
+      },
+    },
+    {
+      first_name: {
+        name: 'First Name',
+        description: 'Anonymizes or generates a new first name.',
+        type: 'string',
+      },
+    },
+    {
+      last_name: {
+        name: 'Last Name',
+        description: 'Anonymizes or generates a new last name.',
+        type: 'string',
+      },
+    },
+    {
+      full_name: {
+        name: 'Full Name',
+        description:
+          'Anonymizes or generates a new full name consisting of a first and last name.',
+        type: 'string',
+      },
+    },
+    {
+      uuid: {
+        name: 'UUID',
+        description: 'Generates a new UUIDv4 id.',
+        type: 'uuid',
+      },
+    },
+    {
+      passthrough: {
+        name: 'Passthrough',
+        description:
+          'Passes the input value through to the desination with no changes.',
+        type: 'passthrough',
+      },
+    },
+    {
+      null: {
+        name: 'Null',
+        description: 'Inserts a <null> string instead of the source value.',
+        type: 'null',
+      },
+    },
+    {
+      random_string: {
+        name: 'Random String',
+        description:
+          'Creates a randomly ordered alphanumeric string with a default length of 10 unless the String Length or Preserve Length parameters are defined.',
+        type: 'string',
+      },
+    },
+    {
+      random_bool: {
+        name: 'Random Bool',
+        description: 'Generates a boolean value at random.',
+        type: 'bool',
+      },
+    },
+    {
+      random_int: {
+        name: 'Random Integer',
+        description:
+          'Generates a random integer value with a default length of 4 unless the Integer Length or Preserve Length paramters are defined. .',
+        type: 'int64',
+      },
+    },
+    {
+      random_float: {
+        name: 'Random Float',
+        description:
+          'Generates a random float value with a default length of <XX.XXX>.',
+        type: 'float',
+      },
+    },
+    {
+      gender: {
+        name: 'Gender',
+        description:
+          'Randomly generates one of the following genders: female, male, undefined, nonbinary.',
+        type: 'string',
+      },
+    },
+    {
+      utc_timestamp: {
+        name: 'UTC Timestamp',
+        description: 'Randomly generates a UTC timestamp.',
+        type: 'time',
+      },
+    },
+    {
+      unix_timestamp: {
+        name: 'Unix Timestamp',
+        description: 'Randomly generates a Unix timestamp.',
+        type: 'int64',
+      },
+    },
+    {
+      street_address: {
+        name: 'Street Address',
+        description:
+          'Randomly generates a street address in the format: {street_num} {street_addresss} {street_descriptor}. For example, 123 Main Street.',
+        type: 'string',
+      },
+    },
+    {
+      city: {
+        name: 'City',
+        description:
+          'Randomly selects a city from a list of predefined US cities.',
+        type: 'string',
+      },
+    },
+    {
+      zipcode: {
+        name: 'Zip Code',
+        description:
+          'Randomly selects a zip code from a list of predefined US cities.',
+        type: 'string',
+      },
+    },
+    {
+      state: {
+        name: 'State',
+        description:
+          'Randomly selects a US state and returns the two-character state code.',
+        type: 'string',
+      },
+    },
+    {
+      full_address: {
+        name: 'Full Address',
+        description:
+          'Randomly generates a street address in the format: {street_num} {street_addresss} {street_descriptor} {city}, {state} {zipcode}. For example, 123 Main Street Boston, Massachusetts 02169. ',
+        type: 'string',
+      },
+    },
+  ];
+
+  const def = {
+    default: {
+      name: 'Undefined',
+      description: 'Undefined Transformer',
+      type: 'undefined',
+    },
+  };
+
+  if (!value) {
+    return def.default;
+  }
+  const res = tEntries.find((item) => item[value]);
+
+  return res ? res[value] : def.default;
 }
