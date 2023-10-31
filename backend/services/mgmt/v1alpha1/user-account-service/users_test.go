fe23a873694e7fe71e77bf8e6d090d7bcad12a42
Alisha <alisha@nucleuscloud.com>
2023-10-30 19:12:49 -0700

    user tests working

--- backend/services/mgmt/v1alpha1/user-account-service/users_test.go
+++ backend/services/mgmt/v1alpha1/user-account-service/users_test.go
@@ -2,8 +2,6 @@ package v1alpha1_useraccountservice
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"testing"
 
 	"connectrpc.com/connect"
@@ -15,8 +13,6 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type tokenContextKey struct{}
-
 func Test_GetUser_Anonymous(t *testing.T) {
 	mockDbtx := nucleusdb.NewMockDBTX(t)
 	mockQuerier := db_queries.NewMockQuerier(t)
@@ -27,7 +23,6 @@ func Test_GetUser_Anonymous(t *testing.T) {
 	idUuid, _ := nucleusdb.ToUuid(userId)
 	user := db_queries.NeosyncApiUser{ID: idUuid}
 
-	// getAnonymousUser
 	mockQuerier.On("GetAnonymousUser", context.Background(), mock.Anything).Return(user, nil)
 
 	resp, err := service.GetUser(context.Background(), &connect.Request[mgmtv1alpha1.GetUserRequest]{})
@@ -45,19 +40,15 @@ func Test_GetUser(t *testing.T) {
 
 	userId := "d5e29f1f-b920-458c-8b86-f3a180e06d98"
 	idUuid, _ := nucleusdb.ToUuid(userId)
-	user := db_queries.NeosyncApiUser{ID: idUuid}
 
 	authProviderId := "test-provider"
 	data := &authjwt.TokenContextData{AuthUserId: authProviderId}
 	ctx := context.WithValue(context.Background(), authjwt.TokenContextKey{}, data)
-	// getUserAssociationByAuth0Id
-	mockQuerier.On("GetUserAssociationByAuth0Id", ctx, mock.Anything, authProviderId).Return(user, nil)
+	userAssociation := db_queries.NeosyncApiUserIdentityProviderAssociation{UserID: idUuid, Auth0ProviderID: authProviderId}
+	mockQuerier.On("GetUserAssociationByAuth0Id", ctx, mock.Anything, authProviderId).Return(userAssociation, nil)
 
 	resp, err := service.GetUser(ctx, &connect.Request[mgmtv1alpha1.GetUserRequest]{})
-	jsonF, _ := json.MarshalIndent(resp, "", " ")
-	fmt.Printf("\n\n  %s \n\n", string(jsonF))
 
-	fmt.Println(resp.Msg.GetUserId())
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
 	assert.Equal(t, userId, resp.Msg.GetUserId())
