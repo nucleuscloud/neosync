518f7289b476a2a84d9f451fa4b0de9581b009dc
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2023-10-31 12:24:11 -0700

    NEOS-261: update failing random string tests (#464)

--- worker/internal/benthos/transformers/random_string_test.go
+++ worker/internal/benthos/transformers/random_string_test.go
@@ -1,90 +1,57 @@
 package neosync_transformers
 
 import (
-	"unicode"
+	"testing"
+
+	"github.com/benthosdev/benthos/v4/public/bloblang"
+	"github.com/stretchr/testify/assert"
 )
 
-// func TestRandomStringPreserveLengthTrue(t *testing.T) {
+func TestRandomStringPreserveLengthTrue(t *testing.T) {
 
-// 	val := "hellothe"
-// 	expectedLength := 8
+	val := "hellothe"
+	expectedLength := 8
 
-// 	res, err := ProcessRandomString(val, true, 0)
+	res, err := ProcessRandomString(val, true, -1)
 
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, len(res), expectedLength, "The output string should be as long as the input string")
-// 	assert.True(t, isAllCapitalized(res), isAllCapitalized(val))
-
-// }
-
-// func TestRandomStringPreserveLengthFalse(t *testing.T) {
-
-// 	val := "hello"
-// 	expectedLength := 10
-
-// 	res, err := ProcessRandomString(val, false, 0)
-
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, len(res), expectedLength, "The output string should be as long as the input string")
-// 	assert.True(t, isAllLower(res), isAllLower(val))
-
-// }
-
-// func TestRandomStringPreserveLengthFalseStrLength(t *testing.T) {
-
-// 	val := "hello"
-// 	expectedLength := 14
-
-// 	res, err := ProcessRandomString(val, false, int64(expectedLength))
-
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, len(res), expectedLength, "The output string should be as long as the input string")
-// 	assert.True(t, isTitleCased(res), isTitleCased(val))
-
-// }
-
-// func TestRandomStringTransformer(t *testing.T) {
-// 	mapping := `root = this.randomstringtransformer(true, 6, "UPPER")`
-// 	ex, err := bloblang.Parse(mapping)
-// 	assert.NoError(t, err, "failed to parse the random string transformer")
-
-// 	testVal := "testte"
-
-// 	res, err := ex.Query(testVal)
-// 	assert.NoError(t, err)
-
-// 	assert.Equal(t, len(testVal), len(res.(string)), "Generated string must be the same length as the input string")
-// 	assert.IsType(t, res, testVal)
-// }
-
-func isTitleCased(s string) bool {
-	if s == "" {
-		// An empty string is not capitalized.
-		return false
-	}
-
-	// Compare the first character with its uppercase version
-	return rune(s[0]) == unicode.ToUpper(rune(s[0]))
+	assert.NoError(t, err)
+	assert.Equal(t, len(res), expectedLength, "The output string should be as long as the input string")
 }
 
-func isAllCapitalized(s string) bool {
-	for _, char := range s {
-		if unicode.IsDigit(char) {
-			// Skip digits (numbers)
-			continue
-		}
-		if !unicode.IsUpper(char) {
-			return false
-		}
-	}
-	return true
+func TestRandomStringPreserveLengthFalse(t *testing.T) {
+
+	val := "hello"
+	expectedLength := 10
+
+	res, err := ProcessRandomString(val, false, -1)
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(res), expectedLength, "The output string should be a default 10 characters long")
+
 }
 
-func isAllLower(s string) bool {
-	for _, char := range s {
-		if unicode.IsUpper(char) {
-			return false
-		}
-	}
-	return true
+func TestRandomStringPreserveLengthFalseStrLength(t *testing.T) {
+
+	val := "hello"
+	expectedLength := 14
+
+	res, err := ProcessRandomString(val, false, int64(expectedLength))
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(res), expectedLength, "The output string should be as long as the input string")
+
+}
+
+func TestRandomStringTransformer(t *testing.T) {
+	mapping := `root = this.randomstringtransformer(true, -1)`
+	ex, err := bloblang.Parse(mapping)
+	assert.NoError(t, err, "failed to parse the random string transformer")
+
+	testVal := "testte"
+
+	res, err := ex.Query(testVal)
+	assert.NoError(t, err)
+
+	assert.Equal(t, len(testVal), len(res.(string)), "Generated string must be the same length as the input string")
+	assert.IsType(t, res, testVal)
 }
