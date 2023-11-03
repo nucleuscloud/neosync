aa7b0b52447b7bf0a63b4d101eff31fc8960fe84
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-03 22:25:24 +0000

    fix prettier

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -55,8 +55,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/nucleuscloud/neosync/docs',
+          editUrl: 'https://github.com/nucleuscloud/neosync/docs',
         },
         blog: false,
         theme: {
