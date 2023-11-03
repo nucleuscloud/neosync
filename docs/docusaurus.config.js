98d0f7bb802efa2211d5601d1e22cd205369c67e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-03 23:32:07 +0000

    wip on docusaurus 3.0

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -92,8 +91,8 @@ const config = {
         copyright: `Copyright © Nucleus Cloud Corp ${new Date().getFullYear()}`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: themes.github,
+        darkTheme: themes.dracula,
       },
     }),
 };
