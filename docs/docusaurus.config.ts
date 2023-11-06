087e85ef8a2c2790b6cdb0de5847ed49f2c199a2
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-06 14:31:14 -0600

    fix edit url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -54,7 +54,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/nucleuscloud/neosync/docs',
+          editUrl: 'https://github.com/nucleuscloud/neosync/blob/main/docs',
         },
         blog: false,
         theme: {
