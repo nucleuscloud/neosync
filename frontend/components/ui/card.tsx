484e2c07ddb15edf67c00aea455e5e443dc158f5
Evis Drenova <evis@usenucleus.cloud>
2023-11-07 19:29:43 -0800

    connection card styling updates

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-xl border bg-card text-card-foreground shadow',
+      'rounded-xl border  border-gray-300 bg-card text-card-foreground hover:border-gray-400 hover:shadow-sm',
       className
     )}
     {...props}
