6322d75fd7d3a34b90f63ed96e2622ace3f08e74
Alisha <alisha@nucleuscloud.com>
2023-11-08 08:07:16 -0800

    rename to account invites

--- backend/sql/postgresql/schema/20231108001411_account-invites.up.sql
+++ backend/sql/postgresql/schema/20231108001411_account-invites.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS neosync_api.invites (
+CREATE TABLE IF NOT EXISTS neosync_api.account_invites (
 	id uuid NOT NULL DEFAULT gen_random_uuid(),
   account_id uuid NOT NULL,
   sender_user_id uuid,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS neosync_api.invites (
   CONSTRAINT fk_invites_accounts_id FOREIGN KEY (account_id) REFERENCES neosync_api.accounts(id) ON DELETE CASCADE,
   CONSTRAINT fk_invites_users_id FOREIGN KEY (id) REFERENCES neosync_api.users(id) ON DELETE SET NULL
 );
-ALTER TABLE neosync_api.invites OWNER TO neosync_api_owner;
-GRANT ALL ON TABLE neosync_api.invites TO neosync_api_owner;
-GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE neosync_api.invites TO neosync_api_readwrite;
-GRANT SELECT ON TABLE neosync_api.invites TO neosync_api_readonly;
+ALTER TABLE neosync_api.account_invites OWNER TO neosync_api_owner;
+GRANT ALL ON TABLE neosync_api.account_invites TO neosync_api_owner;
+GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE neosync_api.account_invites TO neosync_api_readwrite;
+GRANT SELECT ON TABLE neosync_api.account_invites TO neosync_api_readonly;
