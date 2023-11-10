5295f1e30932020f2584b195e6c93f8771de9e5c
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-10 19:17:33 +0000

    [migration] changes api key constraint to be global

---
+++ backend/sql/postgresql/schema/20231110191350_account-api-key-value-unique.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE neosync_api.account_api_keys
+DROP CONSTRAINT account_api_keys_account_id_key_value;
+
+ALTER TABLE neosync_api.account_api_keys
+ADD CONSTRAINT account_api_keys_key_value UNIQUE(key_value);
