8baaf22571e4f9ca8b47ed0db6aa7e031594b4bf
Evis Drenova <evis@usenucleus.cloud>
2023-11-13 15:13:46 -0800

    added image icon and styling

---
+++ docs/src/CustomComponents/DocsImage.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export function DocsImage(): ReactElement {
+  return (
+    <div className="docsImage">
+      <img src="https://assets.nucleuscloud.com/neosync/docs/transformers-page.png" />
+    </div>
+  );
+}
