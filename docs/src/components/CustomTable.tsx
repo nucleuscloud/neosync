9bf9dab75a850d84cf0db56607242c5792ee0334
Evis Drenova <evis@usenucleus.cloud>
2023-11-12 22:04:18 -0800

    email tranformer docs

--- docs/src/components/CustomTable.tsx
+++ docs/src/components/CustomTable.tsx
@@ -5,10 +5,10 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className="flex  overflow-auto">
     <table
       ref={ref}
-      className={cn('w-full caption-bottom text-sm', className)}
+      className={cn('caption-bottom text-sm', className)}
       {...props}
     />
   </div>
