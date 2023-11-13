edd44530b1dc9631b5a1a8b568d9d25178dd4dc4
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-13 16:32:42 +0000

    remove commented code from new down migration

--- backend/sql/postgresql/schema/20231113155315_machine-user.down.sql
+++ backend/sql/postgresql/schema/20231113155315_machine-user.down.sql
@@ -1,7 +1,5 @@
 ALTER TABLE neosync_api.users
 DROP COLUMN user_type;
 
--- ALTER TABLE neosync_api.account_api_keys
--- DROP CONSTRAINT IF EXISTS fk_account_api_keys_user_id_users_id;
 ALTER TABLE neosync_api.account_api_keys
 DROP COLUMN user_id;
