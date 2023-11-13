60d0460501f207eeba8276d86b00a89744f04f96
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-13 16:38:01 +0000

    make migrations more idempotent

--- backend/sql/postgresql/schema/20231113155315_machine-user.down.sql
+++ backend/sql/postgresql/schema/20231113155315_machine-user.down.sql
@@ -1,5 +1,5 @@
 ALTER TABLE neosync_api.users
-DROP COLUMN user_type;
+DROP COLUMN IF EXISTS user_type;
 
 ALTER TABLE neosync_api.account_api_keys
-DROP COLUMN user_id;
+DROP COLUMN IF EXISTS user_id;
