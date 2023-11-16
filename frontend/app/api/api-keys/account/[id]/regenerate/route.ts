031b6d7f61d3568f84a5cd5e751fcefe197ba12e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-16 16:44:23 +0000

    fix regen route build error

--- frontend/app/api/api-keys/account/[id]/regenerate/route.ts
+++ frontend/app/api/api-keys/account/[id]/regenerate/route.ts
@@ -1,12 +1,8 @@
 import { withNeosyncContext } from '@/api-only/neosync-context';
 import { RegenerateAccountApiKeyRequest } from '@/neosync-api-client/mgmt/v1alpha1/api_key_pb';
-import { RequestContext } from '@/shared';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function PUT(
-  req: NextRequest,
-  { params }: RequestContext
-): Promise<NextResponse> {
+export async function PUT(req: NextRequest): Promise<NextResponse> {
   return withNeosyncContext(async (ctx) => {
     const body = RegenerateAccountApiKeyRequest.fromJson(await req.json());
     return ctx.apikeyClient.regenerateAccountApiKey(body);
