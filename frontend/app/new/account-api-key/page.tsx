6020e3ded528cbc410bd4e2f09b56e58a1c75473
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-16 00:45:36 +0000

    adds cal wigit for expires at

---
+++ frontend/app/new/account-api-key/page.tsx
@@ -0,0 +1,19 @@
+import OverviewContainer from '@/components/containers/OverviewContainer';
+import PageHeader from '@/components/headers/PageHeader';
+import { ReactElement } from 'react';
+import NewApiKeyForm from './NewApiKeyForm';
+
+export default function NewAccountApiKey(): ReactElement {
+  return (
+    <OverviewContainer
+      Header={
+        <PageHeader
+          header="Create a new account API Key"
+          description="API Keys can be used to interact with Neosync programmatically"
+        />
+      }
+    >
+      <NewApiKeyForm />
+    </OverviewContainer>
+  );
+}
