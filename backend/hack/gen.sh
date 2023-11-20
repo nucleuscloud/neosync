a7907e72b916de8cb68f180faa4b4f7cbb52e86a
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-11-20 09:39:31 -0800

    makes backend gen more efficient

--- backend/hack/gen.sh
+++ backend/hack/gen.sh
@@ -1,14 +1,47 @@
+update_frontend_client() {
+  rm -rf ../frontend/neosync-api-client
+  mkdir -p ../frontend/neosync-api-client
+  mv gen/es/protos/** ../frontend/neosync-api-client
+  rm -rf gen/es
+}
+
 # `buf format -w` writes to each file, so causes tilt to loop. instead we instruct buf
 # to just output the changes in diff format and apply those.
 BUF_VERSION=$(cat BUF_VERSION)
 SQLC_VERSION=$(cat SQLC_VERSION)
-docker run --rm -i --volume "./:/workspace" --workdir "/workspace" "bufbuild/buf:${BUF_VERSION}" format -d | patch --quiet
-docker run --rm -i --volume "./:/workspace" --workdir "/workspace" "bufbuild/buf:${BUF_VERSION}" generate &
-docker run --rm -i --volume "./:/workspace" --workdir "/workspace" "bufbuild/buf:${BUF_VERSION}" generate --template ./buf-es.gen.yaml &
-docker run --rm -i --volume "./:/src" --workdir "/src" "sqlc/sqlc:${SQLC_VERSION}" generate &
+
+# buf fmt
+docker run --rm -i \
+  --volume "./protos:/workspace" \
+  --workdir "/workspace" \
+  "bufbuild/buf:${BUF_VERSION}" format -d | patch --quiet
+
+# buf generate
+docker run --rm -i \
+  --volume "./gen:/workspace/gen" \
+  --volume "./buf.work.yaml:/workspace/buf.work.yaml" \
+  --volume "./buf.gen.yaml:/workspace/buf.gen.yaml" \
+  --volume "./protos:/workspace/protos" \
+  --workdir "/workspace" \
+  "bufbuild/buf:${BUF_VERSION}" generate &
+docker run --rm -i \
+  --volume "./gen:/workspace/gen" \
+  --volume "./buf.work.yaml:/workspace/buf.work.yaml" \
+  --volume "./buf-es.gen.yaml:/workspace/buf.gen.yaml" \
+  --volume "./protos:/workspace/protos" \
+  --workdir "/workspace" \
+  "bufbuild/buf:${BUF_VERSION}" generate &
+
+# sqlc
+docker run --rm -i \
+  --volume "./gen:/workspace/gen" \
+  --volume "./sql:/workspace/sql" \
+  --volume "./sqlc.yaml:/workspace/sqlc.yaml" \
+  --workdir "/workspace" \
+  "sqlc/sqlc:${SQLC_VERSION}" generate &
 wait
 
-rm -rf ../frontend/neosync-api-client
-mkdir -p ../frontend/neosync-api-client
-mv gen/es/protos/** ../frontend/neosync-api-client
-rm -rf gen/es
+# docker run --rm -i --volume "./:/src" --workdir "/src" "vektra/mockery:${MOCKERY_VERSION}" &
+update_frontend_client &
+
+wait
