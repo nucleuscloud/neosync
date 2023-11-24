bf9ba36ef7594c162b9a6d4944783bacfeda4af4
Evis Drenova <evis@usenucleus.cloud>
2023-11-23 22:00:43 -0800

    updated generate rand int

---
+++ worker/internal/benthos/transformers/generate_int.go
@@ -0,0 +1,96 @@
+package transformers
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+
+	"github.com/benthosdev/benthos/v4/public/bloblang"
+	_ "github.com/benthosdev/benthos/v4/public/components/io"
+	transformer_utils "github.com/nucleuscloud/neosync/worker/internal/benthos/transformers/utils"
+)
+
+func init() {
+
+	spec := bloblang.NewPluginSpec().
+		Param(bloblang.NewInt64Param("int_length")).
+		Param(bloblang.NewStringParam("sign"))
+
+	err := bloblang.RegisterFunctionV2("generate_random_int", spec, func(args *bloblang.ParsedParams) (bloblang.Function, error) {
+
+		intLength, err := args.GetInt64("int_length")
+		if err != nil {
+			return nil, err
+		}
+
+		sign, err := args.GetString("sign")
+		if err != nil {
+			return nil, err
+		}
+
+		return func() (any, error) {
+			res, err := GenerateRandomInt(intLength, sign)
+			return res, err
+		}, nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+// Generates a random integer up to 18 digits in length
+// The sign param determines either a positive, negative or randomly assigned sign
+func GenerateRandomInt(intLength int64, sign string) (int64, error) {
+
+	var returnValue int64
+
+	if sign != "positive" && sign != "negative" && sign != "random" {
+		return 0, errors.New("sign can only be 'positive', 'negative', or 'random'")
+	}
+
+	if intLength <= 1 {
+		return 0, errors.New("the length of the integer cannot be zero or less than zero")
+	}
+
+	if intLength > 10 {
+		return 0, errors.New("the length of the integer cannot be greater than 18 digits")
+	}
+
+	// user didnt set the int length so we can randomly generate one of the default length four
+
+	// max length of intLength is 9 digits so no issues with casting an int64 to an int32
+	val, err := transformer_utils.GenerateRandomInt(int(intLength))
+	if err != nil {
+		return 0, fmt.Errorf("unable to generate a random string with length")
+	}
+
+	returnValue = int64(val)
+
+	if sign == "random" {
+
+		randInt := rand.Intn(2)
+		if randInt == 1 {
+			// return the positive value
+			return returnValue, nil
+		} else {
+			// return the negative value
+			return returnValue * -1, nil
+		}
+
+	} else if sign == "negative" {
+		return returnValue * -1, nil
+	} else {
+		return returnValue, nil
+	}
+
+}
+
+func IsNegative(val int64) bool {
+	if (val * -1) < 0 {
+		return false
+	} else {
+		return true
+	}
+}
---
+++ worker/internal/benthos/transformers/generate_int_test.go
@@ -0,0 +1,107 @@
+package transformers
+
+import (
+	"testing"
+
+	"github.com/benthosdev/benthos/v4/public/bloblang"
+	transformer_utils "github.com/nucleuscloud/neosync/worker/internal/benthos/transformers/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomIntDefualtLength(t *testing.T) {
+
+	val := int64(67543543)
+
+	_, err := GenerateRandomInt(val, "postive")
+
+	assert.Error(t, err, "The sign should be either positive, negative or random")
+
+}
+
+func TestGenerateRandomIntWrongSign(t *testing.T) {
+
+	val := int64(67543543)
+
+	_, err := GenerateRandomInt(val, "nosign")
+
+	assert.Error(t, err, "The sign should be either positive, negative or random")
+
+}
+
+func TestGenerateRandomIntNegativeLength(t *testing.T) {
+
+	val := int64(-1)
+
+	_, err := GenerateRandomInt(val, "positive")
+
+	assert.Error(t, err, "The integer length can't be less than 1")
+
+}
+
+func TestGenerateRandomIntLengthTooLong(t *testing.T) {
+
+	val := int64(5678976578965789)
+
+	_, err := GenerateRandomInt(val, "positive")
+
+	assert.Error(t, err, "The sign should be either positive, negative or random")
+
+}
+
+func TestRandomIntTransformerRandomSign(t *testing.T) {
+	mapping := `root = generate_random_int(6, "random")`
+	ex, err := bloblang.Parse(mapping)
+	assert.NoError(t, err, "failed to parse the random int transformer")
+
+	res, err := ex.Query(nil)
+	assert.NoError(t, err)
+
+	if res.(int64) < 0 {
+		assert.Equal(t, int64(6), transformer_utils.GetIntLength(res.(int64)*-1), "The expected value should be negative and 6 digits in length")
+	} else {
+		assert.Equal(t, int64(6), transformer_utils.GetIntLength(res.(int64)), "The expected value should be positive and 6 digits in length")
+	}
+
+	assert.IsType(t, res, int64(2), "The expected value should be an int64")
+}
+
+func TestGenerateRandomIntTransformerWithNoLength(t *testing.T) {
+
+	mapping := `root = generate_random_int(6,"negative")`
+	ex, err := bloblang.Parse(mapping)
+	assert.NoError(t, err, "failed to parse the random int transformer")
+
+	res, err := ex.Query(nil)
+	assert.NoError(t, err)
+
+	assert.Equal(t, int64(6), transformer_utils.GetIntLength(res.(int64)*-1))
+	assert.Equal(t, IsNegative(res.(int64)), true, "The value return should be negative")
+	assert.IsType(t, res, int64(2))
+}
+
+func Test_GenerateRandomIntTransformerWithLength(t *testing.T) {
+	mapping := `root = generate_random_int(5, "positive")`
+	ex, err := bloblang.Parse(mapping)
+	assert.NoError(t, err, "failed to parse the random int transformer")
+
+	res, err := ex.Query(nil)
+	assert.NoError(t, err)
+
+	assert.Equal(t, int64(5), transformer_utils.GetIntLength(res.(int64)), "The value should be the same length as the intLength")
+	assert.Equal(t, IsNegative(res.(int64)), false, "The value return should be positive")
+	assert.IsType(t, res, int64(2), "The value should be an int64")
+}
+
+func Test_isNegativeTrue(t *testing.T) {
+
+	val := IsNegative(-1)
+
+	assert.True(t, val, "The value should be negative")
+}
+
+func Test_isNegativeFalse(t *testing.T) {
+
+	val := IsNegative(1)
+
+	assert.False(t, val, "The value should be postive")
+}
