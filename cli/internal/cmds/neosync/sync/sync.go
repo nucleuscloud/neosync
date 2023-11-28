7707a2abce19899fd1ccef662d620866e77940fa
Alisha <alisha@nucleuscloud.com>
2023-11-28 12:49:52 -0500

    stash

---
+++ cli/internal/cmds/neosync/sync/sync.go
@@ -0,0 +1,53 @@
+package sync_cmd
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"connectrpc.com/connect"
+	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
+	"github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1/mgmtv1alpha1connect"
+	"github.com/nucleuscloud/neosync/cli/internal/auth"
+	auth_interceptor "github.com/nucleuscloud/neosync/cli/internal/connect/interceptors/auth"
+	"github.com/nucleuscloud/neosync/cli/internal/serverconfig"
+	"github.com/spf13/cobra"
+)
+
+func NewCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sync",
+		Short: "One off sync job to local resource",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			apiKey, err := cmd.Flags().GetString("api-key")
+			if err != nil {
+				return err
+			}
+			cmd.SilenceUsage = true
+			return sync(cmd.Context(), &apiKey)
+		},
+	}
+
+	return cmd
+}
+
+func sync(ctx context.Context, apiKey *string) error {
+	isAuthEnabled, err := auth.IsAuthEnabled(ctx)
+	if err != nil {
+		return err
+	}
+	userclient := mgmtv1alpha1connect.NewUserAccountServiceClient(
+		http.DefaultClient,
+		serverconfig.GetApiBaseUrl(),
+		connect.WithInterceptors(
+			auth_interceptor.NewInterceptor(isAuthEnabled, auth.AuthHeader, auth.GetAuthHeaderTokenFn(apiKey)),
+		),
+	)
+	resp, err := userclient.GetUser(ctx, connect.NewRequest(&mgmtv1alpha1.GetUserRequest{}))
+	if err != nil {
+		return err
+	}
+	// todo: layer in account data and access/id token information for even more goodness
+	fmt.Println("UserId:", resp.Msg.UserId) // nolint
+	return nil
+}
