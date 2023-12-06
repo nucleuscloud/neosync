45466b96f1935cf9b342c8375f661fce262c9e56
Evis Drenova <evis@usenucleus.cloud>
2023-12-05 17:13:52 -0800

    clean up

--- frontend/app/new/job/define/page.tsx
+++ frontend/app/new/job/define/page.tsx
@@ -65,7 +65,7 @@ export default function Page({ searchParams }: PageProps): ReactElement {
 
   const newJobType = getNewJobType(getSingleOrUndefined(searchParams?.jobType));
 
-  async function onSubmit(values: DefineFormValues) {
+  async function onSubmit(_values: DefineFormValues) {
     if (disableSchedule) {
       form.setValue('cronSchedule', '');
     }
