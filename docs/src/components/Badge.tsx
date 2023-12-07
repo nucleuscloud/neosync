5f78055bdb988d166e634cbd87f56868e47c6546
Evis Drenova <evis@usenucleus.cloud>
2023-12-07 15:31:25 -0800

    updated tables and table stylings

--- docs/src/components/Badge.tsx
+++ docs/src/components/Badge.tsx
@@ -14,7 +14,7 @@ const badgeVariants = cva(
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
-        outline: 'text-foreground bg-gray-100 h-[25px]',
+        outline: 'text-foreground bg-gray-100 h-[25px] dark:bg-[#1c1c1c]',
       },
     },
     defaultVariants: {
