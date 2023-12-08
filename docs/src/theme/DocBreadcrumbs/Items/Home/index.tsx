633203e608f089a144239f64001e7cfe8c3f8905
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-08 15:37:51 -0800

    revert home icon tyle

--- docs/src/theme/DocBreadcrumbs/Items/Home/index.tsx
+++ docs/src/theme/DocBreadcrumbs/Items/Home/index.tsx
@@ -1,9 +1,9 @@
 import Link from '@docusaurus/Link';
 import { translate } from '@docusaurus/Translate';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import IconHome from '@theme/Icon/Home';
 import React, { ReactElement } from 'react';
 
+import { HomeIcon } from '@radix-ui/react-icons';
 import styles from './styles.module.css';
 
 interface Props {}
@@ -11,17 +11,17 @@ export default function HomeBreadcrumbItem(props: Props): ReactElement {
   const homeHref = useBaseUrl('/');
 
   return (
-    <li className="breadcrumbs__item">
+    <li className="breadcrumbs__item breadcrumbs__home">
       <Link
         aria-label={translate({
           id: 'theme.docs.breadcrumbs.home',
           message: 'Home page',
           description: 'The ARIA label for the home page in the breadcrumbs',
         })}
-        className="breadcrumbs__link"
+        className="breadcrumbs__link pl-0"
         href={homeHref}
       >
-        <IconHome className={styles['home-icon']} />
+        <HomeIcon className={styles['home-icon']} />
       </Link>
     </li>
   );
