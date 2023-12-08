a41659a6023577ab7bf374e5a2afe382b7bd0ca2
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-08 12:35:30 -0800

    adds prebuild step

--- docs/protos/proto-sidebars.js
+++ docs/protos/proto-sidebars.js
@@ -1,2 +1,3 @@
 
-module.exports = { "protodocs": [{ "type": "category", "label": "Files", "items": [{ "type": "category", "label": "/mgmt/v1alpha1", "items": [{ "type": "doc", "id": "mgmt/v1alpha1/api_key.proto" }, { "type": "doc", "id": "mgmt/v1alpha1/auth.proto" }, { "type": "doc", "id": "mgmt/v1alpha1/connection.proto" }, { "type": "doc", "id": "mgmt/v1alpha1/transformer.proto" }, { "type": "doc", "id": "mgmt/v1alpha1/job.proto" }, { "type": "doc", "id": "mgmt/v1alpha1/user_account.proto" }] }] }] };
+module.exports = {"protodocs":[{"type":"category","label":"Files","items":[{"type":"category","label":"/mgmt/v1alpha1","items":[{"type":"doc","id":"mgmt/v1alpha1/api_key.proto"},{"type":"doc","id":"mgmt/v1alpha1/auth.proto"},{"type":"doc","id":"mgmt/v1alpha1/connection.proto"},{"type":"doc","id":"mgmt/v1alpha1/transformer.proto"},{"type":"doc","id":"mgmt/v1alpha1/job.proto"},{"type":"doc","id":"mgmt/v1alpha1/user_account.proto"}]}]}]};
+  
