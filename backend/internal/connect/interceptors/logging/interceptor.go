bbd30d179724e89fa21e6285bd3e37830d9487e0
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-08 10:05:49 -0800

    Adds basic logging interceptor to services

--- backend/internal/connect/interceptors/logging/interceptor.go
+++ backend/internal/connect/interceptors/logging/interceptor.go
@@ -2,7 +2,9 @@ package logging_interceptor
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"time"
 
 	"connectrpc.com/connect"
 )
@@ -17,7 +19,45 @@ func NewInterceptor(logger *slog.Logger) connect.Interceptor {
 
 func (i *Interceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
 	return func(ctx context.Context, request connect.AnyRequest) (connect.AnyResponse, error) {
-		return next(ctx, request)
+		now := time.Now()
+		i.logger.Info(
+			"started call",
+			"time_ms", fmt.Sprintf("%d", now.UnixMilli()),
+			"stream_type", request.Spec().StreamType.String(),
+			"procedure", request.Spec().Procedure,
+			"http_method", request.HTTPMethod(),
+			"peer_protocol", request.Peer().Protocol,
+		)
+		resp, err := next(ctx, request)
+		endNow := time.Now()
+		fields := []any{
+			"time_ms", fmt.Sprintf("%d", endNow.UnixMilli()),
+			"duration_ms", fmt.Sprintf("%d", endNow.Sub(now).Milliseconds()),
+			"stream_type", request.Spec().StreamType.String(),
+			"procedure", request.Spec().Procedure,
+			"http_method", request.HTTPMethod(),
+			"peer_protocol", request.Peer().Protocol,
+		}
+
+		if err != nil {
+			fields = append(fields, "error", fmt.Sprintf("%v", err))
+			connectErr, ok := err.(*connect.Error)
+			if ok {
+				fields = append(fields, "connect.code", connectErr.Code().String())
+			}
+
+			i.logger.Info(
+				"finished call",
+				fields...,
+			)
+			return nil, err
+		}
+		fields = append(fields, "connect.code", "ok")
+		i.logger.Info(
+			"finished call",
+			fields...,
+		)
+		return resp, nil
 	}
 }
 
@@ -29,6 +69,41 @@ func (i *Interceptor) WrapStreamingClient(next connect.StreamingClientFunc) conn
 
 func (i *Interceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
 	return func(ctx context.Context, conn connect.StreamingHandlerConn) error {
-		return next(ctx, conn)
+		now := time.Now()
+		i.logger.Info(
+			"started call",
+			"time_ms", fmt.Sprintf("%d", now.UnixMilli()),
+			"stream_type", conn.Spec().StreamType.String(),
+			"procedure", conn.Spec().Procedure,
+			"peer_protocol", conn.Peer().Protocol,
+		)
+		err := next(ctx, conn)
+		endNow := time.Now()
+		fields := []any{
+			"time_ms", fmt.Sprintf("%d", endNow.UnixMilli()),
+			"duration_ms", fmt.Sprintf("%d", endNow.Sub(now).Milliseconds()),
+			"stream_type", conn.Spec().StreamType.String(),
+			"procedure", conn.Spec().Procedure,
+			"peer_protocol", conn.Peer().Protocol,
+		}
+		if err != nil {
+			fields = append(fields, "error", fmt.Sprintf("%v", err))
+			connectErr, ok := err.(*connect.Error)
+			if ok {
+				fields = append(fields, "connect.code", connectErr.Code().String())
+			}
+
+			i.logger.Info(
+				"finished call",
+				fields...,
+			)
+			return err
+		}
+		fields = append(fields, "connect.code", "ok")
+		i.logger.Info(
+			"finished call",
+			fields...,
+		)
+		return nil
 	}
 }
