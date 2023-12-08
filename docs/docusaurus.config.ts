f0e3e3d34e5b615c0d512716179cf70ef47d6fbf
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2023-12-07 16:13:22 -0800

    NEOS-464: updated docs nav bar for dark mode (#805)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -68,8 +68,8 @@ const config: Config = {
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       defaultMode: 'light',
-      disableSwitch: true,
-      respectPrefersColorScheme: false,
+      // disableSwitch: false,
+      respectPrefersColorScheme: true,
     },
     navbar: {
       logo: {
