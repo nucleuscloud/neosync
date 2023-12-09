4b5fe137d8d362951c3d98e8d179f1bc4f2a27c3
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-09 12:46:11 -0800

    fix gen script

--- backend/hack/gen.sh
+++ backend/hack/gen.sh
@@ -1,5 +1,5 @@
 update_frontend_client() {
-  rm -rf ../sdk/ts-client/src/client/v1alpha1
+  rm -rf ../sdk/ts-client/src/client/mgmt
   mv gen/es/protos/** ../sdk/ts-client/src/client
   rm -rf gen/es
 }
