306f2637089fdb22ce6727be66a1a3d283c7d1a8
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-12 11:19:57 -0800

    moving back to npm

--- frontend/apps/web/next.config.mjs
+++ frontend/apps/web/next.config.mjs
@@ -1,4 +1,11 @@
+import path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export default {
   reactStrictMode: true,
   output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../')
+  }
 };
