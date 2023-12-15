55f10fe8572d85f7b0cf3d498153f8b4ed015262
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-15 22:39:45 +0000

    adds cmment

--- frontend/apps/web/app/api/config/config.ts
+++ frontend/apps/web/app/api/config/config.ts
@@ -1,5 +1,6 @@
 import { SystemAppConfig } from '@/app/config/app-config';
 
+// This will only be hydrated with env vars if invoked on the server
 export function getSystemAppConfig(): SystemAppConfig {
   return {
     isAuthEnabled: process.env.AUTH_ENABLED == 'true',
