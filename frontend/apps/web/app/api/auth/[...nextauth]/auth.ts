4c6a3caed581e1c8d73aaf05cccd2f8c3328a78b
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-15 22:59:56 +0000

    cleaning up types in auth method

--- frontend/apps/web/app/api/auth/[...nextauth]/auth.ts
+++ frontend/apps/web/app/api/auth/[...nextauth]/auth.ts
@@ -1,22 +1,57 @@
 import { type TokenSet } from '@auth/core/types';
 import { addSeconds, isAfter } from 'date-fns';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthConfig } from 'next-auth';
 
-function getAuth0Provider(): any {
-  return {
-    id: 'auth0',
-    name: 'Auth0',
-    type: 'oidc',
-    issuer: process.env.AUTH0_ISSUER,
-    clientId: process.env.AUTH0_CLIENT_ID ?? '',
-    clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
-    authorization: {
-      params: {
-        audience: process.env.AUTH0_AUDIENCE,
-        scope: process.env.AUTH0_SCOPE,
+function getProviders(): NextAuthConfig['providers'] {
+  const providers: NextAuthConfig['providers'] = [];
+
+  const auth0Config = getAuth0Config();
+  if (auth0Config) {
+    providers.push({
+      id: 'auth0',
+      name: 'Auth0',
+      type: 'oidc',
+      issuer: auth0Config.issuer,
+      clientId: auth0Config.clientId,
+      clientSecret: auth0Config.clientSecret,
+      authorization: {
+        params: {
+          audience: auth0Config.audience,
+          scope: auth0Config.scope,
+        },
       },
-    },
+      wellKnown: `${auth0Config.issuer}/.well-known/openid-configuration`,
+    });
+  }
+
+  return providers;
+}
+
+function getAuth0Config(): Auth0Config | null {
+  const issuer = process.env.AUTH0_ISSUER;
+  const clientId = process.env.AUTH0_CLIENT_ID;
+  const clientSecret = process.env.AUTH0_CLIENT_SECRET;
+  const audience = process.env.AUTH0_AUDIENCE;
+  const scope = process.env.AUTH0_SCOPE;
+
+  if (!issuer || !clientId || !clientSecret || !audience || !scope) {
+    return null;
+  }
+  return {
+    issuer,
+    clientId,
+    clientSecret,
+    audience,
+    scope,
   };
+}
+
+interface Auth0Config {
+  issuer: string;
+  clientId: string;
+  clientSecret: string;
+  audience: string;
+  scope: string;
 }
 
 export const {
@@ -28,29 +63,15 @@ export const {
   // server-side signOut. Use signOut from the next-auth/react library for client-side
   signOut,
 } = NextAuth({
-  providers: [
-    {
-      id: 'auth0',
-      name: 'Auth0',
-      type: 'oidc',
-      issuer: process.env.AUTH0_ISSUER,
-      clientId: process.env.AUTH0_CLIENT_ID ?? '',
-      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          audience: process.env.AUTH0_AUDIENCE,
-          scope: process.env.AUTH0_SCOPE,
-        },
-      },
-    },
-  ],
+  providers: getProviders(),
   session: { strategy: 'jwt' },
   callbacks: {
     session: async ({ session, token }) => {
       (session as any).accessToken = (token as any).accessToken;
       return session;
     },
-    jwt: async ({ token, account }) => {
+    jwt: async ({ token, account, profile }) => {
+      console.log('profile', profile);
       // Persist the OAuth access_token and or the user id to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
@@ -67,14 +88,14 @@ export const {
           // token can't be refreshed, fail
           throw new Error('session is expired, no refresh token available');
         }
-        const auth0Provider = getAuth0Provider();
+        const auth0Provider = getAuth0Config();
         const response = await fetch(
-          `${auth0Provider.issuer ?? ''}/oauth/token`,
+          `${auth0Provider?.issuer ?? ''}/oauth/token`,
           {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams({
-              client_id: auth0Provider.clientId ?? '',
-              client_secret: auth0Provider.clientSecret ?? '',
+              client_id: auth0Provider?.clientId ?? '',
+              client_secret: auth0Provider?.clientSecret ?? '',
               grant_type: 'refresh_token',
               refresh_token: (token as any).refreshToken,
             }),
@@ -108,3 +129,11 @@ declare module 'next-auth' {
     accessToken: string;
   }
 }
+
+// declare module '@auth/core/jwt' {
+//   export interface JWT {
+//     refreshToken?: string;
+//     expiresAt?: number;
+//     accessToken?: string;
+//   }
+// }
