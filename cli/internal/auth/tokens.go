0a7f67e635a3aab7ae92a42f92378980064bbd8e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-18 19:04:59 +0000

    fills out refresh logic in cli handler

--- cli/internal/auth/tokens.go
+++ cli/internal/auth/tokens.go
@@ -69,8 +69,24 @@ func getToken(ctx context.Context) (string, error) {
 			slog.Info("unable to find refresh token")
 			return "", err
 		}
-		_ = refreshtoken
-		// todo
+		refreshResp, err := authclient.RefreshCli(ctx, connect.NewRequest(&mgmtv1alpha1.RefreshCliRequest{
+			RefreshToken: refreshtoken,
+		}))
+		if err != nil {
+			slog.Info("unable to refresh token")
+			return "", err
+		}
+		err = userconfig.SetAccessToken(refreshResp.Msg.AccessToken.AccessToken)
+		if err != nil {
+			slog.Warn("unable to write refreshed access token back to user config", "error", err.Error())
+		}
+		if refreshResp.Msg.AccessToken.RefreshToken != nil {
+			err = userconfig.SetRefreshToken(*refreshResp.Msg.AccessToken.RefreshToken)
+			if err != nil {
+				slog.Warn("unable to write refreshed refresh token back to user config", "error", err.Error())
+			}
+		}
+		return refreshResp.Msg.AccessToken.AccessToken, nil
 	}
 	return accessToken, nil
 }
