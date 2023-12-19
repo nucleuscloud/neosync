cd75004d4ae5987ed9ef85d417c63d46c2babb76
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-19 03:42:13 +0000

    adds comment to layout

--- frontend/apps/web/app/invite/layout.tsx
+++ frontend/apps/web/app/invite/layout.tsx
@@ -6,5 +6,7 @@ export default async function InviteLayout({
 }: {
   children: ReactNode;
 }): Promise<ReactElement> {
+  // Server Signin is disabled for the invite page due to inability to access path or search params on the server
+  // Without this, the signin redirect url is set to the root url instead of /invite?token=<token>
   return <BaseLayout disableServerSignin>{children}</BaseLayout>;
 }
