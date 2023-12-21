bb90b56f221e09a831548b6d0d6f61f26139c90b
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2023-12-21 14:43:05 -0800

    NEOS-502: updated filter to filter by name instead of source (#950)

--- frontend/apps/web/components/jobs/SchemaTable/VirtualizedSchemaTable.tsx
+++ frontend/apps/web/components/jobs/SchemaTable/VirtualizedSchemaTable.tsx
@@ -254,7 +254,7 @@ const Row = memo(function Row({ data, index, style }: RowProps) {
                           transformers={transformers}
                           value={fv}
                           onSelect={field.onChange}
-                          placeholder="Select transformer..."
+                          placeholder="Select Transformer..."
                         />
                       </div>
                       <EditTransformerOptions
