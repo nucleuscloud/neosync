2d72d5243cddda83ee1881f031274613d9a74f8e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-12-22 12:41:41 -0800

    remove unused theme override

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,18 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './docs/*.mdx'],
-  theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: '2rem',
-        // screens: {
-        //   xl: '2000px',
-        //   '2xl': '1400px',
-        // },
-      },
-    },
-  },
+  theme: {},
   darkMode: ['class', '[data-theme="dark"]'],
   plugins: [],
 };
