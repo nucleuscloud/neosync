a9d271a7b901536b2690de9c63fa10a23ab41a4b
Evis Drenova <evis@usenucleus.cloud>
2024-01-03 20:48:21 -0800

    add pp

---
+++ marketing/app/privacy-policy/page.tsx
@@ -0,0 +1,234 @@
+'use client';
+
+export default function TOS() {
+  return (
+    <div className="flex justify-center mx-60 py-40 flex-col gap-4">
+      <h1>PRIVACY NOTICE</h1>
+      <p>Last updated 01/03/2024</p>
+      <p>
+        This privacy notice for Nucleus Cloud (&apos;we,&apos; &apos;us,&apos;
+        or &apos;our&apos;), describes how and why we might collect, store, use,
+        and/or share (&apos;process&apos;) your information when you use our
+        services (&apos;Services&apos;), such as when you: Questions or
+        concerns? Reading this privacy notice will help you understand your
+        privacy rights and choices. If you do not agree with our policies and
+        practices, please do not use our Services.
+      </p>
+      <h1>SUMMARY OF KEY POINTS</h1>
+      <p>
+        This summary provides key points from our privacy notice, but you can
+        find out more details about any of these topics by clicking the link
+        following each key point or by using our table of contents below to find
+        the section you are looking for. What personal information do we
+        process? When you visit, use, or navigate our Services, we may process
+        personal information depending on how you interact with us and the
+        Services, the choices you make, and the products and features you use.
+        Learn more about personal information you disclose to us. Do we process
+        any sensitive personal information? We do not process sensitive personal
+        information. Do we receive any information from third parties? We may
+        receive information from public databases, marketing partners, social
+        media platforms, and other outside sources. Learn more about information
+        collected from other sources. How do we process your information? We
+        process your information to provide, improve, and administer our
+        Services, communicate with you, for security and fraud prevention, and
+        to comply with law. We may also process your information for other
+        purposes with your consent. We process your information only when we
+        have a valid legal reason to do so. Learn more about how we process your
+        information. In what situations and with which parties do we share
+        personal information? We may share information in specific situations
+        and with specific third parties. Learn more about when and with whom we
+        share your personal information. What are your rights? Depending on
+        where you are located geographically, the applicable privacy law may
+        mean you have certain rights regarding your personal information. Learn
+        more about your privacy rights. How do you exercise your rights? The
+        easiest way to exercise your rights is by submitting a data subject
+        access request, or by contacting us. We will consider and act upon any
+        request in accordance with applicable data protection laws.
+      </p>
+      <h1>1. WHAT INFORMATION DO WE COLLECT?</h1>
+      <p>
+        Personal information you disclose to us In Short: We collect personal
+        information that you provide to us. We collect personal information that
+        you voluntarily provide to us when you registeron the Services, express
+        an interest in obtaining information about us or our productsand
+        Services, when you participate in activities on the Services, or
+        otherwise whenyou contact us. Sensitive Information. We do not process
+        sensitive information. All personal information that you provide to us
+        must be true, complete, and accurate,and you must notify us of any
+        changes to such personal information. Information automatically
+        collected In Short: Some information — such as your Internet Protocol
+        (IP) address and/orbrowser and device characteristics — is collected
+        automatically when you visit ourServices. We automatically collect
+        certain information when you visit, use, or navigate theServices. This
+        information does not reveal your specific identity (like your name
+        orcontact information) but may include device and usage information,
+        such as your IPaddress, browser and device characteristics, operating
+        system, languagepreferences, referring URLs, device name, country,
+        location, information about howand when you use our Services, and other
+        technical information. This information isprimarily needed to maintain
+        the security and operation of our Services, and for ourinternal
+        analytics and reporting purposes. Like many businesses, we also collect
+        information through cookies and similartechnologies.
+      </p>
+      <h1>2. HOW DO WE PROCESS YOUR INFORMATION? </h1>
+      <p>
+        In Short: We process your information to provide, improve, and
+        administer ourServices, communicate with you, for security and fraud
+        prevention, and to complywith law. We may also process your information
+        for other purposes with your consent. We process your personal
+        information for a variety of reasons, depending onhow you interact with
+        our Services, including:
+      </p>
+      <h1> 3. WHEN AND WITH WHOM DO WE SHARE YOURPERSONAL INFORMATION? </h1>
+      <p>
+        In Short: We may share information in specific situations described in
+        this sectionand/or with the following third parties. We may need to
+        share your personal information in the following situations: Business
+        Transfers. We may share or transfer your information in connectionwith,
+        or during negotiations of, any merger, sale of company assets,
+        financing,or acquisition of all or a portion of our business to another
+        company. Affiliates. We may share your information with our affiliates,
+        in which case wewill require those affiliates to honor this privacy
+        notice. Affiliates include ourparent company and any subsidiaries, joint
+        venture partners, or othercompanies that we control or that are under
+        common control with us. Business Partners. We may share your information
+        with our businesspartners to offer you certain products, services, or
+        promotions.
+      </p>
+      <h1>4. DO WE USE COOKIES AND OTHER TRACKINGTECHNOLOGIES?</h1>
+      <p>
+        In Short: We may use cookies and other tracking technologies to collect
+        and storeyour information. We may use cookies and similar tracking
+        technologies (like web beacons and pixels)to access or store
+        information. Specific information about how we use suchtechnologies and
+        how you can refuse certain cookies is set out in our Cookie Notice .
+      </p>
+      <h1> 5. HOW DO WE HANDLE YOUR SOCIAL LOGINS?</h1>
+      <p>
+        In Short: If you choose to register or log in to our Services using a
+        social mediaaccount, we may have access to certain information about
+        you. Our Services offer you the ability to register and log in using
+        your third-party socialmedia account details (like your Facebook or
+        Twitter logins). Where you choose to dothis, we will receive certain
+        profile information about you from your social mediaprovider. The
+        profile information we receive may vary depending on the social
+        mediaprovider concerned, but will often include your name, email
+        address, friends list, andprofile picture, as well as other information
+        you choose to make public on such asocial media platform. We will use
+        the information we receive only for the purposes that are described in
+        thisprivacy notice or that are otherwise made clear to you on the
+        relevant Services.Please note that we do not control, and are not
+        responsible for, other uses of yourpersonal information by your
+        third-party social media provider. We recommend thatyou review their
+        privacy notice to understand how they collect, use, and share
+        yourpersonal information, and how you can set your privacy preferences
+        on their sitesand apps.
+      </p>
+      <h1> 6. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</h1>{' '}
+      <p>
+        In Short: We may transfer, store, and process your information in
+        countries otherthan your own. Our servers are located in . If you are
+        accessing our Services from outside , please beaware that your
+        information may be transferred to, stored, and processed by us inour
+        facilities and by those third parties with whom we may share your
+        personalinformation (see &apos; WHEN AND WITH WHOM DO WE SHARE YOUR
+        PERSONALINFORMATION? &apos; above), in and other countries. If you are a
+        resident in the European Economic Area (EEA), United Kingdom (UK),
+        orSwitzerland, then these countries may not necessarily have data
+        protection laws orother similar laws as comprehensive as those in your
+        country. However, we will takeall necessary measures to protect your
+        personal information in accordance with thisprivacy notice and
+        applicable law.{' '}
+      </p>
+      <h1>7. HOW LONG DO WE KEEP YOUR INFORMATION? </h1>
+      <p>
+        In Short: We keep your information for as long as necessary to fulfill
+        the purposesoutlined in this privacy notice unless otherwise required by
+        law. We will only keep your personal information for as long as it is
+        necessary for thepurposes set out in this privacy notice, unless a
+        longer retention period is required orpermitted by law (such as tax,
+        accounting, or other legal requirements). When we have no ongoing
+        legitimate business need to process your personalinformation, we will
+        either delete or anonymize such information, or, if this is notpossible
+        (for example, because your personal information has been stored in
+        backuparchives), then we will securely store your personal information
+        and isolate it fromany further processing until deletion is possible.{' '}
+      </p>
+      <h1>8. DO WE COLLECT INFORMATION FROM MINORS? </h1>
+      <p>
+        In Short: We do not knowingly collect data from or market to children
+        under 18 yearsof age . We do not knowingly solicit data from or market
+        to children under 18 years of age. Byusing the Services, you represent
+        that you are at least 18 or that you are the parentor guardian of such a
+        minor and consent to such minor dependent&apos;s use of theServices. If
+        we learn that personal information from users less than 18 years of
+        agehas been collected, we will deactivate the account and take
+        reasonable measures topromptly delete such data from our records. If you
+        become aware of any data wemay have collected from children under age
+        18, please contact us at Nucleus Cloud .
+      </p>
+      <h1>9. WHAT ARE YOUR PRIVACY RIGHTS? </h1>
+      <p>
+        In Short: You may review, change, or terminate your account at any time.
+        Withdrawing your consent: If we are relying on your consent to process
+        yourpersonal information, which may be express and/or implied consent
+        depending onthe applicable law, you have the right to withdraw your
+        consent at any time. You canwithdraw your consent at any time by
+        contacting us by using the contact detailsprovided in the section &apos;
+        HOW CAN YOU CONTACT US ABOUT THIS NOTICE? &apos; below . However, please
+        note that this will not affect the lawfulness of the processing
+        beforeits withdrawal nor, when applicable law allows, will it affect the
+        processing of yourpersonal information conducted in reliance on lawful
+        processing grounds other thanconsent. Account Information If you would
+        at any time like to review or change the information in your account
+        orterminate your account, you can: Upon your request to terminate your
+        account, we will deactivate or delete youraccount and information from
+        our active databases. However, we may retain someinformation in our
+        files to prevent fraud, troubleshoot problems, assist with
+        anyinvestigations, enforce our legal terms and/or comply with applicable
+        legalrequirements.{' '}
+      </p>
+      <h1>10. CONTROLS FOR DO-NOT-TRACK FEATURES</h1>
+      <p>
+        Most web browsers and some mobile operating systems and mobile
+        applicationsinclude a Do-Not-Track ( &apos;DNT&apos; ) feature or
+        setting you can activate to signal yourprivacy preference not to have
+        data about your online browsing activities monitored and collected. At
+        this stage no uniform technology standard for recognizing
+        andimplementing DNT signals has been finalized . As such, we do not
+        currently respondto DNT browser signals or any other mechanism that
+        automatically communicatesyour choice not to be tracked online. If a
+        standard for online tracking is adopted thatwe must follow in the
+        future, we will inform you about that practice in a revisedversion of
+        this privacy notice.
+      </p>
+      <h1> 11. DO WE MAKE UPDATES TO THIS NOTICE? </h1>
+      <p>
+        In Short: Yes, we will update this notice as necessary to stay compliant
+        with relevantlaws. We may update this privacy notice from time to time.
+        The updated version will beindicated by an updated &apos;Revised&apos;
+        date and the updated version will be effective assoon as it is
+        accessible. If we make material changes to this privacy notice, we
+        maynotify you either by prominently posting a notice of such changes or
+        by directlysending you a notification. We encourage you to review this
+        privacy notice frequentlyto be informed of how we are protecting your
+        information.
+      </p>
+      <h1> 12. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</h1>
+      <p>
+        If you have questions or comments about this notice, you may contact us
+        by email at: support@nucleuscloud.com
+      </p>
+      <h1>
+        13. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
+      </h1>
+      <p>
+        Based on the applicable laws of your country, you may have the right to
+        requestaccess to the personal information we collect from you, change
+        that information, ordelete it. To request to review, update, or delete
+        your personal information, please fillout and submit a data subject
+        access request .
+      </p>
+    </div>
+  );
+}
