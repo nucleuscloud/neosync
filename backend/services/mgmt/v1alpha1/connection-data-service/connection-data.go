11836583821d125dcca722018fe7785a3a4cfae8
Alisha <alisha@nucleuscloud.com>
2024-01-05 13:54:39 -0800

    clean up

--- backend/services/mgmt/v1alpha1/connection-data-service/connection-data.go
+++ backend/services/mgmt/v1alpha1/connection-data-service/connection-data.go
@@ -21,7 +21,6 @@ import (
 	dbschemas "github.com/nucleuscloud/neosync/backend/pkg/dbschemas"
 	dbschemas_mysql "github.com/nucleuscloud/neosync/backend/pkg/dbschemas/mysql"
 	dbschemas_postgres "github.com/nucleuscloud/neosync/backend/pkg/dbschemas/postgres"
-	tabledeps "github.com/nucleuscloud/neosync/backend/pkg/table-dependency"
 )
 
 type DatabaseSchema struct {
@@ -616,11 +615,6 @@ func (s *Service) GetConnectionForeignConstraints(
 	default:
 		return nil, errors.New("unsupported fk connection")
 	}
-
-	tables := []string{"public.regions", "public.locations", "public.dependents", "public.employees", "public.departments", "public.countries", "public.jobs"}
-	// tables := []string{"public.employees"}
-	// tables := []string{"public.a", "public.b"}
-	tabledeps.GetSyncConfigs(td, tables)
 
 	tableConstraints := map[string]*mgmtv1alpha1.ForeignConstraintTables{}
 	for tableName, d := range td {
