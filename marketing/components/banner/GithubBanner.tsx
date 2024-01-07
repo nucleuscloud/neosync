c55e6522737f78d0b811ffceff809a4f4659d1ce
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2024-01-06 21:51:50 -0800

    NEOS-569: add referential integrity  blog post (#1048)

--- marketing/components/banner/GithubBanner.tsx
+++ marketing/components/banner/GithubBanner.tsx
@@ -10,13 +10,13 @@ import { Button } from '../ui/button';
 export default function GithubBanner() {
   const router = useRouter();
   return (
-    <div className=" top-0 flex flex-row gap-3 justify-center w-full h-[35px]-400 items-center bg-[#e4e9ec]">
+    <div className=" top-0 flex flex-row gap-3 justify-center w-full h-[35px]-400 items-center bg-[#e5edf6]">
       <div>
         If you like Neosync, give it a{' '}
         <StarFilledIcon className="text-yellow-500 inline h-[20px] w-[20px]" />{' '}
         on GitHub and follow us on Twitter
       </div>
-      <div className="flex flex-row gap-2">
+      <div className="flex flex-row gap-2 items-center">
         <Button
           onClick={() => router.push('https://github.com/nucleuscloud/neosync')}
           variant="ghost"
