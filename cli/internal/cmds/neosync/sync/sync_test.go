4bdb54e98e16c9b97238134d51ff51b770c5894d
Alisha <alisha@nucleuscloud.com>
2024-01-16 15:07:00 -0800

    aws s3 source circular dependency support

--- cli/internal/cmds/neosync/sync/sync_test.go
+++ cli/internal/cmds/neosync/sync/sync_test.go
@@ -4,99 +4,127 @@ import (
 	"math"
 	"testing"
 
-	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
+	tabledependency "github.com/nucleuscloud/neosync/backend/pkg/table-dependency"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_groupConfigsByDependency(t *testing.T) {
-	configs := []*benthosConfigResponse{
+	tests := []struct {
+		name    string
+		configs []*benthosConfigResponse
+		expect  [][]*benthosConfigResponse
+	}{
 		{
-			Name:      "ConfigA",
-			DependsOn: []string{},
+			name: "No dependencies",
+			configs: []*benthosConfigResponse{
+				{Name: "public.users", DependsOn: []*tabledependency.DependsOn{}, Table: "public.users", Columns: []string{"id", "email"}},
+				{Name: "public.accounts", DependsOn: []*tabledependency.DependsOn{}, Table: "public.accounts", Columns: []string{"id", "name"}},
+			},
+			expect: [][]*benthosConfigResponse{
+				{
+					{Name: "public.users", DependsOn: []*tabledependency.DependsOn{}, Table: "public.users", Columns: []string{"id", "email"}},
+					{Name: "public.accounts", DependsOn: []*tabledependency.DependsOn{}, Table: "public.accounts", Columns: []string{"id", "name"}},
+				},
+			},
 		},
 		{
-			Name:      "ConfigB",
-			DependsOn: []string{"ConfigA"},
+			name: "Multiple dependencies",
+			configs: []*benthosConfigResponse{
+				{Name: "public.users", DependsOn: []*tabledependency.DependsOn{}, Table: "public.users", Columns: []string{"id", "email"}},
+				{Name: "public.accounts", DependsOn: []*tabledependency.DependsOn{}, Table: "public.accounts", Columns: []string{"id", "name"}},
+				{Name: "public.jobs", DependsOn: []*tabledependency.DependsOn{{Table: "public.users", Columns: []string{"id"}}}, Table: "public.jobs", Columns: []string{"id", "user_id"}},
+				{Name: "public.regions", DependsOn: []*tabledependency.DependsOn{{Table: "public.accounts", Columns: []string{"id"}}}, Table: "public.regions", Columns: []string{"id", "account_id"}},
+				{Name: "public.tasks", DependsOn: []*tabledependency.DependsOn{{Table: "public.jobs", Columns: []string{"id"}}}, Table: "public.tasks", Columns: []string{"id", "job_id"}},
+			},
+			expect: [][]*benthosConfigResponse{
+				{
+					{Name: "public.users", DependsOn: []*tabledependency.DependsOn{}, Table: "public.users", Columns: []string{"id", "email"}},
+					{Name: "public.accounts", DependsOn: []*tabledependency.DependsOn{}, Table: "public.accounts", Columns: []string{"id", "name"}},
+				},
+				{
+					{Name: "public.jobs", DependsOn: []*tabledependency.DependsOn{{Table: "public.users", Columns: []string{"id"}}}, Table: "public.jobs", Columns: []string{"id", "user_id"}},
+					{Name: "public.regions", DependsOn: []*tabledependency.DependsOn{{Table: "public.accounts", Columns: []string{"id"}}}, Table: "public.regions", Columns: []string{"id", "account_id"}},
+				},
+				{
+					{Name: "public.tasks", DependsOn: []*tabledependency.DependsOn{{Table: "public.jobs", Columns: []string{"id"}}}, Table: "public.tasks", Columns: []string{"id", "job_id"}},
+				},
+			},
 		},
 		{
-			Name:      "ConfigC",
-			DependsOn: []string{"ConfigA"},
+			name: "Simple dependencies",
+			configs: []*benthosConfigResponse{
+				{Name: "public.users", DependsOn: []*tabledependency.DependsOn{}, Table: "public.users", Columns: []string{"id", "email"}},
+				{Name: "public.accounts", DependsOn: []*tabledependency.DependsOn{{Table: "public.users", Columns: []string{"id"}}}, Table: "public.accounts", Columns: []string{"id", "user_id"}},
+				{Name: "public.jobs", DependsOn: []*tabledependency.DependsOn{{Table: "public.accounts", Columns: []string{"id"}}}, Table: "public.jobs", Columns: []string{"id", "account_id"}},
+				{Name: "public.regions", DependsOn: []*tabledependency.DependsOn{{Table: "public.jobs", Columns: []string{"id"}}}, Table: "public.regions", Columns: []string{"id", "job_id"}},
+				{Name: "public.tasks", DependsOn: []*tabledependency.DependsOn{{Table: "public.regions", Columns: []string{"id"}}}, Table: "public.tasks", Columns: []string{"id", "region_id"}},
+			},
+			expect: [][]*benthosConfigResponse{
+				{
+					{Name: "public.users", DependsOn: []*tabledependency.DependsOn{}, Table: "public.users", Columns: []string{"id", "email"}},
+				},
+				{
+					{Name: "public.accounts", DependsOn: []*tabledependency.DependsOn{{Table: "public.users", Columns: []string{"id"}}}, Table: "public.accounts", Columns: []string{"id", "user_id"}},
+				},
+				{
+					{Name: "public.jobs", DependsOn: []*tabledependency.DependsOn{{Table: "public.accounts", Columns: []string{"id"}}}, Table: "public.jobs", Columns: []string{"id", "account_id"}},
+				},
+				{
+					{Name: "public.regions", DependsOn: []*tabledependency.DependsOn{{Table: "public.jobs", Columns: []string{"id"}}}, Table: "public.regions", Columns: []string{"id", "job_id"}},
+				},
+				{
+					{Name: "public.tasks", DependsOn: []*tabledependency.DependsOn{{Table: "public.regions", Columns: []string{"id"}}}, Table: "public.tasks", Columns: []string{"id", "region_id"}},
+				},
+			},
 		},
 		{
-			Name:      "ConfigD",
-			DependsOn: []string{"ConfigB", "ConfigC"},
+			name: "Circular dependencies",
+			configs: []*benthosConfigResponse{
+				{Name: "public.a", DependsOn: []*tabledependency.DependsOn{}, Table: "public.a", Columns: []string{"id"}},
+				{Name: "public.b", DependsOn: []*tabledependency.DependsOn{{Table: "public.c", Columns: []string{"id"}}}, Table: "public.b", Columns: []string{"id", "c_id"}},
+				{Name: "public.c", DependsOn: []*tabledependency.DependsOn{{Table: "public.a", Columns: []string{"id"}}}, Table: "public.c", Columns: []string{"id", "a_id"}},
+				{Name: "public.a.update", DependsOn: []*tabledependency.DependsOn{{Table: "public.b", Columns: []string{"id"}}}, Table: "public.a", Columns: []string{"b_id"}},
+			},
+			expect: [][]*benthosConfigResponse{
+				{
+					{Name: "public.a", DependsOn: []*tabledependency.DependsOn{}, Table: "public.a", Columns: []string{"id"}},
+				},
+				{
+					{Name: "public.c", DependsOn: []*tabledependency.DependsOn{{Table: "public.a", Columns: []string{"id"}}}, Table: "public.c", Columns: []string{"id", "a_id"}},
+				},
+				{
+					{Name: "public.b", DependsOn: []*tabledependency.DependsOn{{Table: "public.c", Columns: []string{"id"}}}, Table: "public.b", Columns: []string{"id", "c_id"}},
+				},
+				{
+					{Name: "public.a.update", DependsOn: []*tabledependency.DependsOn{{Table: "public.b", Columns: []string{"id"}}}, Table: "public.a", Columns: []string{"b_id"}},
+				},
+			},
 		},
 	}
 
-	expected := [][]*benthosConfigResponse{
-		{
-			{
-				Name:      "ConfigA",
-				DependsOn: []string{},
-			},
-		},
-		{
-			{
-				Name:      "ConfigB",
-				DependsOn: []string{"ConfigA"},
-			},
-			{
-				Name:      "ConfigC",
-				DependsOn: []string{"ConfigA"},
-			},
-		},
-		{
-			{
-				Name:      "ConfigD",
-				DependsOn: []string{"ConfigB", "ConfigC"},
-			},
-		},
-	}
-	groups := groupConfigsByDependency(configs)
-
-	for i, group := range groups {
-		assert.Equal(t, len(group), len(expected[i]))
-		for j, cfg := range group {
-			assert.Equal(t, cfg.Name, expected[i][j].Name)
-			assert.ElementsMatch(t, cfg.DependsOn, expected[i][j].DependsOn)
-		}
-
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			groups := groupConfigsByDependency(tt.configs)
+			assert.Len(t, groups, len(tt.expect))
+			for i, group := range groups {
+				assert.Equal(t, len(group), len(tt.expect[i]))
+				for j, cfg := range group {
+					assert.Equal(t, cfg.Name, tt.expect[i][j].Name)
+					assert.ElementsMatch(t, cfg.DependsOn, tt.expect[i][j].DependsOn)
+				}
+			}
+		})
 	}
 }
 
-func Test_groupConfigsByDependency_NoDependency(t *testing.T) {
+func Test_groupConfigsByDependency_Error(t *testing.T) {
 	configs := []*benthosConfigResponse{
-		{
-			Name:      "ConfigA",
-			DependsOn: []string{},
-		},
-		{
-			Name:      "ConfigB",
-			DependsOn: []string{},
-		},
-	}
-
-	expected := [][]*benthosConfigResponse{
-		{
-			{
-				Name:      "ConfigA",
-				DependsOn: []string{},
-			},
-			{
-				Name:      "ConfigB",
-				DependsOn: []string{},
-			},
-		},
+		{Name: "public.a", DependsOn: []*tabledependency.DependsOn{{Table: "public.b", Columns: []string{"id"}}}, Table: "public.a", Columns: []string{"id"}},
+		{Name: "public.b", DependsOn: []*tabledependency.DependsOn{{Table: "public.c", Columns: []string{"id"}}}, Table: "public.b", Columns: []string{"id", "c_id"}},
+		{Name: "public.c", DependsOn: []*tabledependency.DependsOn{{Table: "public.a", Columns: []string{"id"}}}, Table: "public.c", Columns: []string{"id", "a_id"}},
 	}
 	groups := groupConfigsByDependency(configs)
-
-	for i, group := range groups {
-		assert.Equal(t, len(group), len(expected[i]))
-		for j, cfg := range group {
-			assert.Equal(t, cfg.Name, expected[i][j].Name)
-			assert.ElementsMatch(t, cfg.DependsOn, expected[i][j].DependsOn)
-		}
-	}
+	assert.Nil(t, groups)
 }
 
 func Test_buildPlainInsertArgs(t *testing.T) {
@@ -124,48 +152,80 @@ func Test_computeMaxPgBatchCount(t *testing.T) {
 	assert.Equal(t, computeMaxPgBatchCount(0), 65535)
 }
 
-func Test_getSchemaTables(t *testing.T) {
+func Test_buildPostgresInsertQuery(t *testing.T) {
 	tests := []struct {
 		name     string
-		schemas  []*mgmtv1alpha1.DatabaseColumn
-		expected []*SqlTable
+		table    string
+		columns  []string
+		expected string
 	}{
-		{
-			name: "single table single column",
-			schemas: []*mgmtv1alpha1.DatabaseColumn{
-				{Schema: "schema1", Table: "table1", Column: "column1"},
-			},
-			expected: []*SqlTable{
-				{Schema: "schema1", Table: "table1", Columns: []string{"column1"}},
-			},
-		},
-		{
-			name: "single table multiple columns",
-			schemas: []*mgmtv1alpha1.DatabaseColumn{
-				{Schema: "schema1", Table: "table1", Column: "column1"},
-				{Schema: "schema1", Table: "table1", Column: "column2"},
-			},
-			expected: []*SqlTable{
-				{Schema: "schema1", Table: "table1", Columns: []string{"column1", "column2"}},
-			},
-		},
-		{
-			name: "multiple tables and columns",
-			schemas: []*mgmtv1alpha1.DatabaseColumn{
-				{Schema: "schema1", Table: "table1", Column: "column1"},
-				{Schema: "schema1", Table: "table2", Column: "column2"},
-				{Schema: "schema2", Table: "table1", Column: "column3"},
-			},
-			expected: []*SqlTable{
-				{Schema: "schema1", Table: "table1", Columns: []string{"column1"}},
-				{Schema: "schema1", Table: "table2", Columns: []string{"column2"}},
-				{Schema: "schema2", Table: "table1", Columns: []string{"column3"}},
-			},
-		},
+		{"Single Column", "users", []string{"name"}, "INSERT INTO users (name) VALUES ($1);"},
+		{"Multiple Columns", "users", []string{"name", "email"}, "INSERT INTO users (name, email) VALUES ($1, $2);"},
 	}
 
 	for _, tt := range tests {
-		result := getSchemaTables(tt.schemas)
-		assert.ElementsMatch(t, tt.expected, result)
+		t.Run(tt.name, func(t *testing.T) {
+			actual := buildPostgresInsertQuery(tt.table, tt.columns)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func Test_buildPostgresUpdateQuery(t *testing.T) {
+	tests := []struct {
+		name        string
+		table       string
+		columns     []string
+		primaryKeys []string
+		expected    string
+	}{
+		{"Single Column", "users", []string{"name"}, []string{"id"}, "UPDATE users SET name = $1 WHERE id = $2;"},
+		{"Multiple Primary Keys", "users", []string{"name", "email"}, []string{"id", "other"}, "UPDATE users SET name = $1, email = $2 WHERE id = $3 AND other = $4;"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := buildPostgresUpdateQuery(tt.table, tt.columns, tt.primaryKeys)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func Test_buildMysqlInsertQuery(t *testing.T) {
+	tests := []struct {
+		name     string
+		table    string
+		columns  []string
+		expected string
+	}{
+		{"Single Column", "users", []string{"name"}, "INSERT INTO users (name) VALUES (?);"},
+		{"Multiple Columns", "users", []string{"name", "email"}, "INSERT INTO users (name, email) VALUES (?, ?);"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := buildMysqlInsertQuery(tt.table, tt.columns)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func Test_buildMysqlUpdateQuery(t *testing.T) {
+	tests := []struct {
+		name        string
+		table       string
+		columns     []string
+		primaryKeys []string
+		expected    string
+	}{
+		{"Single Column", "users", []string{"name"}, []string{"id"}, "UPDATE users SET name = ? WHERE id = ?;"},
+		{"Multiple Primary Keys", "users", []string{"name", "email"}, []string{"id", "other"}, "UPDATE users SET name = ?, email = ? WHERE id = ? AND other = ?;"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := buildMysqlUpdateQuery(tt.table, tt.columns, tt.primaryKeys)
+			assert.Equal(t, tt.expected, actual)
+		})
 	}
 }
