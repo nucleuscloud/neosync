6b249af187793d8c664b27da9a6883261620c589
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-01-18 14:32:46 -0800

    removes comment

--- frontend/apps/web/app/api/accounts/[accountId]/connections/[id]/schema/route.ts
+++ frontend/apps/web/app/api/accounts/[accountId]/connections/[id]/schema/route.ts
@@ -1,17 +1,32 @@
 import { withNeosyncContext } from '@/api-only/neosync-context';
 import { RequestContext } from '@/shared';
-import { GetConnectionSchemaRequest } from '@neosync/sdk';
+import { DatabaseColumn, GetConnectionSchemaRequest } from '@neosync/sdk';
 import { NextRequest, NextResponse } from 'next/server';
+
+const fakeSchema: DatabaseColumn[] = [];
+for (let i = 0; i < 20000; i++) {
+  fakeSchema.push(
+    new DatabaseColumn({
+      schema: `testschema_${i}`,
+      table: `testtable_${i}`,
+      column: 'id',
+      dataType: 'string',
+    })
+  );
+}
 
 export async function GET(
   req: NextRequest,
   { params }: RequestContext
 ): Promise<NextResponse> {
   return withNeosyncContext(async (ctx) => {
-    return ctx.client.connectiondata.getConnectionSchema(
+    const resp = await ctx.client.connectiondata.getConnectionSchema(
       new GetConnectionSchemaRequest({
         connectionId: params.id,
       })
     );
+    resp.schemas.push(...fakeSchema);
+
+    return resp;
   })(req);
 }
