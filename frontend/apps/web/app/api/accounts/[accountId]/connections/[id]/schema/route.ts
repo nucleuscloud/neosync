431f7c7688202876c38d0a7639c50a43fcfc12c1
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-01-19 10:54:07 -0800

    roll back route fake data change

--- frontend/apps/web/app/api/accounts/[accountId]/connections/[id]/schema/route.ts
+++ frontend/apps/web/app/api/accounts/[accountId]/connections/[id]/schema/route.ts
@@ -1,31 +1,17 @@
 import { withNeosyncContext } from '@/api-only/neosync-context';
 import { RequestContext } from '@/shared';
-import { DatabaseColumn, GetConnectionSchemaRequest } from '@neosync/sdk';
+import { GetConnectionSchemaRequest } from '@neosync/sdk';
 import { NextRequest, NextResponse } from 'next/server';
-
-const fakeSchema: DatabaseColumn[] = [];
-for (let i = 0; i < 20000; i++) {
-  fakeSchema.push(
-    new DatabaseColumn({
-      schema: `testschema_${i}`,
-      table: `testtable_${i}`,
-      column: 'id',
-      dataType: 'string',
-    })
-  );
-}
 
 export async function GET(
   req: NextRequest,
   { params }: RequestContext
 ): Promise<NextResponse> {
   return withNeosyncContext(async (ctx) => {
-    const resp = await ctx.client.connectiondata.getConnectionSchema(
+    return ctx.client.connectiondata.getConnectionSchema(
       new GetConnectionSchemaRequest({
         connectionId: params.id,
       })
     );
-    resp.schemas.push(...fakeSchema);
-    return resp;
   })(req);
 }
