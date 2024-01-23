339bcf001cb8ee46cc37bbc8c4b351dfe27c00e7
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-01-23 11:31:49 -0800

    Adds worker api key type

--- backend/internal/apikey/apikey.go
+++ backend/internal/apikey/apikey.go
@@ -11,6 +11,7 @@ import (
 const (
 	prefix         = "neo"
 	accountTokenId = "at"
+	workerTokenId  = "wt"
 	v1             = "v1"
 	separator      = "_"
 
@@ -18,13 +19,20 @@ const (
 )
 
 var (
-	v1Prefix = strings.Join([]string{prefix, accountTokenId, v1}, separator)
+	v1AtPrefix = strings.Join([]string{prefix, accountTokenId, v1}, separator)
+	v1WtPrefix = strings.Join([]string{prefix, workerTokenId, v1}, separator)
 
 	v1AccountTokenPattern = fmt.Sprintf(
 		`^(%s)%s(%s)%sv([\d+])%s%s$`,
 		prefix, separator, accountTokenId, separator, separator, uuidPattern,
 	)
 	v1AccountTokenRegex = regexp.MustCompile(v1AccountTokenPattern)
+
+	v1WorkerTokenPattern = fmt.Sprintf(
+		`^(%s)%s(%s)%sv([\d+])%s%s$`,
+		prefix, separator, workerTokenId, separator, separator, uuidPattern,
+	)
+	v1WorkerTokenRegex = regexp.MustCompile(v1WorkerTokenPattern)
 )
 
 func NewV1AccountKey() string {
@@ -32,9 +40,21 @@ func NewV1AccountKey() string {
 }
 
 func v1AccountKey(suffix string) string {
-	return v1Prefix + separator + suffix
+	return v1AtPrefix + separator + suffix
 }
 
 func IsValidV1AccountKey(apikey string) bool {
 	return v1AccountTokenRegex.MatchString(apikey)
+}
+
+func NewV1WorkerKey() string {
+	return v1WorkerKey(uuid.NewString())
+}
+
+func v1WorkerKey(suffix string) string {
+	return v1WtPrefix + separator + suffix
+}
+
+func IsValidV1WorkerKey(apiKey string) bool {
+	return v1WorkerTokenRegex.MatchString(apiKey)
 }
