613f5175fa759accf4d3a2a4b6c6459a8665930f
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-01-26 11:41:31 -0800

    moves sqlconnect to pkg

--- backend/services/mgmt/v1alpha1/connection-service/service.go
+++ backend/services/mgmt/v1alpha1/connection-service/service.go
@@ -3,7 +3,7 @@ package v1alpha1_connectionservice
 import (
 	"github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1/mgmtv1alpha1connect"
 	"github.com/nucleuscloud/neosync/backend/internal/nucleusdb"
-	"github.com/nucleuscloud/neosync/backend/internal/sqlconnect"
+	"github.com/nucleuscloud/neosync/backend/pkg/sqlconnect"
 )
 
 type Service struct {
