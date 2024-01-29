8b001f687fb95a161101ff54e129f12801981893
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2024-01-28 20:21:07 -0800

    mobile updates for CTA (#1199)

--- marketing/app/page.tsx
+++ marketing/app/page.tsx
@@ -19,9 +19,9 @@ export default function Home(): ReactElement {
       <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto pt-20 lg:py-40">
         <UseNeosync />
       </div>
-      <div className="bg-[#1E1E1E] py-20 rounded-3xl relative">
+      <div className="bg-[#1E1E1E] py-6 lg:py-20 rounded-3xl relative">
         <div className=" px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto">
-          <div className="pt-20">
+          <div className="pt-4 lg:pt-20">
             <Platform />
           </div>
           <div className="pt-20 lg:pt-40">
@@ -29,7 +29,7 @@ export default function Home(): ReactElement {
           </div>
         </div>
       </div>
-      <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto pt-20 lg:py-40">
+      <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto py-20 lg:py-40">
         <CTA />
       </div>
     </div>
