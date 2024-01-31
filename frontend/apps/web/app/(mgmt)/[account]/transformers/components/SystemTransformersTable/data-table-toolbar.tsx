8fa6063d8c826a6daddcf1c86eaa767ba828849b
Evis Drenova <evis@usenucleus.cloud>
2024-01-30 22:07:48 -0800

    fix transformer table filtering

--- frontend/apps/web/app/(mgmt)/[account]/transformers/components/SystemTransformersTable/data-table-toolbar.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/transformers/components/SystemTransformersTable/data-table-toolbar.tsx
@@ -21,12 +21,12 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter Transformers..."
-          value={(table.getColumn('value')?.getFilterValue() as string) ?? ''}
+          placeholder="Filter System Transformers..."
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
           onChange={(event) =>
-            table.getColumn('value')?.setFilterValue(event.target.value)
+            table.getColumn('name')?.setFilterValue(event.target.value)
           }
-          className="h-8 w-[150px] lg:w-[250px]"
+          className="h-8 w-[150px] lg:w-[650px]"
         />
         {isFiltered && (
           <Button
