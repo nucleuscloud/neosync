5ae6afb741d0f07f54d4433e47e03098e8fed991
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-02 11:11:39 -0800

    adds update test

--- tf-provider/internal/provider/connection_resource_test.go
+++ tf-provider/internal/provider/connection_resource_test.go
@@ -16,7 +16,13 @@ func TestAcc_Connection(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("neosync_connection.test1", "id"),
 					resource.TestCheckResourceAttr("neosync_connection.test1", "name", "foo"),
-					// resource.TestCheckResourceAttr("data.neosync_connection.test", "name", "foosdf"),
+					resource.TestCheckResourceAttr("neosync_connection.test1", "postgres.url", "test-url"),
+				),
+			},
+			{
+				Config: testAccConnectionConfigUpdated,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("neosync_connection.test1", "postgres.url", "test-url2"),
 				),
 			},
 		},
@@ -29,6 +35,15 @@ resource "neosync_connection" "test1" {
 
 	postgres = {
 		url = "test-url"
+	}
+}
+`
+const testAccConnectionConfigUpdated = `
+resource "neosync_connection" "test1" {
+  name = "foo"
+
+	postgres = {
+		url = "test-url2"
 	}
 }
 `
