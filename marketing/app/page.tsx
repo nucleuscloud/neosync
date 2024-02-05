5755415b62335bc33ff56c269a8be2a2405b0262
Evis Drenova <evis@usenucleus.cloud>
2024-02-04 16:09:11 -0800

    fix margin

--- marketing/app/page.tsx
+++ marketing/app/page.tsx
@@ -12,13 +12,13 @@ export default function Home(): ReactElement {
       <div className="py-20 bg-[#FFFFFF] border-b border-b-gray-200">
         <Hero />
       </div>
-      <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto pt-20 lg:pt-40">
-        <ValueProps />
-      </div>
-      <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto pt-20 lg:py-40">
-        <UseNeosync />
-      </div>
       <div className=" bg-[#F5F5F5] lg:p-20 px-4">
+        <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto pt-20 lg:pt-40">
+          <ValueProps />
+        </div>
+        <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto pt-20 lg:py-40">
+          <UseNeosync />
+        </div>
         <div className=" bg-[#1E1E1E] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto rounded-3xl py-10">
           <div className="pt-4 lg:pt-20">
             <Platform />
@@ -27,9 +27,10 @@ export default function Home(): ReactElement {
             <Intergrations />
           </div>
         </div>
-      </div>
-      <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto py-10 lg:py-20">
-        <CTA />
+
+        <div className="bg-[#F5F5F5] px-5 sm:px-10 md:px-20 lg:px-40 max-w-[1800px] mx-auto py-10 lg:py-20">
+          <CTA />
+        </div>
       </div>
     </div>
   );
