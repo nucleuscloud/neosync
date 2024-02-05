ae91af1c5d98f1414d1794ac4af139e745ecdbb2
Evis Drenova <evis@usenucleus.cloud>
2024-02-04 20:35:27 -0800

    implement posthog

--- marketing/app/layout.tsx
+++ marketing/app/layout.tsx
@@ -51,21 +51,10 @@ export default function RootLayout({
           }}
         ></Script>
 
-        <Script type="text/javascript" id="livesession">
+        <Script type="text/javascript" id="posthog">
           {`
-window['__ls_namespace'] = '__ls';
-window['__ls_script_url'] = 'https://cdn.livesession.io/track.js';
-!function(w, d, t, u, n) {
-    if (n in w) {if(w.console && w.console.log) { w.console.log('LiveSession namespace conflict. Please set window["__ls_namespace"].');} return;}
-    if (w[n]) return; var f = w[n] = function() { f.push ? f.push.apply(f, arguments) : f.store.push(arguments)};
-    if (!w[n]) w[n] = f; f.store = []; f.v = "1.1";
-
-    var ls = d.createElement(t); ls.async = true; ls.src = u;
-    var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(ls, s);
-}(window, document, 'script', window['__ls_script_url'], window['__ls_namespace']);
-
-__ls("init", "${process.env.LIVESESSION}", { keystrokes: false });
-__ls("newPageView");
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+posthog.init('${process.env.NEXT_PUBLIC_POSTHOG_KEY}',{api_host:'https://app.posthog.com'})
 
 `}
         </Script>
