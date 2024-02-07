b10d37847579aeb64e7cff10288c2bafead6503f
Evis Drenova <evis@usenucleus.cloud>
2024-02-07 13:02:47 -0800

    update blog header

--- marketing/app/blog/[...slug]/page.tsx
+++ marketing/app/blog/[...slug]/page.tsx
@@ -148,7 +148,7 @@ export default async function PostPage({
             </time>
           </div>
         ) : null}
-        <div className="max-w-4xl">
+        <div className="max-w-2xl">
           <Mdx code={post.body.code} />
         </div>
         <hr className="mt-12" />
