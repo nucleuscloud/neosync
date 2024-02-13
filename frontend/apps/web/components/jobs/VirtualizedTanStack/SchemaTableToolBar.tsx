d98d9076015c67e4b0456ec12232bb1de37768c0
Evis Drenova <evis@usenucleus.cloud>
2024-02-13 12:38:27 -0800

    combined filters into one filter reset button

--- frontend/apps/web/components/jobs/VirtualizedTanStack/SchemaTableToolBar.tsx
+++ frontend/apps/web/components/jobs/VirtualizedTanStack/SchemaTableToolBar.tsx
@@ -31,50 +31,30 @@ export function SchemaTableToolbar<TData>({
     label: item,
   }));
 
-  const schemaFilter = table.getColumn('schema')?.getIsFiltered();
-
   const handleMultiSelectChange = (selectedOptions: Option[]) => {
     setSelectedOptions(selectedOptions);
     const filteredValues = selectedOptions.map((option) => option.value);
     table.getColumn('schema')?.setFilterValue(filteredValues);
   };
 
-  const resetFilters = () => {
-    setSelectedOptions([]);
-    table.resetColumnFilters();
-  };
-
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <div className="w-[150px] lg:w-[650px] z-50 flex flex-col">
-          <div className="flex flex-row items-center gap-2">
-            <MultiSelect
-              defaultOptions={schemaValues}
-              placeholder="Filter by schema(s)..."
-              emptyIndicator={
-                <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-                  No schemas available to filter
-                </p>
-              }
-              value={selectedOptions}
-              hidePlaceholderWhenSelected={true}
-              onChange={(selectedOptions) =>
-                handleMultiSelectChange(selectedOptions)
-              }
-            />
-            {schemaFilter && (
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={resetFilters}
-                className="h-8 px-2 mb-2 lg:px-3"
-              >
-                <Cross2Icon className="mr-2 h-3 w-3" />
-                Reset
-              </Button>
-            )}
-          </div>
+          <MultiSelect
+            defaultOptions={schemaValues}
+            placeholder="Filter by schema(s)..."
+            emptyIndicator={
+              <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                No schemas available to filter
+              </p>
+            }
+            value={selectedOptions}
+            hidePlaceholderWhenSelected={true}
+            onChange={(selectedOptions) =>
+              handleMultiSelectChange(selectedOptions)
+            }
+          />
           <div className="text-xs text-gray-600 dark:text-300">
             Total rows: ({new Intl.NumberFormat('en-US').format(data.length)})
             Rows visible: (
@@ -90,11 +70,14 @@ export function SchemaTableToolbar<TData>({
           <Button
             variant="outline"
             size="sm"
-            onClick={() => table.resetColumnFilters()}
+            onClick={() => {
+              setSelectedOptions([]);
+              table.resetColumnFilters();
+            }}
             className="h-8 px-2 lg:px-3"
           >
             <Cross2Icon className="mr-2 h-3 w-3" />
-            Reset Column Filters
+            Clear Filters
           </Button>
         )}
         <SchemaTableViewOptions table={table} />
