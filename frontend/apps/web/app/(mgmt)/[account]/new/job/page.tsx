f99e72c447dcceb6df202add815f3089d0a71894
Evis Drenova <evis@usenucleus.cloud>
2024-02-15 22:51:42 -0800

    updated the select job page

--- frontend/apps/web/app/(mgmt)/[account]/new/job/page.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/new/job/page.tsx
@@ -3,16 +3,17 @@ import OverviewContainer from '@/components/containers/OverviewContainer';
 import PageHeader from '@/components/headers/PageHeader';
 import { useAccount } from '@/components/providers/account-provider';
 import { PageProps } from '@/components/types';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { SymbolIcon } from '@radix-ui/react-icons';
 import { nanoid } from 'nanoid';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { ReactElement, useEffect, useState } from 'react';
 import { AiOutlineExperiment } from 'react-icons/ai';
 
@@ -47,6 +48,7 @@ export default function NewJob({ params }: PageProps): ReactElement {
         'Synchronize and anonymize data between a source and destination. ',
       href: `/${account?.name}/new/job/define?${dataSyncParams.toString()}`,
       icon: <SymbolIcon />,
+      type: 'data-sync',
     },
     {
       name: 'Data Generation',
@@ -54,56 +56,86 @@ export default function NewJob({ params }: PageProps): ReactElement {
         'Generate synthetic data from scratch for a chosen destination.',
       href: `/${account?.name}/new/job/define?${dataGenParams.toString()}`,
       icon: <AiOutlineExperiment />,
+      type: 'generate-table',
     },
   ] as const;
 
+  const [selectedJobType, setSelectedJobType] =
+    useState<NewJobType>('data-sync');
+
+  const [href, setHref] = useState<string>();
+
+  const handleJobSelection = (jobType: NewJobType, href: string) => {
+    setSelectedJobType(jobType);
+    setHref(href);
+  };
+
+  const router = useRouter();
+
   return (
-    <OverviewContainer
-      Header={
-        <PageHeader
-          header="Create a new job"
-          description="Select a job type to synchronize or generate data"
-          pageHeaderContainerClassName="mx-24"
-        />
-      }
+    <div
+      id="newjobdefine"
+      className="px-12 md:px-24 lg:px-96 flex flex-col pt-4 gap-16"
     >
-      <div className="gap-6 rounded-lg grid lg:grid-cols-2 xl:grid-cols-3 content-stretch mx-24">
-        {jobData.map((jd) => (
-          <JobCard
-            key={jd.name}
-            name={jd.name}
-            description={jd.description}
-            href={jd.href}
-            icon={jd.icon}
-          />
-        ))}
+      <OverviewContainer Header={<PageHeader header="Select a Job type" />}>
+        <div className="flex flex-col justify-center gap-6 pt-8">
+          <RadioGroup
+            value={selectedJobType}
+            onChange={() => setSelectedJobType}
+          >
+            {jobData.map((jd) => (
+              <div key={jd.name}>
+                <Card
+                  className={`cursor-pointer p-2 w-full min-w-[400px] ${selectedJobType === jd.type ? 'border border-black shadow-sm' : 'hover:border hover:border-gray-500'}`}
+                  onClick={() => handleJobSelection(jd.type, jd.href)}
+                >
+                  <CardHeader>
+                    <div className="flex flex-row justify-between items-center h-full">
+                      <div>
+                        <CardTitle>
+                          <div className="flex flex-row items-center gap-2 ">
+                            <div>{jd.icon}</div>
+                            <p>{jd.name}</p>
+                          </div>
+                        </CardTitle>
+                        <CardDescription className="pl-6">
+                          {jd.description}
+                        </CardDescription>
+                      </div>
+                      <RadioGroupItem
+                        value={jd.type}
+                        id={jd.type}
+                        className={`${selectedJobType === jd.type ? 'bg-black text-white' : 'bg-white text-black'}`}
+                      />
+                    </div>
+                  </CardHeader>
+                </Card>
+              </div>
+            ))}
+          </RadioGroup>
+        </div>
+      </OverviewContainer>
+      <div className="flex flex-row justify-between">
+        <Button
+          variant="outline"
+          type="reset"
+          onClick={() => router.push(`/${account?.name}`)}
+        >
+          Back
+        </Button>
+        <Button
+          type="submit"
+          disabled={!selectedJobType}
+          onClick={() =>
+            router.push(
+              href ??
+                `/${account?.name}/new/job/define?${dataSyncParams.toString()}`
+            )
+          }
+        >
+          Next
+        </Button>
       </div>
-    </OverviewContainer>
-  );
-}
-
-interface JobCardProps {
-  name: string;
-  description: string;
-  href: string;
-  icon: JSX.Element;
-}
-
-function JobCard(props: JobCardProps): ReactElement {
-  const { name, description, href, icon } = props;
-  return (
-    <Link href={href}>
-      <Card className="cursor-pointer hover:border hover:border-gray-500 min-h-[110px]">
-        <CardHeader>
-          <CardTitle>
-            <div className="flex flex-row items-center gap-2">
-              <div>{icon}</div>
-              <p>{name}</p>
-            </div>
-          </CardTitle>
-          <CardDescription className="pl-6">{description}</CardDescription>
-        </CardHeader>
-      </Card>
-    </Link>
+    </div>
   );
 }
