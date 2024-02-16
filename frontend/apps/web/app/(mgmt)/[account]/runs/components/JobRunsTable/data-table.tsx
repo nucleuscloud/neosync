c4d952a5c2623cccb8581db78a7f48012acd5adb
Evis Drenova <evis@usenucleus.cloud>
2024-02-16 10:54:08 -0800

    styling updates

--- frontend/apps/web/app/(mgmt)/[account]/runs/components/JobRunsTable/data-table.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/runs/components/JobRunsTable/data-table.tsx
@@ -97,7 +97,7 @@ export function DataTable<TData, TValue, TAutoRefreshInterval extends string>({
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id}>
+                    <TableHead key={header.id} className="pl-2">
                       {header.isPlaceholder
                         ? null
                         : flexRender(
--- frontend/apps/web/app/(mgmt)/[account]/settings/api-keys/components/ApiKeysTable/data-table.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/settings/api-keys/components/ApiKeysTable/data-table.tsx
@@ -79,7 +79,7 @@ export function DataTable<TData, TValue>({
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id}>
+                    <TableHead key={header.id} className="pl-2">
                       {header.isPlaceholder
                         ? null
                         : flexRender(
