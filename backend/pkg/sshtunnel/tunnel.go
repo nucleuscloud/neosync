0d6ac4abf407ba97c248c4f4f642148aa0d586e6
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-21 15:23:40 -0800

    updates shutdowns to be sync map

--- backend/pkg/sshtunnel/tunnel.go
+++ backend/pkg/sshtunnel/tunnel.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"sync"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/ssh"
@@ -23,6 +24,8 @@ type Sshtunnel struct {
 	maxConnectionAttempts uint
 	close                 chan any
 	isOpen                bool
+
+	shutdowns *sync.Map
 }
 
 func New(
@@ -40,7 +43,7 @@ func New(
 	}
 	return &Sshtunnel{
 		logger: logger,
-		close:  make(chan any),
+		close:  make(chan any, 1),
 
 		Local:  local,
 		Server: tunnel,
@@ -53,6 +56,8 @@ func New(
 			Auth:            authmethods,
 			HostKeyCallback: getHostKeyCallback(serverPublicKey),
 		},
+
+		shutdowns: &sync.Map{},
 	}
 }
 
@@ -84,22 +89,34 @@ func (t *Sshtunnel) Serve(listener net.Listener, ready chan<- any) {
 		}
 
 		c := make(chan net.Conn)
+
+		sessionId := uuid.NewString()
 		go newConnectionWaiter(listener, c, ready, hasSignaledReady, t.logger) // begins accepting connections and sends the connection onto the channel
 		hasSignaledReady = true
 		t.logger.Debug(fmt.Sprintf("listening for new local connections on %s", t.Local.String()))
-
+		shutdown := make(chan any, 1)
+		t.shutdowns.Store(sessionId, shutdown)
 		select {
 		case <-t.close:
 			t.logger.Debug("received close signal from client...")
 			t.isOpen = false
 			go func() {
+				t.shutdowns.Range(func(key, value any) bool {
+					sd, ok := value.(chan any)
+					if ok {
+						sd <- struct{}{}
+					} else {
+						t.logger.Warn(fmt.Sprintf("was unable to cast shutdown value to chan any. was %T", sd))
+					}
+					return true
+				})
 				if err := listener.Close(); err != nil {
 					t.logger.Error(err.Error())
 				}
 			}()
 		case conn := <-c:
 			t.logger.Debug(fmt.Sprintf("accepted connection local: %s, remote: %s", conn.LocalAddr().String(), conn.RemoteAddr().String()))
-			go t.forward(conn)
+			go t.forward(conn, sessionId, shutdown)
 		}
 	}
 
@@ -108,48 +125,32 @@ func (t *Sshtunnel) Serve(listener net.Listener, ready chan<- any) {
 
 // Takes the local connection, dials into the SSH server, connects to the remote host with that connection,
 // and then forwards the traffic from the local connection to the remote connection
-func (t *Sshtunnel) forward(localConnection net.Conn) {
-	var sshClient *ssh.Client
-	var err error
-	var attemptsLeft = t.maxConnectionAttempts
-
-	for {
-		sshClient, err = ssh.Dial("tcp", t.Server.String(), t.Config)
-		if err != nil {
-			attemptsLeft--
-
-			if attemptsLeft <= 0 {
-				t.logger.Error(fmt.Sprintf("server dial error: %v: exceeded %d attempts", err, t.maxConnectionAttempts))
-
-				if err := localConnection.Close(); err != nil {
-					t.logger.Error(fmt.Sprintf("failed to close local connection: %v", err))
-					return
-				}
-				t.logger.Debug("closed local connection")
-				return
-			}
-			t.logger.Warn(fmt.Sprintf("server dial error: %v: attempt %d/%d", err, t.maxConnectionAttempts-attemptsLeft, t.maxConnectionAttempts))
-		} else {
-			break
+func (t *Sshtunnel) forward(localConnection net.Conn, sessionId string, shutdown <-chan any) {
+	logger := t.logger.With("tunnelSessionId", sessionId)
+	sshClient, err := getSshClient(t.Server.String(), t.Config, t.maxConnectionAttempts, logger)
+	if err != nil {
+		if err := localConnection.Close(); err != nil {
+			logger.Error(fmt.Sprintf("failed to close local connection: %v", err))
+			return
 		}
+		logger.Error(fmt.Sprintf("unable to reach SSH server: %v", err))
+		return
 	}
 
-	t.logger.Debug(fmt.Sprintf("connected to %s (1 of 2)", t.Server.String()))
+	logger.Debug(fmt.Sprintf("connected to %s (1 of 2)", t.Server.String()))
 
 	remoteConnection, err := sshClient.Dial("tcp", t.Remote.String())
 	if err != nil {
-		t.logger.Error(fmt.Sprintf("remote dial error: %s", err))
+		logger.Error(fmt.Sprintf("remote dial error: %s", err))
 		if err := sshClient.Close(); err != nil {
-			t.logger.Error(fmt.Sprintf("failed to close server connection: %v", err))
+			logger.Error(fmt.Sprintf("failed to close server connection: %v", err))
 		}
 		if err := localConnection.Close(); err != nil {
-			t.logger.Error(fmt.Sprintf("failed to close local connection: %v", err))
+			logger.Error(fmt.Sprintf("failed to close local connection: %v", err))
 		}
 		return
 	}
-	t.logger.Debug(fmt.Sprintf("connected to %s (2 of 2)", t.Remote.String()))
-
-	done := make(chan error)
+	logger.Debug(fmt.Sprintf("connected to %s (2 of 2)", t.Remote.String()))
 
 	cleanup := func() {
 		// not really necessary since the connections are done
@@ -158,14 +159,46 @@ func (t *Sshtunnel) forward(localConnection net.Conn) {
 		remoteConnection.Close()
 		sshClient.Close()
 	}
+
+	// buffering so that we don't block the copyConnection when it sends its result
+	done := make(chan error, 2)
 	go func() {
-		<-done
-		close(done) // we only really care about one of the connections closing before we issue a cleanup.
+		select {
+		case <-shutdown:
+			logger.Debug("issued shutdown of tunnel")
+		case <-done:
+			t.shutdowns.Delete(sessionId)
+		}
 		cleanup()
-		t.logger.Debug("connection done, closed local, remote, and ssh connection")
+		logger.Debug("connection done, closed local, remote, and ssh connection")
 	}()
-	go copyConnection(localConnection, remoteConnection, done, t.logger.With("direction", "remote->local"))
-	go copyConnection(remoteConnection, localConnection, done, t.logger.With("direction", "local->remote"))
+	go copyConnection(localConnection, remoteConnection, done, logger.With("direction", "remote->local"))
+	go copyConnection(remoteConnection, localConnection, done, logger.With("direction", "local->remote"))
+}
+
+func getSshClient(
+	addr string,
+	config *ssh.ClientConfig,
+	maxAttempts uint,
+	logger *slog.Logger,
+) (*ssh.Client, error) {
+	var sshClient *ssh.Client
+	var err error
+	var attemptsLeft = maxAttempts
+	for {
+		sshClient, err = ssh.Dial("tcp", addr, config)
+		if err != nil {
+			attemptsLeft--
+			if attemptsLeft <= 0 {
+				logger.Error(fmt.Sprintf("server dial error: %v: exceeded %d attempts", err, maxAttempts))
+				return nil, err
+			}
+			logger.Warn(fmt.Sprintf("server dial error: %v: attempt %d/%d", err, maxAttempts-attemptsLeft, maxAttempts))
+		} else {
+			break
+		}
+	}
+	return sshClient, err
 }
 
 // Writer is what receives the input (dst), reader is what the input is read from (src)
@@ -179,11 +212,16 @@ func copyConnection(writer, reader net.Conn, done chan<- error, logger *slog.Log
 	done <- err
 }
 
-func newConnectionWaiter(listener net.Listener, c chan<- net.Conn, ready chan<- any, hasSignaledReady bool, logger *slog.Logger) {
+func newConnectionWaiter(
+	listener net.Listener,
+	c chan<- net.Conn,
+	ready chan<- any,
+	hasSignaledReady bool,
+	logger *slog.Logger,
+) {
 	go func() {
 		if !hasSignaledReady {
-			sessionId := uuid.NewString()
-			logger.Debug("notifying ready channel", "sessionId", sessionId)
+			logger.Debug("notifying ready channel")
 			ready <- struct{}{}
 		}
 	}()
