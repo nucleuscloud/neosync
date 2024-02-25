55a2874581e522dbc3722a2dbfcfeadacc9c7c6a
Evis Drenova <evis@usenucleus.cloud>
2024-02-24 22:51:44 -0800

    update spacing

--- marketing/components/cta/CTA.tsx
+++ marketing/components/cta/CTA.tsx
@@ -19,10 +19,10 @@ export default function CTA(): ReactElement {
     <div className=" bg-gradient-to-r from-slate-50 to-zinc-300 border border-gray-400 shadow-xl rounded-xl flex flex-col align-center space-y-6 py-10 justify-center">
       <div className="flex flex-row gap-4 items-center px-2 lg:px-10">
         <div className="flex flex-col gap-8 lg:pl-10">
-          <div className="text-gray-900 text-2xl lg:text-4xl font-satoshi font-bold md:text-center">
+          <div className="text-gray-900 text-2xl lg:text-4xl font-satoshi font-bold md:text-center lg:text-left">
             Start today with Neosync
           </div>
-          <div className="text-md text-gray-800 font-satoshi font-semibold md:px-10 lg:w-[80%] md:text-center">
+          <div className="text-md text-gray-800 font-satoshi font-semibold md:px-10 lg:px-0 lg:w-[80%] md:text-center lg:text-left">
             Get started with Neosync Cloud or easily deploy open source Neosync
             using a Helm chart or Docker Componse file.
           </div>
