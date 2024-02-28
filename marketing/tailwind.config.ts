5c9fe2d17d9130fb3ad60579b67fff94d35b92bd
Evis Drenova <evis@usenucleus.cloud>
2024-02-27 22:37:23 -0800

    minor styling updates

--- marketing/tailwind.config.ts
+++ marketing/tailwind.config.ts
@@ -101,11 +101,22 @@ module.exports = {
           '0%': { backgroundPosition: '0 0' }, // Equivalent to "from"
           '100%': { backgroundPosition: '-200% 0' }, // Equivalent to "to"
         },
+        spotlight: {
+          '0%': {
+            opacity: 0,
+            transform: 'translate(-72%, -62%) scale(0.5)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translate(-50%,-40%) scale(1)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         shimmer: 'shimmer 2s linear infinite',
+        spotlight: 'spotlight 2s ease .75s 1 forwards',
       },
     },
   },
