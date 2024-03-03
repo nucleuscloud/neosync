3d803a5f250acfe47d70a33a8fbe7a28d1e951a1
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-03-02 17:40:24 -0800

    fixes test

--- worker/pkg/workflows/datasync/activities/sync-activity-opts/activity_test.go
+++ worker/pkg/workflows/datasync/activities/sync-activity-opts/activity_test.go
@@ -1,6 +1,7 @@
 package syncactivityopts_activity
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -24,22 +25,22 @@ func Test_getSyncActivityOptionsFromJob(t *testing.T) {
 			SyncOptions: &mgmtv1alpha1.ActivityOptions{
 				StartToCloseTimeout: shared.Ptr(int64(2)),
 			},
-		}, expected: &workflow.ActivityOptions{StartToCloseTimeout: 2, RetryPolicy: defaultOpts.RetryPolicy}},
+		}, expected: &workflow.ActivityOptions{StartToCloseTimeout: 2, RetryPolicy: defaultOpts.RetryPolicy, HeartbeatTimeout: 1 * time.Minute}},
 		{name: "custom schedule to close timeout", input: &mgmtv1alpha1.Job{
 			SyncOptions: &mgmtv1alpha1.ActivityOptions{
 				ScheduleToCloseTimeout: shared.Ptr(int64(2)),
 			},
-		}, expected: &workflow.ActivityOptions{ScheduleToCloseTimeout: 2, RetryPolicy: defaultOpts.RetryPolicy}},
+		}, expected: &workflow.ActivityOptions{ScheduleToCloseTimeout: 2, RetryPolicy: defaultOpts.RetryPolicy, HeartbeatTimeout: 1 * time.Minute}},
 		{name: "custom retry policy", input: &mgmtv1alpha1.Job{
 			SyncOptions: &mgmtv1alpha1.ActivityOptions{
 				RetryPolicy: &mgmtv1alpha1.RetryPolicy{
 					MaximumAttempts: shared.Ptr(int32(2)),
 				},
 			},
-		}, expected: &workflow.ActivityOptions{StartToCloseTimeout: defaultOpts.StartToCloseTimeout, RetryPolicy: &temporal.RetryPolicy{MaximumAttempts: 2}}},
+		}, expected: &workflow.ActivityOptions{StartToCloseTimeout: defaultOpts.StartToCloseTimeout, RetryPolicy: &temporal.RetryPolicy{MaximumAttempts: 2}, HeartbeatTimeout: 1 * time.Minute}},
 	}
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%s_%s", t.Name(), test.name), func(t *testing.T) {
 			output := getSyncActivityOptionsFromJob(test.input)
 			assert.Equal(t, test.expected, output)
 		})
