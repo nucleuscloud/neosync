140b75eae44f055e8cd3a268a0e4af9b62781ef8
Evis Drenova <evis@usenucleus.cloud>
2024-03-05 11:47:16 -0800

    add product checklist tour

--- frontend/apps/web/app/BaseLayout.tsx
+++ frontend/apps/web/app/BaseLayout.tsx
@@ -4,6 +4,7 @@ import { PostHogIdentifier } from '@/components/providers/posthog-provider';
 import { SessionProvider } from '@/components/providers/session-provider';
 import SiteHeader from '@/components/site-header/SiteHeader';
 import { Toaster } from '@/components/ui/toaster';
+import { DoptProvider } from '@dopt/react';
 import { ReactElement, ReactNode, Suspense } from 'react';
 import { auth } from './api/auth/[...nextauth]/auth';
 
@@ -20,14 +21,20 @@ export default async function BaseLayout(props: Props): Promise<ReactElement> {
         <Suspense>
           <PostHogIdentifier />
         </Suspense>
-        <div className="relative flex min-h-screen flex-col">
-          <SiteHeader />
-          <div className="flex-1 container" id="top-level-layout">
-            {children}
+        <DoptProvider
+          userId="users-a611e66369fde320b693ad95843801e728b799e4a895bb54a810fc8d8317a502_MjE2Mw=="
+          apiKey="blocks-28312c460558d766219ae7e8fd7a565d46efc6a6e1d316e48c9e15d30384ab60_MjE2MQ=="
+          flowVersions={{ onboarding: 1 }}
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <div className="flex-1 container" id="top-level-layout">
+              {children}
+            </div>
+            <SiteFooter />
+            <Toaster />
           </div>
-          <SiteFooter />
-          <Toaster />
-        </div>
+        </DoptProvider>
       </AccountProvider>
     </SessionProvider>
   );
---
+++ frontend/apps/web/components/checklist/OnboardingChecklist.tsx
@@ -0,0 +1,50 @@
+import Checklist, { useChecklist } from '@dopt/react-checklist';
+
+export default function OnboardingChecklist() {
+  const checklist = useChecklist('newuserchecklist.three-bobcats-dress');
+
+  return (
+    <Checklist.Root>
+      <Checklist.Header>
+        <Checklist.Title>{checklist.title}</Checklist.Title>
+        <Checklist.DismissIcon onClick={checklist.dismiss} />
+      </Checklist.Header>
+      <Checklist.Body>{checklist.body}</Checklist.Body>
+      <Checklist.Progress
+        value={checklist.count('done')}
+        max={checklist.size}
+      />
+      <Checklist.Items>
+        {checklist.items.map((item, i) => (
+          <Checklist.Item key={i}>
+            <Checklist.ItemIcon>
+              {item.completed ? (
+                <Checklist.IconCompleted />
+              ) : item.skipped ? (
+                <Checklist.IconSkipped />
+              ) : (
+                <Checklist.IconActive />
+              )}
+            </Checklist.ItemIcon>
+            <Checklist.ItemContent>
+              <Checklist.ItemTitle disabled={item.done}>
+                {item.title}
+              </Checklist.ItemTitle>
+
+              <Checklist.ItemBody disabled={item.done}>
+                {item.body}
+              </Checklist.ItemBody>
+
+              {!item.done && (
+                <Checklist.ItemCompleteButton onClick={item.complete}>
+                  {item.completeLabel}
+                </Checklist.ItemCompleteButton>
+              )}
+            </Checklist.ItemContent>
+            {!item.done && <Checklist.ItemSkipIcon onClick={item.skip} />}
+          </Checklist.Item>
+        ))}
+      </Checklist.Items>
+    </Checklist.Root>
+  );
+}
