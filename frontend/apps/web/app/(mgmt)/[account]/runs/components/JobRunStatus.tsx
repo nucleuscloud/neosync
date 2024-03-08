f3985875c200d013d6f783d7a6d894364dfbe099
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2024-03-08 10:31:22 -0800

    NEOS-852: add icons to badges for job run statuses (#1527)

--- frontend/apps/web/app/(mgmt)/[account]/runs/components/JobRunStatus.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/runs/components/JobRunStatus.tsx
@@ -1,3 +1,4 @@
+import Spinner from '@/components/Spinner';
 import { Badge } from '@/components/ui/badge';
 import {
   Tooltip,
@@ -7,6 +8,12 @@ import {
 } from '@/components/ui/tooltip';
 import { cn } from '@/libs/utils';
 import { JobRunStatus as JobRunStatusEnum } from '@neosync/sdk';
+import {
+  CheckIcon,
+  ClockIcon,
+  Cross2Icon,
+  ExclamationTriangleIcon,
+} from '@radix-ui/react-icons';
 import { ReactElement } from 'react';
 
 interface Props {
@@ -20,45 +27,65 @@ export default function JobRunStatus(props: Props): ReactElement {
     case JobRunStatusEnum.ERROR:
       return (
         <Badge variant="destructive" className={cn(className)}>
-          Error
+          <div className="flex flex-row items-center gap-1">
+            <ExclamationTriangleIcon className="w-3 h-3" /> <div>Error</div>
+          </div>
         </Badge>
       );
     case JobRunStatusEnum.COMPLETE:
-      return <Badge variant="success">Complete</Badge>;
+      return (
+        <Badge variant="success">
+          <div className="flex flex-row items-center gap-1">
+            <CheckIcon className="w-3 h-3" /> <div>Complete</div>
+          </div>
+        </Badge>
+      );
     case JobRunStatusEnum.FAILED:
       return (
         <Badge variant="destructive" className={cn(className)}>
-          Failed
+          <div className="flex flex-row items-center gap-1">
+            <Cross2Icon className="w-3 h-3" /> <div>Failed</div>
+          </div>
         </Badge>
       );
     case JobRunStatusEnum.RUNNING:
       return (
         <Badge className={cn('bg-blue-600 hover:bg-blue-500', className)}>
-          Running
+          <div className="flex flex-row items-center gap-1">
+            <Spinner className="w-3 h-3" /> <div>Running</div>
+          </div>
         </Badge>
       );
     case JobRunStatusEnum.PENDING:
       return (
         <Badge className={cn('bg-purple-600  hover:bg-purple-500', className)}>
-          Pending
+          <div className="flex flex-row items-center gap-1">
+            <Spinner className="w-3 h-3" /> <div>Pending</div>
+          </div>
         </Badge>
       );
     case JobRunStatusEnum.TERMINATED:
       return (
         <Badge className={cn('bg-gray-600  hover:bg-gray-500', className)}>
-          Terminated
+          <div className="flex flex-row items-center gap-1">
+            <Cross2Icon className="w-3 h-3" /> <div>Failed</div>
+          </div>
         </Badge>
       );
     case JobRunStatusEnum.CANCELED:
       return (
         <Badge className={cn('bg-gray-600  hover:bg-gray-500', className)}>
-          Canceled
+          <div className="flex flex-row items-center gap-1">
+            <Cross2Icon className="w-3 h-3" /> <div>Failed</div>
+          </div>
         </Badge>
       );
     case JobRunStatusEnum.TIMED_OUT:
       return (
         <Badge className={cn('bg-yellow-600  hover:bg-yellow-500', className)}>
-          Timed Out
+          <div className="flex flex-row items-center gap-1">
+            <ClockIcon className="w-3 h-3" /> <div>Failed</div>
+          </div>
         </Badge>
       );
     default:
