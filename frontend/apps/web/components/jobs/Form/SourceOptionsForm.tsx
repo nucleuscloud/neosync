8905cf9d0c5ae1add34a96256f063b0bcaa0f7ad
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2024-03-19 09:59:01 -0700

    NEOS-888:remove switches (#1596)

--- frontend/apps/web/components/jobs/Form/SourceOptionsForm.tsx
+++ frontend/apps/web/components/jobs/Form/SourceOptionsForm.tsx
@@ -25,32 +25,28 @@ export default function SourceOptionsForm(
   switch (connection?.connectionConfig?.config?.case) {
     case 'pgConfig':
       const value = connection.connectionConfig.config.value;
-      switch (value.connectionConfig.case) {
-        case 'connection':
-          return (
-            <div className={`grid grid-cols-1 md:grid-cols-1 ${grid} gap-4`}>
-              <div>
-                <FormField
-                  name="sourceOptions.haltOnNewColumnAddition"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <SwitchCard
-                          isChecked={field.value || false}
-                          onCheckedChange={field.onChange}
-                          title="Halt Job on new column addition"
-                          description="Stops job runs if new column is detected"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-          );
-      }
-      return <></>;
+      return (
+        <div className={`grid grid-cols-1 md:grid-cols-1 ${grid} gap-4`}>
+          <div>
+            <FormField
+              name="sourceOptions.haltOnNewColumnAddition"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <SwitchCard
+                      isChecked={field.value || false}
+                      onCheckedChange={field.onChange}
+                      title="Halt Job on new column addition"
+                      description="Stops job runs if new column is detected"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+      );
     case 'mysqlConfig':
       const mysqlValue = connection.connectionConfig.config.value;
       switch (mysqlValue.connectionConfig.case) {
