d3bbca8bf9e32acbe659fbcfd8ce564822c76722
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-03-19 16:12:07 -0700

    undo comment

--- frontend/apps/web/app/(mgmt)/[account]/new/job/generate/single/schema/page.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/new/job/generate/single/schema/page.tsx
@@ -235,21 +235,21 @@ export default function Page({ searchParams }: PageProps): ReactElement {
   /* this fixes a bug that was happening due to the schema and table values not resetting if you went back and changed
 the connection which caused the schema page to not load correctly when you went back to the schema page. This checks if the selected connection contains the schema and
   */
-  // useEffect(() => {
-  //   const isValidSchemaAndTable =
-  //     form.getValues('schema') != '' && form.getValues('table') != '';
-  //   const tableInSchema = schemaTableMap
-  //     .get(form.getValues('schema'))
-  //     ?.find((item) => item == form.getValues('table'));
-  //   if (isValidSchemaAndTable && !tableInSchema) {
-  //     form.reset({
-  //       ...form.getValues(),
-  //       schema: '',
-  //       table: '',
-  //       mappings: [],
-  //     });
-  //   }
-  // }, [schemaTableMap, form]);
+  useEffect(() => {
+    const isValidSchemaAndTable =
+      form.getValues('schema') != '' && form.getValues('table') != '';
+    const tableInSchema = schemaTableMap
+      .get(form.getValues('schema'))
+      ?.find((item) => item == form.getValues('table'));
+    if (isValidSchemaAndTable && !tableInSchema) {
+      form.reset({
+        ...form.getValues(),
+        schema: '',
+        table: '',
+        mappings: [],
+      });
+    }
+  }, [schemaTableMap, form]);
 
   return (
     <div className="flex flex-col gap-5">
