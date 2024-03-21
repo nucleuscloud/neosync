1a388f749f6b86ab7ccd8e2757b47ea4a83aabfc
alishakawaguchi <alisha@nucleuscloud.com>
2024-03-21 16:09:37 -0700

    Benthos error processor test (#1624)

--- worker/internal/benthos/error/processor_error_test.go
+++ worker/internal/benthos/error/processor_error_test.go
@@ -1,1 +1,24 @@
 package neosync_benthos_error
+
+import (
+	"context"
+	"testing"
+
+	"github.com/benthosdev/benthos/v4/public/service"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ErrorProcessorEmptyShutdown(t *testing.T) {
+	errorProcessor := newErrorProcessor(service.MockResources().Logger(), nil)
+	require.NoError(t, errorProcessor.Close(context.Background()))
+}
+
+func Test_ErrorProcessorSendSignal(t *testing.T) {
+	stopWorkflowChan := make(chan bool, 1)
+	errorProcessor := newErrorProcessor(service.MockResources().Logger(), stopWorkflowChan)
+	ctx := context.Background()
+	_, err := errorProcessor.ProcessBatch(ctx, service.MessageBatch{})
+	out := <-stopWorkflowChan
+	require.True(t, out)
+	require.Error(t, err)
+}
