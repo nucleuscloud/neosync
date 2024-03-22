43013138fc10a1a615fbb7b978a0ae2ce2a7739b
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-03-22 09:57:12 -0700

    generate_last_name tests

--- worker/internal/benthos/transformers/generate_last_name.go
+++ worker/internal/benthos/transformers/generate_last_name.go
@@ -1,21 +1,37 @@
 package transformers
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/benthosdev/benthos/v4/public/bloblang"
+	transformers_dataset "github.com/nucleuscloud/neosync/worker/internal/benthos/transformers/data-sets"
 	transformer_utils "github.com/nucleuscloud/neosync/worker/internal/benthos/transformers/utils"
 )
 
 func init() {
-	spec := bloblang.NewPluginSpec().Param(bloblang.NewInt64Param("max_length"))
+	spec := bloblang.NewPluginSpec().
+		Param(bloblang.NewInt64Param("max_length").Default(10000)).
+		Param(bloblang.NewInt64Param("seed").Default(time.Now().UnixNano()))
 
 	err := bloblang.RegisterFunctionV2("generate_last_name", spec, func(args *bloblang.ParsedParams) (bloblang.Function, error) {
 		maxLength, err := args.GetInt64("max_length")
 		if err != nil {
 			return nil, err
 		}
+		seed, err := args.GetInt64("seed")
+		if err != nil {
+			return nil, err
+		}
+		randomizer := rand.New(rand.NewSource(seed))
 
 		return func() (any, error) {
-			return GenerateRandomLastName(maxLength)
+			output, err := generateRandomLastName(randomizer, maxLength)
+			if err != nil {
+				return nil, fmt.Errorf("unable to run generate_last_name")
+			}
+			return output, nil
 		}, nil
 	})
 
@@ -24,26 +40,12 @@ func init() {
 	}
 }
 
-/* Generates a random last name with a randomly selected length between [2,12] characters */
-func GenerateRandomLastName(maxLength int64) (string, error) {
-	if maxLength < 12 && maxLength >= 2 {
-		names := lastNames[maxLength]
-		res, err := transformer_utils.GetRandomValueFromSlice[string](names)
-		if err != nil {
-			return "", err
-		}
-		return res, nil
-	} else {
-		randInd, err := transformer_utils.GenerateRandomInt64InValueRange(2, 12)
-		if err != nil {
-			return "", err
-		}
-
-		names := lastNames[randInd]
-		res, err := transformer_utils.GetRandomValueFromSlice[string](names)
-		if err != nil {
-			return "", err
-		}
-		return res, nil
+func generateRandomLastName(randomizer *rand.Rand, maxLength int64) (string, error) {
+	candidates := transformer_utils.GetSmallerOrEqualNumbers(transformers_dataset.LastNameIndices, maxLength)
+	if len(candidates) == 0 {
+		return "", fmt.Errorf("unable to find last name smaller than requested max length: %d", maxLength)
 	}
+	randIdx := randomizer.Int63n(int64(len(candidates)))
+	lastNames := transformers_dataset.LastNameMap[candidates[randIdx]]
+	return lastNames[randomizer.Intn(len(lastNames))], nil
 }
--- worker/internal/benthos/transformers/generate_last_name_test.go
+++ worker/internal/benthos/transformers/generate_last_name_test.go
@@ -2,42 +2,35 @@ package transformers
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/benthosdev/benthos/v4/public/bloblang"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_GenerateRandomLastName(t *testing.T) {
-	res, err := GenerateRandomLastName(maxCharacterLimit)
+	randomizer := rand.New(rand.NewSource(1))
+	res, err := generateRandomLastName(randomizer, maxCharacterLimit)
 
 	assert.NoError(t, err)
-	assert.GreaterOrEqual(t, len(res), 2, "The last name should be more than 2 characters")
+	assert.NotEmpty(t, res)
 	assert.LessOrEqual(t, int64(len(res)), maxCharacterLimit, "The last name should be less than or equal to the max character limit")
-	assert.IsType(t, "", res, "The last name should be a string")
-}
-
-func Test_GenerateRandomLastNameMaxlengthLessThan12(t *testing.T) {
-	lowMaxCharLimit := int64(6)
-
-	res, err := GenerateRandomLastName(lowMaxCharLimit)
-
-	assert.NoError(t, err)
-	assert.Greater(t, len(res), 2, "The last name should be more than 2 characters")
-	assert.Equal(t, int64(len(res)), lowMaxCharLimit, "The last name should be less than or equal to the max character limit")
-	assert.IsType(t, "", res, "The last name should be a string")
 }
 
 func Test_GenerateRandomLastNameTransformer(t *testing.T) {
 	mapping := fmt.Sprintf(`root = generate_last_name(max_length:%d)`, maxCharacterLimit)
 	ex, err := bloblang.Parse(mapping)
 	assert.NoError(t, err, "failed to parse the last name transformer")
+	assert.NotNil(t, ex)
 
 	res, err := ex.Query(nil)
 	assert.NoError(t, err)
 
-	assert.NoError(t, err)
-	assert.GreaterOrEqual(t, len(res.(string)), 2, "The last name should be more than 0 characters")
-	assert.Less(t, len(res.(string)), 13, "The last name should be more than 0 characters")
-	assert.IsType(t, "", res, "The last name should be a string")
+	resStr, ok := res.(string)
+	require.True(t, ok)
+
+	assert.NotEmpty(t, resStr)
+	assert.LessOrEqual(t, int64(len(resStr)), maxCharacterLimit, "The last name should be less than or equal to max char limit")
 }
