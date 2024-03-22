c8b5eecd0ddf4885b32b1a7547abcb0d87afe23b
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-03-22 09:53:46 -0700

    updates to generate_first_name

--- worker/internal/benthos/transformers/generate_first_name_test.go
+++ worker/internal/benthos/transformers/generate_first_name_test.go
@@ -2,42 +2,36 @@ package transformers
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/benthosdev/benthos/v4/public/bloblang"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_GenerateRandomFirstName(t *testing.T) {
-	res, err := GenerateRandomFirstName(maxCharacterLimit)
+	randomizer := rand.New(rand.NewSource(1))
+	res, err := generateRandomFirstName(randomizer, maxCharacterLimit)
 
 	assert.NoError(t, err)
 	assert.GreaterOrEqual(t, len(res), 2, "The first name should be more than 2 characters")
 	assert.LessOrEqual(t, int64(len(res)), maxCharacterLimit, "The first name should be less than or equal to the max character limit")
-	assert.IsType(t, "", res, "The first name should be a string")
-}
-
-func Test_GenerateRandomFirstNameMaxlengthLessThan12(t *testing.T) {
-	lowMaxCharLimit := int64(6)
-
-	res, err := GenerateRandomFirstName(lowMaxCharLimit)
-
-	assert.NoError(t, err)
-	assert.Greater(t, len(res), 2, "The first name should be more than 2 characters")
-	assert.Equal(t, int64(len(res)), lowMaxCharLimit, "The first name should be less than or equal to the max character limit")
-	assert.IsType(t, "", res, "The first name should be a string")
+	assert.NotEmpty(t, res)
 }
 
 func Test_GenerateRandomFirstNameTransformer(t *testing.T) {
 	mapping := fmt.Sprintf(`root = generate_first_name(max_length:%d)`, maxCharacterLimit)
 	ex, err := bloblang.Parse(mapping)
 	assert.NoError(t, err, "failed to parse the first name transformer")
+	assert.NotNil(t, ex)
 
 	res, err := ex.Query(nil)
 	assert.NoError(t, err)
 
-	assert.NoError(t, err)
-	assert.GreaterOrEqual(t, len(res.(string)), 2, "The first name should be more than 0 characters")
-	assert.Less(t, len(res.(string)), 13, "The first name should be more than 0 characters")
-	assert.IsType(t, "", res, "The first name should be a string")
+	resStr, ok := res.(string)
+	require.True(t, ok)
+
+	assert.LessOrEqual(t, int64(len(resStr)), maxCharacterLimit, "output should be less than or equal to max char limit")
+	assert.NotEmpty(t, res)
 }
