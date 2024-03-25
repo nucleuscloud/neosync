a7203fc3af4b78f0a54daf66b7dea76cc464e2d7
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-03-24 17:24:38 -0700

    remove corpus struct for now

--- worker/internal/benthos/transformers/data-sets/generators.go
+++ worker/internal/benthos/transformers/data-sets/generators.go
@@ -3,22 +3,3 @@ package transformers_dataset
 //go:generate go run data_generator.go datasets/emaildomains.txt EmailDomain $GOPACKAGE
 //go:generate go run data_generator.go datasets/first_names.txt FirstName $GOPACKAGE
 //go:generate go run data_generator.go datasets/last_names.txt LastName $GOPACKAGE
-
-// type Corpus struct {
-// 	Values     []string
-// 	LengthMap  map[int64][2]int
-// 	MapIndices []int64
-// }
-
-// func (c *Corpus) GetValuesInRange(leftKey, rightKey int64) []string {
-// 	range1, ok := c.LengthMap[leftKey]
-// 	if !ok {
-// 		return []string{}
-// 	}
-// 	range2, ok := c.LengthMap[rightKey]
-// 	if !ok {
-// 		return []string{}
-// 	}
-// 	minIdx := range1[0]
-// 	maxIdx := range2[1]
-// }
