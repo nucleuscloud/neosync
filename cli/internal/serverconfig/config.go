961fd5cc79af765f03d8b1a7159a40e2114c747b
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-03-26 19:08:08 -0700

    updates CLI to use neosync prod at build time

--- cli/internal/serverconfig/config.go
+++ cli/internal/serverconfig/config.go
@@ -2,10 +2,12 @@ package serverconfig
 
 import "github.com/spf13/viper"
 
+var defaultBaseUrl string = "http://localhost:8080"
+
 func GetApiBaseUrl() string {
 	baseurl := viper.GetString("NEOSYNC_API_URL")
 	if baseurl == "" {
-		return "http://localhost:8080"
+		return defaultBaseUrl
 	}
 	return baseurl
 }
