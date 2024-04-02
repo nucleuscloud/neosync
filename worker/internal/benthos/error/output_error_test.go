627dd8e5310482243b31556aba63c869d00b55aa
Alisha <alisha@nucleuscloud.com>
2024-04-02 14:28:27 -0700

    add more tests

---
+++ worker/internal/benthos/error/output_error_test.go
@@ -0,0 +1,69 @@
+package neosync_benthos_error
+
+import (
+	"context"
+	"testing"
+
+	"github.com/benthosdev/benthos/v4/public/service"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ErrorOutputEmptyShutdown(t *testing.T) {
+	conf := `
+error_msg: "test error"
+`
+	spec := errorOutputSpec()
+	env := service.NewEnvironment()
+
+	config, err := spec.ParseYAML(conf, env)
+	require.NoError(t, err)
+	errorOutput, err := newErrorOutput(config, service.MockResources(), nil)
+	require.NoError(t, err)
+	require.NoError(t, errorOutput.Close(context.Background()))
+}
+
+func Test_ErrorOutputSendSignal(t *testing.T) {
+	conf := `
+error_msg: "${! meta(\"key\") }"
+`
+	spec := errorOutputSpec()
+	env := service.NewEnvironment()
+
+	config, err := spec.ParseYAML(conf, env)
+	require.NoError(t, err)
+	stopActivityChan := make(chan error, 1)
+	errorOutput, err := newErrorOutput(config, service.MockResources(), stopActivityChan)
+	require.NoError(t, err)
+	msg := service.NewMessage([]byte("content"))
+	msg.MetaSet("key", "some error")
+
+	batch := service.MessageBatch{msg}
+
+	ctx := context.Background()
+	err = errorOutput.WriteBatch(ctx, batch)
+	require.NoError(t, err)
+	out := <-stopActivityChan
+	require.Error(t, out)
+}
+
+func Test_ErrorOutputMaxConnError(t *testing.T) {
+	conf := `
+error_msg: "${! meta(\"key\") }"
+`
+	spec := errorOutputSpec()
+	env := service.NewEnvironment()
+
+	config, err := spec.ParseYAML(conf, env)
+	require.NoError(t, err)
+	stopActivityChan := make(chan error, 1)
+	errorOutput, err := newErrorOutput(config, service.MockResources(), stopActivityChan)
+	require.NoError(t, err)
+	msg := service.NewMessage([]byte("content"))
+	msg.MetaSet("key", "too many clients already")
+
+	batch := service.MessageBatch{msg}
+
+	ctx := context.Background()
+	err = errorOutput.WriteBatch(ctx, batch)
+	require.Error(t, err)
+}
--- worker/internal/benthos/error/processor_error_test.go
+++ worker/internal/benthos/error/processor_error_test.go
@@ -2,6 +2,7 @@ package neosync_benthos_error
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/benthosdev/benthos/v4/public/service"
@@ -18,7 +19,7 @@ func Test_ErrorProcessorSendSignal(t *testing.T) {
 	errorProcessor := newErrorProcessor(service.MockResources().Logger(), stopActivityChan)
 	ctx := context.Background()
 	_, err := errorProcessor.ProcessBatch(ctx, service.MessageBatch{})
-	out := <-stopActivityChan
-	require.Error(t, out)
 	require.NoError(t, err)
+	out := <-stopActivityChan
+	require.Equal(t, out, fmt.Errorf("Processor Error"))
 }
