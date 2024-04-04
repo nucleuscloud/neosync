645cfc2e1e4bdb437dd5017857388078720e5583
Alisha <alisha@nucleuscloud.com>
2024-04-04 13:19:37 -0700

    uncomment test

--- worker/pkg/workflows/datasync/activities/sync/pool-provider_test.go
+++ worker/pkg/workflows/datasync/activities/sync/pool-provider_test.go
@@ -1,23 +1,22 @@
 package sync_activity
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	mysql_queries "github.com/nucleuscloud/neosync/backend/gen/go/db/dbschemas/mysql"
-// 	neosync_benthos_sql "github.com/nucleuscloud/neosync/worker/internal/benthos/sql"
-// 	"github.com/stretchr/testify/assert"
-// )
+	neosync_benthos_sql "github.com/nucleuscloud/neosync/worker/internal/benthos/sql"
+	"github.com/stretchr/testify/assert"
+)
 
-// func Test_newPoolProvider(t *testing.T) {
-// 	assert.NotNil(t, newPoolProvider(nil))
-// }
+func Test_newPoolProvider(t *testing.T) {
+	assert.NotNil(t, newPoolProvider(nil))
+}
 
-// func Test_newPoolProvider_GetDb(t *testing.T) {
-// 	provider := newPoolProvider(func(dsn string) (neosync_benthos_sql.SqlDbtx, error) {
-// 		return mysql_queries.NewMockDBTX(t), nil
-// 	})
-// 	assert.NotNil(t, provider)
-// 	db, err := provider.GetDb("foo", "bar")
-// 	assert.NoError(t, err)
-// 	assert.NotNil(t, db)
-// }
+func Test_newPoolProvider_GetDb(t *testing.T) {
+	provider := newPoolProvider(func(dsn string) (neosync_benthos_sql.SqlDbtx, error) {
+		return neosync_benthos_sql.NewMockSqlDbtx(t), nil
+	})
+	assert.NotNil(t, provider)
+	db, err := provider.GetDb("foo", "bar")
+	assert.NoError(t, err)
+	assert.NotNil(t, db)
+}
