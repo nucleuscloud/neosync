1a0ad300463686966a18c32a1c6833749a455a79
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-04-24 14:36:44 -0700

    fix permissions link

--- frontend/apps/web/app/(mgmt)/[account]/connections/[id]/permissions/page.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/connections/[id]/permissions/page.tsx
@@ -185,7 +185,7 @@ function PermissionsPageContainer(props: PermissionsPageContainerProps) {
       <div className="flex flex-row justify-between items-center w-full">
         <div className="text-muted-foreground text-sm">
           Review the permissions that Neoynsc has to your connection.{' '}
-          <LearnMoreTag href="https://docs.neosync.dev/transformers/user-defined#transform-character-scramble" />
+          <LearnMoreTag href="https://docs.neosync.dev/connections/postgres#testing-your-connection" />
         </div>
         <Button variant="outline" onClick={handleMutate}>
           {isMutating ? <Spinner /> : <UpdateIcon />}
