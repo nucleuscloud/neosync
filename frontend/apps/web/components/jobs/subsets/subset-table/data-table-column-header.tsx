fae723cb781597e63bbab0b13a7df41576e34a94
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-05-03 18:09:14 -0700

    more subset tweaks

--- frontend/apps/web/components/jobs/subsets/subset-table/data-table-column-header.tsx
+++ frontend/apps/web/components/jobs/subsets/subset-table/data-table-column-header.tsx
@@ -25,10 +25,6 @@ export function DataTableColumnHeader<TData, TValue>({
   title,
   className,
 }: DataTableColumnHeaderProps<TData, TValue>) {
-  if (!column.getCanSort()) {
-    return <div className={cn(className)}>{title}</div>;
-  }
-
   return (
     <div className={cn('flex items-center', className)}>
       <div className="flex flex-row space-x-2 items-center">
@@ -58,14 +54,18 @@ export function DataTableColumnHeader<TData, TValue>({
               placeholder={`Search...`}
               className="w-36 border rounded"
             />
-            <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
-              <ArrowUpIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-              Asc
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => column.toggleSorting(true)}>
-              <ArrowDownIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-              Desc
-            </DropdownMenuItem>
+            {column.getCanSort() && (
+              <>
+                <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
+                  <ArrowUpIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                  Asc
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => column.toggleSorting(true)}>
+                  <ArrowDownIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                  Desc
+                </DropdownMenuItem>
+              </>
+            )}
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
