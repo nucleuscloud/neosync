86f7b21f59cc65468d8df1cc28ecc1c620b27232
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2024-05-08 10:15:57 -0700

    Nick/neos 1071 formatting on permissions table is off (#1942)

--- frontend/apps/web/components/permissions/PermissionsDataTable.tsx
+++ frontend/apps/web/components/permissions/PermissionsDataTable.tsx
@@ -67,9 +67,9 @@ export default function PermissionsDataTable<TData, TValue>({
 
   return (
     <div className="space-y-4">
-      <div className="flex flex-row items-center gap-2 justify-between">
+      <div className="flex flex-col lg:flex-row items-center gap-2 lg:gap-4 justify-between">
         <div>{ConnectionAlert}</div>
-        <div className="flex flex-row items-center gap-2">
+        <div className="flex flex-col lg:flex-row items-center gap-2">
           {TestConnectionButton && TestConnectionButton}
           <Button
             disabled={table.getState().columnFilters.length === 0}
@@ -90,7 +90,7 @@ export default function PermissionsDataTable<TData, TValue>({
         ref={tableContainerRef}
       >
         <StickyHeaderTable>
-          <TableHeader className="bg-gray-100 dark:bg-gray-800 sticky top-0 z-10 flex w-full">
+          <TableHeader className="bg-gray-100 dark:bg-gray-800 sticky top-0 z-10">
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow
                 key={headerGroup.id}
