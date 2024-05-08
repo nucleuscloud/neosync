4b9c5c327712da65c5428914ffa521b355e54941
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-05-08 09:15:27 -0700

    adds generate email form for transformers

--- frontend/apps/web/app/(mgmt)/[account]/new/transformer/UserDefinedTransformerForms/UserDefinedTransformerForm.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/new/transformer/UserDefinedTransformerForms/UserDefinedTransformerForm.tsx
@@ -2,6 +2,7 @@ import { TransformerSource } from '@neosync/sdk';
 import { ReactElement } from 'react';
 import UserDefinedGenerateCardNumberForm from './UserDefinedGenerateCardNumber';
 import UserDefinedGenerateCategoricalForm from './UserDefinedGenerateCategoricalForm';
+import UserDefinedGenerateEmailForm from './UserDefinedGenerateEmailForm';
 import UserDefinedGenerateFloat64Form from './UserDefinedGenerateFloat64Form';
 import UserDefinedGenerateGenderForm from './UserDefinedGenerateGenderForm';
 import UserDefinedGenerateInt64Form from './UserDefinedGenerateInt64Form';
@@ -59,6 +60,8 @@ export function UserDefinedTransformerForm(props: Props): ReactElement {
       return <UserDefinedTransformE164NumberForm isDisabled={disabled} />;
     case TransformerSource.TRANSFORM_EMAIL:
       return <UserDefinedTransformEmailForm isDisabled={disabled} />;
+    case TransformerSource.GENERATE_EMAIL:
+      return <UserDefinedGenerateEmailForm isDisabled={disabled} />;
     case TransformerSource.TRANSFORM_FIRST_NAME:
       return <UserDefinedTransformFirstNameForm isDisabled={disabled} />;
     case TransformerSource.TRANSFORM_FLOAT64:
