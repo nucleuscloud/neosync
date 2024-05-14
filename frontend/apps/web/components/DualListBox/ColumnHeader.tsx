00b9559a30733f7650ad4780dd04f993f05f295a
Evis Drenova <evis@usenucleus.cloud>
2024-05-14 11:54:03 -0700

    add filtering and searching support in subste table

--- frontend/apps/web/components/DualListBox/ColumnHeader.tsx
+++ frontend/apps/web/components/DualListBox/ColumnHeader.tsx
@@ -11,12 +11,14 @@ interface DataTableColumnHeaderProps<TData, TValue>
   extends React.HTMLAttributes<HTMLDivElement> {
   column: Column<TData, TValue>;
   title: string;
+  placeholder?: string;
 }
 
 export default function ColumnHeader<TData, TValue>({
   column,
   title,
   className,
+  placeholder,
 }: DataTableColumnHeaderProps<TData, TValue>) {
   return (
     <div className="flex flex-row gap-2 items-center justify-start">
@@ -26,7 +28,7 @@ export default function ColumnHeader<TData, TValue>({
             type="text"
             value={(column.getFilterValue() ?? '') as string}
             onChange={(e) => column.setFilterValue(e.target.value)}
-            placeholder={`Search ...`}
+            placeholder={placeholder ? placeholder : 'Search ...'}
             className="border border-gray-300 dark:border-gray-700 rounded bg-transparent text-xs h-8"
           />
         </div>
