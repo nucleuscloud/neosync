437ea4a23069ff9bc7481132f1769cbdc5583b14
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2024-05-24 14:32:27 -0700

    Fixes int32 types for system table schemas for Mysql (#2025)

--- backend/pkg/dbschemas/sql/mysql/schema/system.sql
+++ backend/pkg/dbschemas/sql/mysql/schema/system.sql
@@ -3,14 +3,14 @@ CREATE TABLE information_schema.columns (
   table_schema text not null,
   table_name text not null,
   column_name text not null,
-  ordinal_position int not null,
+  ordinal_position bigint not null,
   column_default text null,
   is_nullable text not null,
   data_type text not null,
-  character_maximum_length int,
-  numeric_precision int,
-  numeric_scale int,
-  extra text null
+  character_maximum_length bigint,
+  numeric_precision bigint,
+  numeric_scale bigint,
+  extra longtext null
 );
 
 create table information_schema.tables (
@@ -23,7 +23,7 @@ create table information_schema.key_column_usage (
   table_schema text not null,
   table_name text not null,
   column_name text not null,
-  ordinal_position int not null,
+  ordinal_position bigint not null,
   referenced_table_schema text not null,
   referenced_table_name text not null,
   referenced_column_name text not null
