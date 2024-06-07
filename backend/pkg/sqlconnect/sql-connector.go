fff9a4109aec7366170152a4820244e06e3ae3a5
Evis Drenova <evis@usenucleus.cloud>
2024-06-07 14:12:44 -0700

    fix linting errors

--- backend/pkg/sqlconnect/sql-connector.go
+++ backend/pkg/sqlconnect/sql-connector.go
@@ -457,6 +457,10 @@ func getGeneralDbConnectionConfigFromMysql(config *mgmtv1alpha1.ConnectionConfig
 				return nil, err
 			}
 
+			// #nosec G109
+			// this throws a linter error due to strconv.Atoi conversion above from string -> int32
+			// mysql ports are unsigned 16-bit numbers so they should never overflow in an in32
+			// https://stackoverflow.com/questions/20379491/what-is-the-optimal-way-to-store-port-numbers-in-a-mysql-database#:~:text=Port%20number%20is%20an%20unsinged,highest%20value%20can%20be%2065535.
 			port = int32(portInt)
 		}
 
