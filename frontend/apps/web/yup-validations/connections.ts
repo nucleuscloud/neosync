fcc753a4cef9fac266c3651e4ee51cbd9c483541
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-06-11 16:00:55 -0700

    fixes knips

--- frontend/apps/web/yup-validations/connections.ts
+++ frontend/apps/web/yup-validations/connections.ts
@@ -53,15 +53,6 @@ const connectionNameSchema = Yup.string()
     }
   );
 
-export const POSTGRES_CONNECTION = Yup.object({
-  host: Yup.string().required(),
-  name: Yup.string().required(),
-  user: Yup.string().required(),
-  pass: Yup.string().required(),
-  port: Yup.number().integer().positive().required(),
-  sslMode: Yup.string().optional(),
-});
-
 // todo: need to do better validation here
 export const SshTunnelFormValues = Yup.object({
   host: Yup.string(),
@@ -91,20 +82,6 @@ export const ClientTlsFormValues = Yup.object({
   clientKey: Yup.string(),
 });
 export type ClientTlsFormValues = Yup.InferType<typeof ClientTlsFormValues>;
-
-export const NEW_POSTGRES_CONNECTION = Yup.object({
-  connectionName: connectionNameSchema,
-  connection: POSTGRES_CONNECTION,
-  tunnel: SshTunnelFormValues,
-  options: SQL_OPTIONS_FORM_SCHEMA,
-});
-
-export const EXISTING_POSTGRES_CONNECTION = Yup.object({
-  id: Yup.string().uuid().required(),
-  connection: POSTGRES_CONNECTION,
-  tunnel: SshTunnelFormValues,
-  options: SQL_OPTIONS_FORM_SCHEMA,
-});
 
 export const SSL_MODES = [
   'disable',
