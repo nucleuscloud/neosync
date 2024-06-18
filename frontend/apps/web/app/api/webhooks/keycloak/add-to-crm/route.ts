5125e4be73e8b98df746c1d506552068dee010c7
Evis Drenova <evis@usenucleus.cloud>
2024-06-18 16:25:56 -0700

    add in attio crm update

---
+++ frontend/apps/web/app/api/webhooks/keycloak/add-to-crm/route.ts
@@ -0,0 +1,153 @@
+import { createHmac, timingSafeEqual } from 'crypto';
+import { NextRequest, NextResponse } from 'next/server';
+import * as Yup from 'yup';
+
+const SIG_SECRET = process.env.KEYCLOAK_SLACK_WEBHOOK_HMAC_SECRET;
+const KEYCLOAK_SIG_HEADER = 'X-Keycloak-Signature';
+const ATTIO_API_KEY = process.env.ATTIO_API_KEY;
+
+const RegisterEvent = Yup.object({
+  time: Yup.number().required(),
+  type: Yup.string().oneOf(['access.REGISTER']).required(),
+  authDetails: Yup.object({
+    userId: Yup.string().required(),
+    ipAddress: Yup.string().required(),
+  }),
+  details: Yup.object({
+    email: Yup.string().required(),
+    first_name: Yup.string(),
+    last_name: Yup.string(),
+    identity_provider: Yup.string(),
+  }).required(),
+});
+type RegisterEvent = Yup.InferType<typeof RegisterEvent>;
+
+// Note, when testing this method, the body must be sent in the raw, unbeautified format for the signature to work correctly
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  if (!SIG_SECRET) {
+    return NextResponse.json(
+      { message: 'missing signature secret in environment' },
+      { status: 500 }
+    );
+  }
+  const incomingSignature = req.headers.get(KEYCLOAK_SIG_HEADER);
+  if (!incomingSignature) {
+    return NextResponse.json(
+      { message: 'must provide sigure in header' },
+      { status: 403 }
+    );
+  }
+
+  try {
+    const text = await req.text();
+
+    const isTrusted = verifySignature(text, SIG_SECRET, incomingSignature);
+    if (!isTrusted) {
+      return NextResponse.json(
+        {
+          message:
+            'the signature in the header differs from the computed request body',
+        },
+        { status: 403 }
+      );
+    }
+
+    const registerEvent = await RegisterEvent.validate(JSON.parse(text));
+
+    let peopleRecordId = '';
+
+    const peopleBody = {
+      data: {
+        values: {
+          email_addresses: [registerEvent.details.email],
+        },
+      },
+    };
+
+    const commonHeaders = {
+      Authorization: `Bearer ${ATTIO_API_KEY}`,
+      'Content-Type': 'application/json',
+    };
+
+    // create the person first and then use that to associate it when we create the deal otherwise it fails
+    try {
+      const response = await fetch(
+        'https://api.attio.com/v2/objects/people/records',
+        {
+          method: 'POST',
+          headers: commonHeaders,
+          body: JSON.stringify(peopleBody),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('API call failed with status ' + response.statusText);
+      }
+
+      const responseData = await response.json();
+      peopleRecordId = responseData.data.id.record_id;
+    } catch (err) {
+      return NextResponse.json(
+        { message: 'unable to create person record', error: err?.toString() },
+        { status: 500 }
+      );
+    }
+
+    const dealbody = {
+      data: {
+        values: {
+          name: 'test',
+          stage: 'App sign up',
+          owner: 'evis@nucleuscloud.com',
+          associated_people: [
+            {
+              target_object: 'people',
+              target_record_id: peopleRecordId,
+            },
+          ],
+        },
+      },
+    };
+
+    // Then try to create the deal
+    try {
+      const response = await fetch(
+        'https://api.attio.com/v2/objects/deals/records/',
+        {
+          method: 'POST',
+          headers: commonHeaders,
+          body: JSON.stringify(dealbody),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          'Deal creation failed with status ' + response.statusText
+        );
+      }
+
+      return NextResponse.json({ message: 'ok', contents: JSON.parse(text) });
+    } catch (err) {
+      return NextResponse.json(
+        { message: 'unable to create deal record', error: err?.toString() },
+        { status: 500 }
+      );
+    }
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'unable to complete request', error: err },
+      { status: 500 }
+    );
+  }
+}
+
+function verifySignature(
+  body: string,
+  secret: string,
+  untrustedSignature: string
+): boolean {
+  const signature = createHmac('sha256', secret).update(body).digest('hex');
+  const trusted = Buffer.from(signature, 'ascii');
+  const untrusted = Buffer.from(untrustedSignature, 'ascii');
+  return timingSafeEqual(trusted, untrusted);
+}
