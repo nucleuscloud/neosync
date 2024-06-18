769fbe44b8073fed4837ab37c27a767cb75628b9
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-06-17 17:07:47 -0700

    simplifies role migration

--- backend/sql/postgresql/schema/20240617193058_role-simplification.up.sql
+++ backend/sql/postgresql/schema/20240617193058_role-simplification.up.sql
@@ -4,8 +4,5 @@ BEGIN
     IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'neosync_api_owner') THEN
         REASSIGN OWNED BY neosync_api_owner TO current_user;
     END IF;
-    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'neosync_api_schemauser') THEN
-        REASSIGN OWNED BY neosync_api_schemauser TO current_user;
-    END IF;
 END
 $$;
