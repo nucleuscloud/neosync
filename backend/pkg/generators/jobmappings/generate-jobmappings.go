9b4eafcdd1883075b75e32d7978593d2d94e5bd2
Alisha <alisha@nucleuscloud.com>
2024-06-25 10:27:57 -0700

    stash

--- backend/pkg/generators/jobmappings/generate-jobmappings.go
+++ backend/pkg/generators/jobmappings/generate-jobmappings.go
@@ -9,6 +9,8 @@ import (
 	"regexp"
 
 	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
+	pg_query "github.com/pganalyze/pg_query_go/v4"
+	pgquery "github.com/wasilibs/go-pgquery"
 )
 
 type Column struct {
@@ -52,6 +54,68 @@ func parseSQLSchema(sql string) ([]Table, error) {
 		})
 	}
 	return tables, nil
+}
+
+func qualifyPostgresWhereColumnNames(sql string, schema *string, table string) (string, error) {
+	tree, err := pgquery.Parse(sql)
+	if err != nil {
+		return "", err
+	}
+
+	for _, stmt := range tree.GetStmts() {
+		selectStmt := stmt.GetStmt().GetSelectStmt()
+
+		if selectStmt.WhereClause != nil {
+			updatePostgresExpr(schema, table, selectStmt.WhereClause)
+		}
+	}
+	updatedSql, err := pgquery.Deparse(tree)
+	if err != nil {
+		return "", err
+	}
+	return updatedSql, nil
+}
+
+func updatePostgresExpr(schema *string, table string, node *pg_query.Node) {
+	switch expr := node.Node.(type) {
+	case *pg_query.Node_SubLink:
+		updatePostgresExpr(schema, table, node.GetSubLink().GetTestexpr())
+	case *pg_query.Node_BoolExpr:
+		for _, arg := range expr.BoolExpr.GetArgs() {
+			updatePostgresExpr(schema, table, arg)
+		}
+	case *pg_query.Node_AExpr:
+		updatePostgresExpr(schema, table, expr.AExpr.GetLexpr())
+		updatePostgresExpr(schema, table, expr.AExpr.Rexpr)
+	case *pg_query.Node_ColumnDef:
+	case *pg_query.Node_ColumnRef:
+		col := node.GetColumnRef()
+		isQualified := false
+		var colName *string
+		// find col name and check if already has schema + table name
+		for _, f := range col.Fields {
+			val := f.GetString_().GetSval()
+			if schema != nil && val == *schema {
+				continue
+			}
+			if val == table {
+				isQualified = true
+				break
+			}
+			colName = &val
+		}
+		if !isQualified && colName != nil && *colName != "" {
+			fields := []*pg_query.Node{}
+			if schema != nil && *schema != "" {
+				fields = append(fields, pg_query.MakeStrNode(*schema))
+			}
+			fields = append(fields, []*pg_query.Node{
+				pg_query.MakeStrNode(table),
+				pg_query.MakeStrNode(*colName),
+			}...)
+			col.Fields = fields
+		}
+	}
 }
 
 func generateJobMapping(tables []Table) []*mgmtv1alpha1.JobMapping {
