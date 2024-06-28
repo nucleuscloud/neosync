b780f418e577b5440b873b6d371b344bf40264e4
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-06-28 13:16:11 -0700

    centralizes to a single mockery file

--- backend/internal/gcp/mock_ClientInterface.go
+++ backend/internal/gcp/mock_ClientInterface.go
@@ -140,6 +140,55 @@ func (_c *MockClientInterface_GetDbSchemaFromPrefix_Call) RunAndReturn(run func(
 	return _c
 }
 
+// GetRecordStreamFromPrefix provides a mock function with given fields: ctx, bucketName, prefix, onRecord
+func (_m *MockClientInterface) GetRecordStreamFromPrefix(ctx context.Context, bucketName string, prefix string, onRecord func(map[string][]byte) error) error {
+	ret := _m.Called(ctx, bucketName, prefix, onRecord)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetRecordStreamFromPrefix")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string, func(map[string][]byte) error) error); ok {
+		r0 = rf(ctx, bucketName, prefix, onRecord)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// MockClientInterface_GetRecordStreamFromPrefix_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetRecordStreamFromPrefix'
+type MockClientInterface_GetRecordStreamFromPrefix_Call struct {
+	*mock.Call
+}
+
+// GetRecordStreamFromPrefix is a helper method to define mock.On call
+//   - ctx context.Context
+//   - bucketName string
+//   - prefix string
+//   - onRecord func(map[string][]byte) error
+func (_e *MockClientInterface_Expecter) GetRecordStreamFromPrefix(ctx interface{}, bucketName interface{}, prefix interface{}, onRecord interface{}) *MockClientInterface_GetRecordStreamFromPrefix_Call {
+	return &MockClientInterface_GetRecordStreamFromPrefix_Call{Call: _e.mock.On("GetRecordStreamFromPrefix", ctx, bucketName, prefix, onRecord)}
+}
+
+func (_c *MockClientInterface_GetRecordStreamFromPrefix_Call) Run(run func(ctx context.Context, bucketName string, prefix string, onRecord func(map[string][]byte) error)) *MockClientInterface_GetRecordStreamFromPrefix_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string), args[2].(string), args[3].(func(map[string][]byte) error))
+	})
+	return _c
+}
+
+func (_c *MockClientInterface_GetRecordStreamFromPrefix_Call) Return(_a0 error) *MockClientInterface_GetRecordStreamFromPrefix_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *MockClientInterface_GetRecordStreamFromPrefix_Call) RunAndReturn(run func(context.Context, string, string, func(map[string][]byte) error) error) *MockClientInterface_GetRecordStreamFromPrefix_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewMockClientInterface creates a new instance of MockClientInterface. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewMockClientInterface(t interface {
