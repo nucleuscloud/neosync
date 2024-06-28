1f1b0d8c66c97853deb111143f49922b880b0f57
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2024-06-28 13:06:21 -0700

    Cleaning up docs, moving mdx back to md, fixes ul styling (#2225)

--- docs/src/CustomComponents/HeroImage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { HeroHeader } from './MainHero';
-
-export const HeroImage = () => (
-  <div className="heroImage">
-    {/* <img src="https://assets.nucleuscloud.com/neosync/docs/neosync-main-header-animated.svg" /> */}
-    <HeroHeader />
-  </div>
-);
