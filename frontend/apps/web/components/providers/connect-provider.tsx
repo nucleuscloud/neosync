4bf1074f44fdeeb602c7ab3e0428e0d021540261
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-07-01 09:34:08 -0700

    gets toast working for react query

--- frontend/apps/web/components/providers/connect-provider.tsx
+++ frontend/apps/web/components/providers/connect-provider.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { getErrorMessage } from '@/util/util';
 import { TransportProvider } from '@connectrpc/connect-query';
 import { createConnectTransport } from '@connectrpc/connect-web';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 import { ReactElement, ReactNode, useMemo } from 'react';
+import { useToast } from '../ui/use-toast';
 
 interface Props {
   children: ReactNode;
@@ -13,6 +19,7 @@ interface Props {
 export default function ConnectProvider(props: Props): ReactElement {
   const { children, apiBaseUrl } = props;
   const { data } = useSession();
+  const { toast } = useToast();
   const connectTransport = useMemo(() => {
     return createConnectTransport({
       baseUrl: apiBaseUrl,
@@ -27,7 +34,24 @@ export default function ConnectProvider(props: Props): ReactElement {
     });
   }, [apiBaseUrl, data?.accessToken]);
 
-  const queryClient = useMemo(() => new QueryClient(), []);
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          // good blog here: https://tkdodo.eu/blog/react-query-error-handling
+          onError(error, query) {
+            toast({
+              title: 'Something went wrong',
+              description: getErrorMessage(error),
+              variant: 'destructive',
+              key: query.queryKey.toString(),
+            });
+          },
+        }),
+        defaultOptions: {},
+      }),
+    []
+  );
 
   return (
     <TransportProvider transport={connectTransport}>
