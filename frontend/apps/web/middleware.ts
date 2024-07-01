4136fbec263a1b6e6eb00e02283289df398c9af5
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-07-01 12:40:36 -0700

    updates middleware to properly set auth on req headers only

--- frontend/apps/web/middleware.ts
+++ frontend/apps/web/middleware.ts
@@ -5,14 +5,12 @@ import { PUBLIC_PATHNAME, getSystemAppConfig } from './app/api/config/config';
 export default auth((req) => {
   if (req.nextUrl.pathname.startsWith(PUBLIC_PATHNAME)) {
     const sysConfig = getSystemAppConfig();
-    const newheaders = new Headers(req.headers);
     if (req.auth?.accessToken) {
-      newheaders.set('Authorization', `Bearer ${req.auth.accessToken}`);
+      req.headers.set('Authorization', `Bearer ${req.auth.accessToken}`);
     }
     return NextResponse.rewrite(
       `${sysConfig.neosyncApiBaseUrl}${trimPrefix(req.nextUrl.pathname, PUBLIC_PATHNAME)}${req.nextUrl.search}`,
       {
-        headers: newheaders,
         request: req,
       }
     );
