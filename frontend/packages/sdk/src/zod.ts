f34a9a1461f9e9f6969b60a0b8c5043659bc9e41
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-07-05 15:29:17 -0700

    sets up separate entrypoint for zod

---
+++ frontend/packages/sdk/src/zod.ts
@@ -0,0 +1,4 @@
+export * from './client/mgmt/v1alpha1/job_zod.js';
+export * from './client/mgmt/v1alpha1/metrics_zod.js';
+export * from './client/mgmt/v1alpha1/transformer_zod.js';
+export * from './client/mgmt/v1alpha1/user_account_zod.js';
