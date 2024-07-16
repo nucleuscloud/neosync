0ba3203acb75deceaf9aa2de82ad63357d626229
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-07-16 19:02:24 -0400

    updates generate script to include env file

--- backend/scripts/gen.sh
+++ backend/scripts/gen.sh
@@ -26,6 +26,7 @@ else
 fi
 mkdir -p ${BUF_CACHE_DIRECTORY}
 
+
 # `buf format -w` writes to each file, so causes tilt to loop. instead we instruct buf
 # to just output the changes in diff format and apply those.
 docker run --rm -i \
@@ -36,18 +37,26 @@ docker run --rm -i \
   --workdir "/protos" \
   "bufbuild/buf:${BUF_VERSION}" format -d | patch -d ./protos -p0 --quiet
 
-# buf generate
-docker run --rm -i \
-  --user "${USER_ID}:${GROUP_ID}" \
+ENV_FILE="./.env.dev.secrets"
+BUF_GENERATE_CMD="docker run --rm -i"
+# Check if the environment file exists and include it if it does
+if [ -f "$ENV_FILE" ]; then
+  BUF_GENERATE_CMD="$BUF_GENERATE_CMD --env-file $ENV_FILE"
+fi
+
+BUF_GENERATE_CMD="$BUF_GENERATE_CMD \
+  --user \"${USER_ID}:${GROUP_ID}\" \
   --env BUF_CACHE_DIR=/workspace/.cache \
-  --volume "./gen:/workspace/gen" \
-  --volume "./buf.yaml:/workspace/buf.yaml" \
-  --volume "./buf.lock:/workspace/buf.lock" \
-  --volume "./buf.gen.yaml:/workspace/buf.gen.yaml" \
-  --volume "./protos:/workspace/protos" \
-  --volume "${BUF_CACHE_DIRECTORY}:/workspace/.cache" \
-  --workdir "/workspace" \
-  "bufbuild/buf:${BUF_VERSION}" generate &
+  --volume \"./gen:/workspace/gen\" \
+  --volume \"./buf.yaml:/workspace/buf.yaml\" \
+  --volume \"./buf.lock:/workspace/buf.lock\" \
+  --volume \"./buf.gen.yaml:/workspace/buf.gen.yaml\" \
+  --volume \"./protos:/workspace/protos\" \
+  --volume \"${BUF_CACHE_DIRECTORY}:/workspace/.cache\" \
+  --workdir \"/workspace\" \
+  \"bufbuild/buf:${BUF_VERSION}\" generate &"
+
+eval $BUF_GENERATE_CMD
 
 # sqlc
 docker run --rm -i \
