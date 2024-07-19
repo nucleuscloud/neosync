1bc0f8853e4bdaf35a06d211c3e3ac2e834b3fe2
Alisha <alisha@nucleuscloud.com>
2024-07-18 17:30:35 -0700

    go lint

--- worker/pkg/benthos/transformers/generator_utils.go
+++ worker/pkg/benthos/transformers/generator_utils.go
@@ -111,7 +111,7 @@ func ParseBloblangSpec(benthosSpec *BenthosSpec) (*ParsedBenthosSpec, error) {
 }
 
 func lowercaseFirst(s string) string {
-	if len(s) == 0 {
+	if s == "" {
 		return s
 	}
 	return strings.ToLower(string(s[0])) + s[1:]
