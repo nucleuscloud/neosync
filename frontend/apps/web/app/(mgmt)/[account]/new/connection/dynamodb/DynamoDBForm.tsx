7ca6d9b9d355e2f3095327c1899d90dc0ef64539
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-07-24 12:10:02 -0700

    put dynamodb config behind accordions

--- frontend/apps/web/app/(mgmt)/[account]/new/connection/dynamodb/DynamoDBForm.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/new/connection/dynamodb/DynamoDBForm.tsx
@@ -8,6 +8,12 @@ import PermissionsDialog from '@/components/permissions/PermissionsDialog';
 import { useAccount } from '@/components/providers/account-provider';
 import SkeletonForm from '@/components/skeleton/SkeletonForm';
 import SwitchCard from '@/components/switches/SwitchCard';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import {
@@ -286,145 +292,179 @@ export default function NewDynamoDBForm(): ReactElement {
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="db.region"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>AWS Region</FormLabel>
-              <FormDescription>The AWS region to target</FormDescription>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="db.endpoint"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Custom Endpoint</FormLabel>
-              <FormDescription>
-                Allows specifying a custom endpoint for the AWS API
-              </FormDescription>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="db.credentials.profile"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>AWS Profile Name</FormLabel>
-              <FormDescription>AWS Profile Name</FormDescription>
-              <FormControl>
-                <Input placeholder="default" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="db.credentials.accessKeyId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Access Key Id</FormLabel>
-              <FormDescription>Access Key Id</FormDescription>
-              <FormControl>
-                <Input placeholder="Access Key Id" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="db.credentials.secretAccessKey"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>AWS Secret Access Key</FormLabel>
-              <FormDescription>AWS Secret Access Key</FormDescription>
-              <FormControl>
-                <PasswordInput placeholder="Secret Access Key" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="db.credentials.sessionToken"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>AWS Session Token</FormLabel>
-              <FormDescription>AWS Session Token</FormDescription>
-              <FormControl>
-                <Input placeholder="Session Token" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="db.credentials.fromEc2Role"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <SwitchCard
-                  isChecked={field.value || false}
-                  onCheckedChange={field.onChange}
-                  title="From EC2 Role"
-                  description="Use the credentials of a host EC2 machine configured to assume an IAM role associated with the instance."
+        <Accordion
+          type="multiple"
+          defaultValue={['credentials']}
+          className="w-full"
+        >
+          <AccordionItem value="credentials">
+            <AccordionTrigger>AWS Credentials</AccordionTrigger>
+            <AccordionContent className="space-y-4">
+              <p className="text-sm">
+                This section is used to configure authentication credentials to
+                allow access to the DynamoDB tables
+              </p>
+              <div className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="db.credentials.profile"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>AWS Profile Name</FormLabel>
+                      <FormDescription>AWS Profile Name</FormDescription>
+                      <FormControl>
+                        <Input placeholder="default" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
 
-        <FormField
-          control={form.control}
-          name="db.credentials.roleArn"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>AWS Role ARN</FormLabel>
-              <FormDescription>Role ARN</FormDescription>
-              <FormControl>
-                <Input placeholder="Role Arn" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormField
+                  control={form.control}
+                  name="db.credentials.accessKeyId"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Access Key Id</FormLabel>
+                      <FormDescription>Access Key Id</FormDescription>
+                      <FormControl>
+                        <Input placeholder="Access Key Id" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-        <FormField
-          control={form.control}
-          name="db.credentials.roleExternalId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>AWS Role External Id</FormLabel>
-              <FormDescription>Role External Id</FormDescription>
-              <FormControl>
-                <Input placeholder="Role External Id" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormField
+                  control={form.control}
+                  name="db.credentials.secretAccessKey"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>AWS Secret Access Key</FormLabel>
+                      <FormDescription>AWS Secret Access Key</FormDescription>
+                      <FormControl>
+                        <PasswordInput
+                          placeholder="Secret Access Key"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
+                <FormField
+                  control={form.control}
+                  name="db.credentials.sessionToken"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>AWS Session Token</FormLabel>
+                      <FormDescription>AWS Session Token</FormDescription>
+                      <FormControl>
+                        <Input placeholder="Session Token" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="db.credentials.fromEc2Role"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <SwitchCard
+                          isChecked={field.value || false}
+                          onCheckedChange={field.onChange}
+                          title="From EC2 Role"
+                          description="Use the credentials of a host EC2 machine configured to assume an IAM role associated with the instance."
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="db.credentials.roleArn"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>AWS Role ARN</FormLabel>
+                      <FormDescription>Role ARN</FormDescription>
+                      <FormControl>
+                        <Input placeholder="Role Arn" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="db.credentials.roleExternalId"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>AWS Role External Id</FormLabel>
+                      <FormDescription>Role External Id</FormDescription>
+                      <FormControl>
+                        <Input placeholder="Role External Id" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="advanced">
+            <AccordionTrigger>AWS Advanced Configuration</AccordionTrigger>
+            <AccordionContent className="space-y-4">
+              <p className="text-sm">
+                This is an optional section and is used if you need to tweak the
+                AWS SDK to connect to a different region or endpoint other than
+                the default.
+              </p>
+              <div className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="db.region"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>AWS Region</FormLabel>
+                      <FormDescription>
+                        The AWS region to target
+                      </FormDescription>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="db.endpoint"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Custom Endpoint</FormLabel>
+                      <FormDescription>
+                        Allows specifying a custom endpoint for the AWS API
+                      </FormDescription>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
         <PermissionsDialog
           checkResponse={
             validationResponse ?? new CheckConnectionConfigResponse({})
