5bb25f3bbfb279a3b6124640ea242eb19f62b1e9
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-07-25 15:03:43 -0700

    fixes proc configs for mongo

--- worker/pkg/workflows/datasync/activities/gen-benthos-configs/sync-mongo.go
+++ worker/pkg/workflows/datasync/activities/gen-benthos-configs/sync-mongo.go
@@ -62,7 +62,7 @@ func (b *benthosBuilder) getMongoDbSyncBenthosConfigResponses(
 		processorConfigs, err := buildProcessorConfigsByRunType(
 			ctx,
 			b.transformerclient,
-			&tabledependency.RunConfig{RunType: tabledependency.RunTypeInsert},
+			&tabledependency.RunConfig{RunType: tabledependency.RunTypeInsert, Table: tableMapping.Table, SelectColumns: columns, InsertColumns: columns},
 			map[string][]*referenceKey{},
 			map[string][]*referenceKey{},
 			b.jobId,
