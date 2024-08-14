0e75515b2134d5426b1da5b251655dd9e8afdfe2
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-08-14 16:22:58 -0700

    consolidates file

--- backend/pkg/sqlmanager/postgres_sql-manager_integration_test.go
+++ backend/pkg/sqlmanager/postgres_sql-manager_integration_test.go
@@ -13,6 +13,7 @@ import (
 	pg_queries "github.com/nucleuscloud/neosync/backend/gen/go/db/dbschemas/postgresql"
 	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
 	"github.com/nucleuscloud/neosync/backend/pkg/sqlconnect"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/testcontainers/testcontainers-go"
@@ -98,4 +99,54 @@ func TestPostgresIntegrationTestSuite(t *testing.T) {
 		return
 	}
 	suite.Run(t, new(PostgresIntegrationTestSuite))
+}
+
+func (s *PostgresIntegrationTestSuite) Test_NewPooledSqlDb() {
+	t := s.T()
+
+	conn, err := s.sqlmanager.NewPooledSqlDb(s.ctx, slog.Default(), s.mgmtconn)
+	requireNoConnErr(t, conn, err)
+	requireValidDatabase(t, s.ctx, conn, "postgres", "SELECT 1")
+	conn.Db.Close()
+}
+
+func (s *PostgresIntegrationTestSuite) Test_NewSqlDb() {
+	t := s.T()
+
+	connTimeout := 5
+	conn, err := s.sqlmanager.NewSqlDb(s.ctx, slog.Default(), s.mgmtconn, &connTimeout)
+	requireNoConnErr(t, conn, err)
+
+	requireValidDatabase(t, s.ctx, conn, "postgres", "SELECT 1")
+	conn.Db.Close()
+}
+
+func (s *PostgresIntegrationTestSuite) Test_NewSqlDbFromUrl() {
+	t := s.T()
+	conn, err := s.sqlmanager.NewSqlDbFromUrl(s.ctx, "postgres", s.pgcfg.GetUrl())
+	requireNoConnErr(t, conn, err)
+
+	requireValidDatabase(t, s.ctx, conn, "postgres", "SELECT 1")
+	conn.Db.Close()
+}
+
+func (s *PostgresIntegrationTestSuite) Test_NewSqlDbFromConnectionConfig() {
+	t := s.T()
+	connTimeout := 5
+	conn, err := s.sqlmanager.NewSqlDbFromConnectionConfig(s.ctx, slog.Default(), s.mgmtconn.GetConnectionConfig(), &connTimeout)
+	requireNoConnErr(t, conn, err)
+
+	requireValidDatabase(t, s.ctx, conn, "postgres", "SELECT 1")
+	conn.Db.Close()
+}
+
+func requireNoConnErr(t testing.TB, conn *SqlConnection, err error) {
+	require.NoError(t, err)
+	require.NotNil(t, conn)
+}
+
+func requireValidDatabase(t testing.TB, ctx context.Context, conn *SqlConnection, driver, statement string) {
+	require.Equal(t, conn.Driver, driver)
+	err := conn.Db.Exec(ctx, statement)
+	require.NoError(t, err)
 }
