ca1c1e9f791a5b204d24bed8c0c5dcbaacfce243
Frankie Murillo <frankidatank@gmail.com>
2024-08-18 15:18:14 -0700

    feat: remove Form components, only update font size and weight

--- frontend/apps/web/app/(mgmt)/[account]/new/job/aigenerate/single/schema/SampleTable/SampleTable.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/new/job/aigenerate/single/schema/SampleTable/SampleTable.tsx
@@ -1,10 +1,4 @@
 import {
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-} from '@/components/ui/form';
-import {
   Table,
   TableBody,
   TableCell,
@@ -20,14 +14,12 @@ import {
 } from '@tanstack/react-table';
 import { ReactElement } from 'react';
 import { SampleRecord } from '../types';
-
 interface Props {
   columns: ColumnDef<SampleRecord>[];
   records: SampleRecord[];
 }
 export default function SampleTable(props: Props): ReactElement {
   const { records, columns } = props;
-
   const table = useReactTable({
     data: records,
     columns: columns,
@@ -36,65 +28,70 @@ export default function SampleTable(props: Props): ReactElement {
   });
 
   return (
-    <FormField
-      name="syntheticDataSample"
-      render={() => (
-        <FormItem>
-          <FormLabel>Synthetic Data Sample</FormLabel>
-          <FormDescription>
+    <div className="space-y-4">
+      <div className="flex flex-col gap-2">
+        <div>
+          <h2 className="font-semibold tracking-tight">
+            Synthetic Data Sample
+          </h2>
+        </div>
+        <div>
+          <p className="text-sm font-light tracking-tight">
             A sample of synthetic data given the inputs above. Returns at most
             10 records.
-          </FormDescription>
-          <div className="rounded-md border overflow-auto dark:border-gray-700">
-            <Table>
-              <TableHeader className="bg-gray-100 dark:bg-gray-800">
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => (
-                      <TableHead key={header.id} className="pl-2">
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </TableHead>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableHeader>
-              <TableBody>
-                {table.getRowModel().rows?.length ? (
-                  table.getRowModel().rows.map((row) => (
-                    <TableRow
-                      key={row.id}
-                      data-state={row.getIsSelected() && 'selected'}
-                    >
-                      {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
+          </p>
+        </div>
+      </div>
+      <div className="rounded-md border overflow-auto dark:border-gray-700">
+        <Table>
+          <TableHeader className="bg-gray-100 dark:bg-gray-800">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} className="pl-2">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
                           )}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell
-                      colSpan={columns.length}
-                      className="h-24 text-center"
-                    >
-                      Click Sample to generate a preview of synthetic data.
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
                     </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </div>
-        </FormItem>
-      )}
-    />
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Click Sample to generate a preview of synthetic data.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 }
