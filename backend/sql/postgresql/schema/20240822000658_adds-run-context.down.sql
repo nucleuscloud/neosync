d3b68e18023a62b49cf350150518cec3a3607811
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-08-21 17:11:02 -0700

    adds new table run contexts

---
+++ backend/sql/postgresql/schema/20240822000658_adds-run-context.down.sql
@@ -0,0 +1,3 @@
+DROP TRIGGER IF EXISTS update_neosync_api_runcontexts_updated_at ON neosync_api.runcontexts;
+
+DROP TABLE IF EXISTS neosync_api.runcontexts;
---
+++ backend/sql/postgresql/schema/20240822000658_adds-run-context.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE neosync_api.runcontexts (
+    workflow_id TEXT NOT NULL,
+    external_id TEXT NOT NULL,
+    value TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by_id UUID NOT NULL,
+    updated_by_id UUID NOT NULL,
+    PRIMARY KEY (workflow_id, external_id)
+);
+
+CREATE TRIGGER update_neosync_api_runcontexts_updated_at
+BEFORE UPDATE ON neosync_api.runcontexts
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
