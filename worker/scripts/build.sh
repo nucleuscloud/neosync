3314f964c235d0c55b4a42180193a3654c847272
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-08-23 13:39:05 -0700

    Reapply "NEOS-1360: enables cgo for worker to enable postgres query pa… (#2527)
    This reverts commit 3177ff76692ecdaeacf1971892edc3af61f2ed62.

--- worker/scripts/build.sh
+++ worker/scripts/build.sh
@@ -2,9 +2,9 @@
 
 if [ ! -z "${TILT_HOST}" ]; then
   # when invoked by tilt set the OS so that binary will run on linux container
-  CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o bin/worker cmd/worker/*.go
+  GOOS=linux go build -ldflags="-s -w" -o bin/worker cmd/worker/*.go
 elif [ ! -z "${GOOS}" ]; then
-  CGO_ENABLED=0 GOOS="${GOOS}" go build -ldflags="-s -w" -o bin/worker cmd/worker/*.go
+  GOOS="${GOOS}" go build -ldflags="-s -w" -o bin/worker cmd/worker/*.go
 else
-  CGO_ENABLED=0 go build -ldflags="-s -w" -o bin/worker cmd/worker/*.go
+  go build -ldflags="-s -w" -o bin/worker cmd/worker/*.go
 fi
