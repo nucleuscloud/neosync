58424796d0fad9336d507b0145f54c438a3ab043
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-09-06 11:37:50 -0700

    more

--- backend/services/mgmt/v1alpha1/user-account-service/billing.go
+++ backend/services/mgmt/v1alpha1/user-account-service/billing.go
@@ -2,15 +2,37 @@ package v1alpha1_useraccountservice
 
 import (
 	"context"
+	"fmt"
 
 	"connectrpc.com/connect"
 	mgmtv1alpha1 "github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1"
+)
+
+const (
+	AccountTypePersonal = iota
+	AccountTypeTeam
 )
 
 func (s *Service) GetAccountStatus(
 	ctx context.Context,
 	req *connect.Request[mgmtv1alpha1.GetAccountStatusRequest],
 ) (*connect.Response[mgmtv1alpha1.GetAccountStatusResponse], error) {
+	accountId, err := s.verifyUserInAccount(ctx, req.Msg.GetAccountId())
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := s.db.Q.GetAccount(ctx, s.db.Db, *accountId)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve account: %w", err)
+	}
+
+	if s.cfg.IsNeosyncCloud {
+		if account.AccountType == 0 {
+
+		}
+	}
+
 	return connect.NewResponse(&mgmtv1alpha1.GetAccountStatusResponse{}), nil
 }
 
