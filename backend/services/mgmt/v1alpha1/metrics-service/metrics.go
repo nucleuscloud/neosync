992a4546401fbe46765477565613db0192e344a8
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-09-09 10:55:31 -0700

    file

--- backend/services/mgmt/v1alpha1/metrics-service/metrics.go
+++ backend/services/mgmt/v1alpha1/metrics-service/metrics.go
@@ -3,9 +3,6 @@ package v1alpha1_metricsservice
 import (
 	"context"
 	"fmt"
-	"log/slog"
-	"sort"
-	"sync"
 	"time"
 
 	"connectrpc.com/connect"
@@ -13,10 +10,6 @@ import (
 	logger_interceptor "github.com/nucleuscloud/neosync/backend/internal/connect/interceptors/logger"
 	nucleuserrors "github.com/nucleuscloud/neosync/backend/internal/errors"
 	"github.com/nucleuscloud/neosync/backend/pkg/metrics"
-	"golang.org/x/sync/errgroup"
-
-	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
-	"github.com/prometheus/common/model"
 )
 
 func (s *Service) GetDailyMetricCount(
@@ -78,11 +71,11 @@ func (s *Service) GetDailyMetricCount(
 		return nil, nucleuserrors.NewBadRequest("must provide a valid identifier to proceed")
 	}
 
-	query, err := getPromQueryFromMetric(req.Msg.GetMetric(), queryLabels, "1d")
+	query, err := metrics.GetPromQueryFromMetric(req.Msg.GetMetric(), queryLabels, "1d")
 	if err != nil {
 		return nil, fmt.Errorf("unable to compute valid prometheus query: %w", err)
 	}
-	results, _, err := getDailyUsageFromProm(ctx, s.prometheusclient, query, start, end, logger)
+	results, _, err := metrics.GetDailyUsageFromProm(ctx, s.prometheusclient, query, start, end, logger)
 	if err != nil {
 		return nil, err
 	}
@@ -155,12 +148,12 @@ func (s *Service) GetMetricCount(
 	}
 
 	dayWindow := daysBetween(start, end)
-	query, err := getPromQueryFromMetric(req.Msg.GetMetric(), queryLabels, fmt.Sprintf("%dd", dayWindow))
+	query, err := metrics.GetPromQueryFromMetric(req.Msg.GetMetric(), queryLabels, fmt.Sprintf("%dd", dayWindow))
 	if err != nil {
 		return nil, fmt.Errorf("unable to compute valid prometheus query: %w", err)
 	}
 
-	totalCount, err := getTotalUsageFromProm(ctx, s.prometheusclient, query, end, logger)
+	totalCount, err := metrics.GetTotalUsageFromProm(ctx, s.prometheusclient, query, end, logger)
 	if err != nil {
 		return nil, err
 	}
@@ -177,133 +170,6 @@ func daysBetween(start, end time.Time) int {
 	days := int(duration.Hours()/24) + 1
 	// Convert the number of days to a string and return
 	return days
-}
-
-func getPromQueryFromMetric(
-	metric mgmtv1alpha1.RangedMetricName,
-	labels metrics.MetricLabels,
-	timeWindow string,
-) (string, error) {
-	metricName, err := getMetricNameFromEnum(metric)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("sum(max_over_time(%s{%s}[%s]))", metricName, labels.ToPromQueryString(), timeWindow), nil
-}
-
-const (
-	inputReceivedTotalMetric = "input_received_total"
-)
-
-func getMetricNameFromEnum(metric mgmtv1alpha1.RangedMetricName) (string, error) {
-	switch metric {
-	case mgmtv1alpha1.RangedMetricName_RANGED_METRIC_NAME_INPUT_RECEIVED:
-		return inputReceivedTotalMetric, nil
-	default:
-		return "", fmt.Errorf("invalid metric name")
-	}
-}
-
-const usageDateFormat = "2006-01-02"
-
-func getDailyUsageFromProm(ctx context.Context, api promv1.API, query string, start, end time.Time, logger *slog.Logger) ([]*mgmtv1alpha1.DayResult, float64, error) {
-	var dailyResults []*mgmtv1alpha1.DayResult
-	dailyTotals := make(map[string]float64)
-	var overallTotal float64
-
-	errgrp, errctx := errgroup.WithContext(ctx)
-	errgrp.SetLimit(10)
-	mu := sync.Mutex{}
-	// Iterate through each day in the range
-	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
-		d := d
-		errgrp.Go(func() error {
-			dayStart := time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, time.UTC)
-			dayEnd := dayStart.AddDate(0, 0, 1).Add(-time.Nanosecond)
-			result, warnings, err := api.Query(errctx, query, dayEnd)
-			if err != nil {
-				return fmt.Errorf("error querying Prometheus for date %s: %w", d.Format(usageDateFormat), err)
-			}
-			if len(warnings) > 0 {
-				logger.Warn(fmt.Sprintf("[PROMETHEUS]: %v", warnings))
-			}
-
-			// Process the results for this day
-			vector, ok := result.(model.Vector)
-			if !ok {
-				return fmt.Errorf("error casting result to model.Vector for date %s", d.Format(usageDateFormat))
-			}
-
-			for _, sample := range vector {
-				// using the dayStart timestamp here instead of the sample TS because sometimes it comes back as UTC for the next day
-				// which throws off the count. Using the dayStart we know that it will
-				day := dayStart.Format(usageDateFormat)
-				value := float64(sample.Value)
-				mu.Lock()
-				dailyTotals[day] += value
-				mu.Unlock()
-			}
-			return nil
-		})
-	}
-	err := errgrp.Wait()
-	if err != nil {
-		return nil, -1, err
-	}
-
-	var dates []string
-	for day := range dailyTotals {
-		dates = append(dates, day)
-	}
-	sort.Strings(dates)
-
-	for _, day := range dates {
-		date, err := time.Parse(usageDateFormat, day)
-		if err != nil {
-			return nil, 0, err
-		}
-		mgmtDate := timeToDate(date)
-		dailyResults = append(dailyResults, &mgmtv1alpha1.DayResult{
-			Date:  &mgmtDate,
-			Count: uint64(dailyTotals[day]),
-		})
-		overallTotal += dailyTotals[day]
-	}
-
-	return dailyResults, overallTotal, nil
-}
-
-func getTotalUsageFromProm(ctx context.Context, api promv1.API, query string, dayEnd time.Time, logger *slog.Logger) (float64, error) {
-	var overallTotal float64
-
-	result, warnings, err := api.Query(ctx, query, dayEnd)
-	if err != nil {
-		return -1, fmt.Errorf("error querying Prometheus for date %s: %w", dayEnd, err)
-	}
-	if len(warnings) > 0 {
-		logger.Warn(fmt.Sprintf("[PROMETHEUS]: %v", warnings))
-	}
-
-	// Process the results for this day
-	vector, ok := result.(model.Vector)
-	if !ok {
-		return -1, fmt.Errorf("error casting result to model.Vector for date %s", dayEnd)
-	}
-
-	for _, sample := range vector {
-		value := float64(sample.Value)
-		overallTotal += value
-	}
-
-	return overallTotal, nil
-}
-
-func timeToDate(t time.Time) mgmtv1alpha1.Date {
-	return mgmtv1alpha1.Date{
-		Year:  uint32(t.Year()),  //nolint:gosec // Ignoring for now
-		Month: uint32(t.Month()), //nolint:gosec // Ignoring for now
-		Day:   uint32(t.Day()),   //nolint:gosec // Ignoring for now
-	}
 }
 
 func dateToTime(d *mgmtv1alpha1.Date) time.Time {
