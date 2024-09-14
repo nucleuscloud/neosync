135dddbd49cf97c1aed15b21b085c5763539313c
Evis Drenova <evis@usenucleus.cloud>
2024-09-13 23:21:01 -0700

    add optional chaining to toDate() func to aviod invalid dates

--- frontend/apps/web/app/(mgmt)/[account]/runs/[id]/components/JobRunActivityTable/columns.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/runs/[id]/components/JobRunActivityTable/columns.tsx
@@ -41,7 +41,7 @@ export function getColumns(props: GetColumnsProps): ColumnDef<JobRunEvent>[] {
         return (
           <div className="flex space-x-2">
             <span className="max-w-[500px] truncate font-medium">
-              {startTime && formatDateTimeMilliseconds(startTime.toDate())}
+              {startTime && formatDateTimeMilliseconds(startTime?.toDate())}
             </span>
           </div>
         );
