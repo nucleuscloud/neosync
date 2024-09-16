294685265fe33ca54dcb4094415b52c3164d4249
Alisha <alisha@nucleuscloud.com>
2024-09-16 15:26:23 -0700

    fix storybook gh action

--- frontend/apps/web/.storybook/main.ts
+++ frontend/apps/web/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-import { dirname, join } from 'path';
+import path, { dirname, join } from 'path';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -26,6 +26,13 @@ const config: StorybookConfig = {
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
     options: {},
+  },
+  webpackFinal: (config) => {
+    if (config.resolve && config.resolve.alias) {
+      config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+    }
+
+    return config;
   },
 };
 export default config;
