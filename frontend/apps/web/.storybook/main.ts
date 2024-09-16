4be7c8dc768ecd41c7825e21cda2887686ab381f
Alisha <alisha@nucleuscloud.com>
2024-09-16 13:28:30 -0700

    add story book play tests

--- frontend/apps/web/.storybook/main.ts
+++ frontend/apps/web/.storybook/main.ts
@@ -21,6 +21,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath("@storybook/addon-themes")
   ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
--- frontend/apps/web/.storybook/preview.ts
+++ frontend/apps/web/.storybook/preview.ts
@@ -1,5 +1,17 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
 import type { Preview } from '@storybook/react';
+
 import '../app/globals.css';
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
 
 const preview: Preview = {
   parameters: {
