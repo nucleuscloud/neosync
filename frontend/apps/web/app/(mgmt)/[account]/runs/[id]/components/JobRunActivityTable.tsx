3301ecb56495255c068685366d705314fe008933
Evis Drenova <evis@usenucleus.cloud>
2024-09-18 23:02:57 -0700

    refactor funcs

--- frontend/apps/web/app/(mgmt)/[account]/runs/[id]/components/JobRunActivityTable.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/runs/[id]/components/JobRunActivityTable.tsx
@@ -1,4 +1,4 @@
-import RunTimeline, { Task } from '@/components/RunTImeline/RunTimeline';
+import RunTimeline from '@/components/RunTImeline/RunTimeline';
 import SkeletonTable from '@/components/skeleton/SkeletonTable';
 import { JobRunEvent } from '@neosync/sdk';
 import { ReactElement, useMemo } from 'react';
@@ -22,12 +22,11 @@ export default function JobRunActivityTable(
   }
   const isError = jobRunEvents.some((e) => e.tasks.some((t) => t.error));
 
+  console.log('jres', jobRunEvents);
+
   return (
     <div className="flex flex-col gap-4">
-      <RunTimeline
-        isError={isError}
-        tasks={convertJobRunEventToTask(jobRunEvents)}
-      />
+      <RunTimeline isError={isError} tasks={jobRunEvents} />
       <DataTable
         columns={columns}
         data={jobRunEvents}
@@ -36,71 +35,4 @@ export default function JobRunActivityTable(
       />
     </div>
   );
-}
-
-/*
-
-this is what one object looks like
-{
-    "id": "5",
-    "type": "RetrieveActivityOptions",
-    "startTime": "2024-09-15T06:18:30.274318131Z",
-    "closeTime": "2024-09-15T06:18:30.279649339Z",
-    "tasks": [
-        {
-            "id": "5",
-            "type": "ActivityTaskScheduled",
-            "eventTime": "2024-09-15T06:18:30.271563631Z"
-        },
-        {
-            "id": "6",
-            "type": "ActivityTaskStarted",
-            "eventTime": "2024-09-15T06:18:30.274318131Z"
-        },
-        {
-            "id": "7",
-            "type": "ActivityTaskCompleted",
-            "eventTime": "2024-09-15T06:18:30.279649339Z"
-        }
-    ]
-}
-*/
-
-function convertJobRunEventToTask(jre: JobRunEvent[]): Task[] {
-  console.log('Input job run events:', jre);
-
-  /* 
-  some tasks, like the checkAccoutStatus will get scheudled but never run if the job finishes before it's set to run, and as a result, the JobRunEvent doesn't return a closeTime, so we need to filter these out so they don't continue to run in the table
-  */
-
-  const tasks = jre
-    .filter(
-      (event) => event.closeTime !== undefined && event.closeTime !== null
-    )
-    .flatMap((event: JobRunEvent, eventIndex: number): Task[] => {
-      const mainTask: Task = {
-        id: `event-${eventIndex}`,
-        name: event.type,
-        start: convertTimestampToDate(event.startTime),
-        end: convertTimestampToDate(event.closeTime),
-      };
-
-      return [mainTask];
-    });
-
-  console.log('Converted tasks:', tasks);
-  return tasks;
-}
-
-function convertTimestampToDate(
-  timestamp: { seconds: bigint; nanos: number } | undefined
-): Date {
-  if (!timestamp) return new Date();
-
-  const millisecondsFromSeconds = Number(timestamp.seconds) * 1000;
-  const millisecondsFromNanos = timestamp.nanos / 1_000_000;
-
-  const totalMilliseconds = millisecondsFromSeconds + millisecondsFromNanos;
-
-  return new Date(totalMilliseconds);
 }
--- frontend/apps/web/components/RunTImeline/RunTimeline.tsx
+++ frontend/apps/web/components/RunTImeline/RunTimeline.tsx
@@ -5,21 +5,21 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { cn } from '@/libs/utils';
-import { format, formatDuration, intervalToDuration } from 'date-fns';
+import { Timestamp } from '@bufbuild/protobuf';
+import { JobRunEvent } from '@neosync/sdk';
+import {
+  addMilliseconds,
+  format,
+  formatDuration,
+  intervalToDuration,
+} from 'date-fns';
 import { ReactElement, useMemo, useState } from 'react';
 import { Badge } from '../ui/badge';
 
 interface Props {
   isError: boolean;
-  tasks: Task[];
-  onTaskClick?: (task: Task) => void;
-}
-export interface Task {
-  id: string;
-  name: string;
-  start: Date;
-  end: Date;
-  dependencies?: string[];
+  tasks: JobRunEvent[];
+  onTaskClick?: (task: JobRunEvent) => void;
 }
 
 export default function RunTimeline(props: Props): ReactElement {
@@ -27,14 +27,27 @@ export default function RunTimeline(props: Props): ReactElement {
 
   const [hoveredTask, setHoveredTask] = useState<string | null>(null);
 
-  const formatFullDate = (date: Date) => {
-    return format(date, 'MM/dd/yyyy HH:mm:ss:SSS');
+  const formatFullDate = (date: Timestamp | Date | undefined) => {
+    if (!date) return 'N/A';
+
+    if (date instanceof Timestamp) {
+      return format(convertTimestampToDate(date), 'MM/dd/yyyy HH:mm:ss:SSS');
+    }
+
+    if (date instanceof Date) {
+      return format(date, 'MM/dd/yyyy HH:mm:ss:SSS');
+    }
   };
 
   const formatDate = (date: Date) => format(date, 'MM/dd/yyyy');
   const formatTime = (date: Date) => format(date, 'HH:mm:ss:SSS');
 
-  const formatTaskDuration = (start: Date, end: Date) => {
+  const formatTaskDuration = (
+    s: Timestamp | undefined,
+    end: Date | undefined
+  ) => {
+    if (!s || !end) return 'N/A';
+    const start = convertTimestampToDate(s);
     const duration = intervalToDuration({ start, end });
     const milliseconds = end.getTime() - start.getTime();
     const millis = milliseconds % 1000;
@@ -51,33 +64,48 @@ export default function RunTimeline(props: Props): ReactElement {
 
     return `${formattedDuration}${millis > 0 ? `, ${millis} ms` : ''}`;
   };
-  const { timelineStart, totalDuration, timeLabels } = useMemo(() => {
-    // find the first start time
-    const start = new Date(Math.min(...tasks.map((t) => t.start.getTime())));
-    // find the last end time
-    const end = new Date(Math.max(...tasks.map((t) => t.end.getTime())));
-    // find duration
-    const duration = end.getTime() - start.getTime();
+  const { timelineStart, timelineEnd, totalDuration, timeLabels } =
+    useMemo(() => {
+      const start = new Date(
+        Math.min(
+          ...tasks.map((t) => convertTimestampToDate(t.startTime).getTime())
+        )
+      );
+      const end = new Date(
+        Math.max(
+          ...tasks.map((t) => {
+            const errorDate = getCloseOrErrorDate(t);
+            return Math.max(
+              errorDate.getTime(),
+              convertTimestampToDate(t.closeTime || t.startTime).getTime()
+            );
+          })
+        )
+      );
 
-    // add some padding to the start and end to get an adjusted duration to get everything into view
-    const adjustedStart = new Date(start.getTime() - duration * 0.05);
-    const adjustedEnd = new Date(end.getTime() + duration * 0.05);
-    const adjustedDuration = adjustedEnd.getTime() - adjustedStart.getTime();
+      let duration = end.getTime() - start.getTime();
 
-    const labelCount = 6;
-    const labels: Date[] = Array.from(
-      { length: labelCount },
-      (_, i) =>
-        new Date(adjustedStart.getTime() + (adjustedDuration * i) / labelCount)
-    );
+      // Add padding, but limit it to a maximum of 100ms on each side
+      const padding = Math.min(duration * 0.1, 100);
+      const adjustedStart = new Date(start.getTime() - padding);
+      const adjustedEnd = new Date(end.getTime() + padding);
+      const adjustedDuration = adjustedEnd.getTime() - adjustedStart.getTime();
 
-    return {
-      timelineStart: adjustedStart,
-      timelineEnd: adjustedEnd,
-      totalDuration: adjustedDuration,
-      timeLabels: labels,
-    };
-  }, [tasks]);
+      const labelCount = 5;
+      const labels: Date[] = Array.from({ length: labelCount }, (_, i) =>
+        addMilliseconds(
+          adjustedStart,
+          (adjustedDuration * i) / (labelCount - 1)
+        )
+      );
+
+      return {
+        timelineStart: adjustedStart,
+        timelineEnd: adjustedEnd,
+        totalDuration: adjustedDuration,
+        timeLabels: labels,
+      };
+    }, [tasks]);
 
   // calculates where in the timeline axis something should be relative to the total duration
   const getPositionPercentage = (time: Date) => {
@@ -85,12 +113,15 @@ export default function RunTimeline(props: Props): ReactElement {
   };
 
   console.log('tasks', tasks);
+  console.log('isError', isError);
+
+  // TODO: only show
   return (
     <div
-      className="w-full relative border border-gray-400 dark:border-gray-700 rounded overflow-hidden max-h-[800px]"
+      className="w-full relative border border-gray-400 dark:border-gray-700 rounded overflow-hidden  max-h-[800px]"
       // style={{ height: `${tasks.length * 40 + 200}px` }}
     >
-      <div className="flex flex-row h-full">
+      <div className="flex flex-row h-full w-full">
         <div className="w-1/6">
           <div className="sticky top-0 h-14 bg-gray-200 dark:bg-gray-800 z-10 px-6 border-b border-gray-300 dark:border-gray-700" />
           <div className="border-r border-gray-300 dark:border-gray-700 flex flex-col h-full text-sm ">
@@ -105,7 +136,7 @@ export default function RunTimeline(props: Props): ReactElement {
                       'border-b border-gray-300 dark:border-gray-700'
                   )}
                 >
-                  <div>{task.name}</div>
+                  <div>{task.type}</div>
                 </div>
               );
             })}
@@ -127,9 +158,15 @@ export default function RunTimeline(props: Props): ReactElement {
             />
           ))}
           {tasks.map((task, index) => {
-            const left = getPositionPercentage(task.start);
-            const width = getPositionPercentage(task.end) - left;
+            const left = getPositionPercentage(
+              convertTimestampToDate(task.startTime)
+            );
 
+            const endTime = getCloseOrErrorDate(task);
+            const width = getPositionPercentage(endTime) - left;
+            const errorTask = task.tasks.find((item) => item.error);
+
+            // TODO: only highlight in red the task that failed
             return (
               <div className="flex flex-row">
                 <TooltipProvider delayDuration={100}>
@@ -146,12 +183,12 @@ export default function RunTimeline(props: Props): ReactElement {
                           width: `${width}%`,
                           top: `${index * 40 + 60}px`,
                         }}
-                        onClick={() => onTaskClick?.(task)}
+                        // onClick={() => onTaskClick?.(task)}
                       >
                         <div className="px-2 text-gray-900 dark:text-gray-200 text-sm w-full flex flex-row gap-4 items-center">
-                          <p>{task.name}</p>
+                          <p>{task.type}</p>
                           <span className="text-xs bg-black text-white px-1 py-0.5 rounded text-nowrap">
-                            {formatTaskDuration(task.start, task.end)}
+                            {formatTaskDuration(task.startTime, endTime)}
                           </span>
                         </div>
                       </div>
@@ -160,13 +197,13 @@ export default function RunTimeline(props: Props): ReactElement {
                       <div>
                         <strong>Start:</strong>{' '}
                         <Badge variant="default">
-                          {formatFullDate(task.start)}
+                          {formatFullDate(task.startTime)}
                         </Badge>
                       </div>
                       <div>
                         <strong>Finish:</strong>{' '}
                         <Badge variant="default">
-                          {formatFullDate(task.end)}
+                          {formatFullDate(endTime)}
                         </Badge>
                       </div>
                     </TooltipContent>
@@ -190,7 +227,7 @@ export default function RunTimeline(props: Props): ReactElement {
     const { formatDate, getPositionPercentage, timeLabels } = props;
 
     return (
-      <div className="sticky top-0 h-14 border-b border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-800 z-10 px-6">
+      <div className="w-full sticky top-0 h-14 border-b border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-800 z-10 ">
         <div className="relative w-full h-full">
           {timeLabels.map((label, index) => (
             <div
@@ -207,4 +244,25 @@ export default function RunTimeline(props: Props): ReactElement {
       </div>
     );
   }
+}
+
+function convertTimestampToDate(
+  timestamp: { seconds: bigint; nanos: number } | undefined
+): Date {
+  if (!timestamp) return new Date();
+
+  const millisecondsFromSeconds = Number(timestamp.seconds) * 1000;
+  const millisecondsFromNanos = timestamp.nanos / 1_000_000;
+
+  const totalMilliseconds = millisecondsFromSeconds + millisecondsFromNanos;
+
+  return new Date(totalMilliseconds);
+}
+
+function getCloseOrErrorDate(task: JobRunEvent): Date {
+  const errorTask = task.tasks.find((item) => item.error);
+  const errorTime = errorTask ? errorTask.eventTime : undefined;
+  return errorTime
+    ? convertTimestampToDate(errorTime)
+    : convertTimestampToDate(task.closeTime);
 }
