77acacb35bedc4bdebdf2afd09904eefd282ad3b
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-10-02 16:23:56 -0700

    removes unneeded bento import

--- worker/pkg/workflows/datasync/activities/sync/activity.go
+++ worker/pkg/workflows/datasync/activities/sync/activity.go
@@ -16,7 +16,6 @@ import (
 	_ "github.com/warpstreamlabs/bento/public/components/pure"
 	_ "github.com/warpstreamlabs/bento/public/components/pure/extended"
 	_ "github.com/warpstreamlabs/bento/public/components/redis"
-	_ "github.com/warpstreamlabs/bento/public/components/sql"
 
 	neosynclogger "github.com/nucleuscloud/neosync/backend/pkg/logger"
 	"github.com/nucleuscloud/neosync/backend/pkg/metrics"
