aba1147af670bae26ede16ec624f00d7f42dc408
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2024-10-10 16:59:35 -0700

    NEOS-1513: Update upgrade and warning prompts (#2784)

--- backend/services/mgmt/v1alpha1/integration_tests/user-account-service_integration_test.go
+++ backend/services/mgmt/v1alpha1/integration_tests/user-account-service_integration_test.go
@@ -501,6 +501,7 @@ func (s *IntegrationTestSuite) Test_UserAccountService_IsAccountStatusValid_Neos
 
 	require.True(s.T(), resp.Msg.GetIsValid())
 	require.Empty(s.T(), resp.Msg.GetReason())
+	require.Equal(s.T(), mgmtv1alpha1.AccountStatus_ACCOUNT_STATUS_REASON_UNSPECIFIED, resp.Msg.GetAccountStatus())
 	require.True(s.T(), resp.Msg.GetShouldPoll())
 }
 
@@ -526,6 +527,9 @@ func (s *IntegrationTestSuite) Test_UserAccountService_IsAccountStatusValid_Neos
 
 	require.False(s.T(), resp.Msg.GetIsValid())
 	require.NotEmpty(s.T(), resp.Msg.GetReason())
+	require.Equal(s.T(), mgmtv1alpha1.AccountStatus_ACCOUNT_STATUS_EXCEEDS_ALLOWED_LIMIT, resp.Msg.GetAccountStatus())
+	require.Equal(s.T(), uint64(100), resp.Msg.GetUsedRecordCount())
+	require.Equal(s.T(), uint64(100), resp.Msg.GetAllowedRecordCount())
 	require.False(s.T(), resp.Msg.GetShouldPoll())
 }
 
@@ -554,6 +558,9 @@ func (s *IntegrationTestSuite) Test_UserAccountService_IsAccountStatusValid_Neos
 
 		require.False(s.T(), resp.Msg.GetIsValid())
 		require.NotEmpty(s.T(), resp.Msg.GetReason())
+		require.Equal(s.T(), mgmtv1alpha1.AccountStatus_ACCOUNT_STATUS_REQUESTED_EXCEEDS_LIMIT, resp.Msg.GetAccountStatus())
+		require.Equal(s.T(), uint64(50), resp.Msg.GetUsedRecordCount())
+		require.Equal(s.T(), uint64(100), resp.Msg.GetAllowedRecordCount())
 		require.False(s.T(), resp.Msg.GetShouldPoll())
 	})
 	t.Run("under the limit", func(t *testing.T) {
@@ -565,6 +572,7 @@ func (s *IntegrationTestSuite) Test_UserAccountService_IsAccountStatusValid_Neos
 
 		require.True(s.T(), resp.Msg.GetIsValid())
 		require.Empty(s.T(), resp.Msg.GetReason())
+		require.Equal(s.T(), mgmtv1alpha1.AccountStatus_ACCOUNT_STATUS_REASON_UNSPECIFIED, resp.Msg.GetAccountStatus())
 		require.True(s.T(), resp.Msg.GetShouldPoll())
 	})
 }
@@ -587,6 +595,7 @@ func (s *IntegrationTestSuite) Test_UserAccountService_IsAccountStatusValid_Neos
 
 		assert.True(s.T(), resp.Msg.GetIsValid())
 		assert.Empty(s.T(), resp.Msg.GetReason())
+		require.Equal(s.T(), mgmtv1alpha1.AccountStatus_ACCOUNT_STATUS_REASON_UNSPECIFIED, resp.Msg.GetAccountStatus())
 		require.False(s.T(), resp.Msg.GetShouldPoll())
 	})
 	t.Run("inactive", func(t *testing.T) {
@@ -603,6 +612,7 @@ func (s *IntegrationTestSuite) Test_UserAccountService_IsAccountStatusValid_Neos
 
 		assert.False(s.T(), resp.Msg.GetIsValid())
 		assert.NotEmpty(s.T(), resp.Msg.GetReason())
+		require.Equal(s.T(), mgmtv1alpha1.AccountStatus_ACCOUNT_STATUS_ACCOUNT_IN_EXPIRED_STATE, resp.Msg.GetAccountStatus())
 		require.False(s.T(), resp.Msg.GetShouldPoll())
 	})
 }
