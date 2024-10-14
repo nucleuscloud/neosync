e87daa013badb06949e060e49fd3d24cd766184e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-10-14 10:00:52 -0700

    adds unit tests for transform pii text

--- worker/pkg/benthos/transformers/transform_pii_text_test.go
+++ worker/pkg/benthos/transformers/transform_pii_text_test.go
@@ -44,3 +44,121 @@ func Test_TransformPiiText(t *testing.T) {
 	require.True(t, ok)
 	require.Equal(t, mockText, value)
 }
+
+func Test_getDefaultAnonymizer(t *testing.T) {
+	t.Run("redact", func(t *testing.T) {
+		actual, err := getDefaultAnonymizer(&mgmtv1alpha1.PiiAnonymizer{
+			Config: &mgmtv1alpha1.PiiAnonymizer_Redact_{
+				Redact: &mgmtv1alpha1.PiiAnonymizer_Redact{},
+			},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, actual)
+	})
+
+	t.Run("replace", func(t *testing.T) {
+		newval := "newval"
+		actual, err := getDefaultAnonymizer(&mgmtv1alpha1.PiiAnonymizer{
+			Config: &mgmtv1alpha1.PiiAnonymizer_Replace_{
+				Replace: &mgmtv1alpha1.PiiAnonymizer_Replace{
+					Value: &newval,
+				},
+			},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, actual)
+	})
+
+	t.Run("hash", func(t *testing.T) {
+		sha256 := mgmtv1alpha1.PiiAnonymizer_Hash_HASH_TYPE_SHA512
+		actual, err := getDefaultAnonymizer(&mgmtv1alpha1.PiiAnonymizer{
+			Config: &mgmtv1alpha1.PiiAnonymizer_Hash_{
+				Hash: &mgmtv1alpha1.PiiAnonymizer_Hash{
+					Algo: &sha256,
+				},
+			},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, actual)
+	})
+
+	t.Run("mask", func(t *testing.T) {
+		maskingChar := "*"
+		charsTomask := int32(5)
+		fromend := false
+		actual, err := getDefaultAnonymizer(&mgmtv1alpha1.PiiAnonymizer{
+			Config: &mgmtv1alpha1.PiiAnonymizer_Mask_{
+				Mask: &mgmtv1alpha1.PiiAnonymizer_Mask{
+					MaskingChar: &maskingChar,
+					CharsToMask: &charsTomask,
+					FromEnd:     &fromend,
+				},
+			},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, actual)
+	})
+
+	t.Run("default", func(t *testing.T) {
+		actual, err := getDefaultAnonymizer(nil)
+		require.NoError(t, err)
+		require.NotNil(t, actual)
+	})
+}
+
+func Test_toPresidioHashType(t *testing.T) {
+	t.Run("md5", func(t *testing.T) {
+		actual := toPresidioHashType(mgmtv1alpha1.PiiAnonymizer_Hash_HASH_TYPE_MD5)
+		require.Equal(t, presidioapi.Md5, actual)
+	})
+
+	t.Run("sha256", func(t *testing.T) {
+		actual := toPresidioHashType(mgmtv1alpha1.PiiAnonymizer_Hash_HASH_TYPE_SHA256)
+		require.Equal(t, presidioapi.Sha256, actual)
+	})
+
+	t.Run("sha512", func(t *testing.T) {
+		actual := toPresidioHashType(mgmtv1alpha1.PiiAnonymizer_Hash_HASH_TYPE_SHA512)
+		require.Equal(t, presidioapi.Sha512, actual)
+	})
+
+	t.Run("default", func(t *testing.T) {
+		actual := toPresidioHashType(mgmtv1alpha1.PiiAnonymizer_Hash_HASH_TYPE_UNSPECIFIED)
+		require.Equal(t, presidioapi.Md5, actual)
+	})
+}
+
+func Test_handleAnonRespErr(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		err := handleAnonRespErr(nil)
+		require.Error(t, err)
+	})
+
+	t.Run("400", func(t *testing.T) {
+		errtxt := "400 err"
+		err := handleAnonRespErr(&presidioapi.PostAnonymizeResponse{
+			JSON400: &presidioapi.N400BadRequest{Error: &errtxt},
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("422", func(t *testing.T) {
+		errtxt := "422 err"
+		err := handleAnonRespErr(&presidioapi.PostAnonymizeResponse{
+			JSON422: &presidioapi.N422UnprocessableEntity{Error: &errtxt},
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("nil 200", func(t *testing.T) {
+		err := handleAnonRespErr(&presidioapi.PostAnonymizeResponse{})
+		require.Error(t, err)
+	})
+
+	t.Run("valid 200", func(t *testing.T) {
+		err := handleAnonRespErr(&presidioapi.PostAnonymizeResponse{
+			JSON200: &presidioapi.AnonymizeResponse{},
+		})
+		require.NoError(t, err)
+	})
+}
