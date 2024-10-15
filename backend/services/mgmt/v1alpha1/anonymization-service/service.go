5773e8aedf86bb48b7b739ec76857ce0e046c466
Alisha <alisha@nucleuscloud.com>
2024-10-15 10:13:42 -0700

    clean up

--- backend/services/mgmt/v1alpha1/anonymization-service/service.go
+++ backend/services/mgmt/v1alpha1/anonymization-service/service.go
@@ -2,42 +2,35 @@ package v1alpha_anonymizationservice
 
 import (
 	"github.com/nucleuscloud/neosync/backend/gen/go/protos/mgmt/v1alpha1/mgmtv1alpha1connect"
-	"go.opentelemetry.io/otel/metric"
 	presidioapi "github.com/nucleuscloud/neosync/internal/ee/presidio"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type Service struct {
 	cfg                *Config
 	meter              metric.Meter // optional
 	useraccountService mgmtv1alpha1connect.UserAccountServiceClient
-	analyze   presidioapi.AnalyzeInterface
-	anonymize presidioapi.AnonymizeInterface
-	
+	analyze            presidioapi.AnalyzeInterface
+	anonymize          presidioapi.AnonymizeInterface
 }
 
 type Config struct {
-	IsAuthEnabled bool
+	IsAuthEnabled     bool
 	IsPresidioEnabled bool
-}
-
-type Service struct {
-	cfg       *Config
-	analyze   presidioapi.AnalyzeInterface
-	anonymize presidioapi.AnonymizeInterface
 }
 
 func New(
 	cfg *Config,
 	meter metric.Meter,
 	useraccountService mgmtv1alpha1connect.UserAccountServiceClient,
-	analyzeclient   presidioapi.AnalyzeInterface
-	anonymizeclient presidioapi.AnonymizeInterface
+	analyzeclient presidioapi.AnalyzeInterface,
+	anonymizeclient presidioapi.AnonymizeInterface,
 ) *Service {
 	return &Service{
 		cfg:                cfg,
 		meter:              meter,
 		useraccountService: useraccountService,
-		analyze:   analyzeclient,
-		anonymize: anonymizeclient,
+		analyze:            analyzeclient,
+		anonymize:          anonymizeclient,
 	}
 }
