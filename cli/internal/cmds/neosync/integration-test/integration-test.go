fcedc43aadea1bcd3636a57c29476c34c2567fcb
Alisha <alisha@nucleuscloud.com>
2024-10-21 13:52:38 -0700

    cli integration test setup

---
+++ cli/internal/cmds/neosync/integration-test/integration-test.go
@@ -0,0 +1,78 @@
+package integrationtests_test
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	tcneosyncapi "github.com/nucleuscloud/neosync/backend/pkg/integration-test"
+	tcpostgres "github.com/nucleuscloud/neosync/internal/testcontainers/postgres"
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/sync/errgroup"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	ctx context.Context
+
+	neosyncApi *tcneosyncapi.NeosyncApiTestClient
+
+	postgres *tcpostgres.PostgresTestContainer
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.ctx = context.Background()
+
+	var neosyncApiTest *tcneosyncapi.NeosyncApiTestClient
+	var postgresTest *tcpostgres.PostgresTestContainer
+
+	errgrp := errgroup.Group{}
+	errgrp.Go(func() error {
+		p, err := tcpostgres.NewPostgresTestContainer(s.ctx)
+		if err != nil {
+			return err
+		}
+		postgresTest = p
+		return nil
+	})
+
+	errgrp.Go(func() error {
+		api := tcneosyncapi.NewNeosyncApiTestClient(s.ctx)
+		neosyncApiTest = api
+		return nil
+	})
+
+	err := errgrp.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	s.postgres = postgresTest
+	s.neosyncApi = neosyncApiTest
+}
+
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down test suite")
+	err := s.postgres.TearDown()
+	if err != nil {
+		panic(err)
+	}
+	err = s.neosyncApi.TearDown()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestIntegrationTestSuite(t *testing.T) {
+	evkey := "INTEGRATION_TESTS_ENABLED"
+	shouldRun := os.Getenv(evkey)
+	if shouldRun != "1" {
+		slog.Warn(fmt.Sprintf("skipping integration tests, set %s=1 to enable", evkey))
+		return
+	}
+	suite.Run(t, new(IntegrationTestSuite))
+}
