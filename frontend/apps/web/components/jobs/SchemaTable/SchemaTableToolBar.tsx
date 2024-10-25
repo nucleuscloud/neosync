fcaf85559b118e3ca3c76128e96d0c98af19532b
Evis Drenova <evis@usenucleus.cloud>
2024-10-24 22:19:40 -0700

    button and initial logic to export mappings to json file

--- frontend/apps/web/components/jobs/SchemaTable/SchemaTableToolBar.tsx
+++ frontend/apps/web/components/jobs/SchemaTable/SchemaTableToolBar.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { Row, Table } from '@tanstack/react-table';
-
 import { SingleTableSchemaFormValues } from '@/app/(mgmt)/[account]/new/job/job-form-validations';
 import EditTransformerOptions from '@/app/(mgmt)/[account]/transformers/EditTransformerOptions';
 import ButtonText from '@/components/ButtonText';
@@ -16,6 +14,7 @@ import {
   FormItem,
   FormMessage,
 } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { cn } from '@/libs/utils';
 import { isSystemTransformer, Transformer } from '@/shared/transformers';
 import {
@@ -30,6 +29,7 @@ import {
   SchemaFormValues,
 } from '@/yup-validations/jobs';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Editor } from '@monaco-editor/react';
 import {
   GenerateDefault,
   JobMappingTransformer,
@@ -40,8 +40,11 @@ import {
   UserDefinedTransformer,
 } from '@neosync/sdk';
 import { CheckIcon, Cross2Icon } from '@radix-ui/react-icons';
-import { useState } from 'react';
+import { Row, Table } from '@tanstack/react-table';
+import { useTheme } from 'next-themes';
+import { ReactElement, useState } from 'react';
 import { useForm, useFormContext } from 'react-hook-form';
+import { AiOutlineExport, AiOutlineImport } from 'react-icons/ai';
 import { fromRowDataToColKey, getTransformerFilter } from './SchemaColumns';
 import { Row as RowData } from './SchemaPageTable';
 import { SchemaTableViewOptions } from './SchemaTableViewOptions';
@@ -54,6 +57,7 @@ interface DataTableToolbarProps<TData> {
   transformerHandler: TransformerHandler;
   constraintHandler: SchemaConstraintHandler;
   jobType: JobType;
+  data: TData[];
 }
 
 export function SchemaTableToolbar<TData>({
@@ -61,7 +65,11 @@ export function SchemaTableToolbar<TData>({
   transformerHandler,
   constraintHandler,
   jobType,
+  data,
 }: DataTableToolbarProps<TData>) {
+  const [isExporting, setIsExporting] = useState<boolean>(false);
+  const [progress, setProgress] = useState(0);
+
   const isFiltered = table.getState().columnFilters.length > 0;
   const hasSelectedRows = Object.values(table.getState().rowSelection).some(
     (value) => value
@@ -206,76 +214,173 @@ export function SchemaTableToolbar<TData>({
             </Button>
           )}
           {jobType === 'sync' && (
-            <ConfirmationDialog
-              trigger={
-                <Button
-                  variant="outline"
-                  type="button"
-                  disabled={form.getValues('mappings').length == 0}
-                >
-                  <ButtonText text="Apply Default Transformers" />
-                </Button>
-              }
-              headerText="Apply Default Transformers?"
-              description="This setting will apply the 'Passthrough' Transformer to every column that is not Generated, while applying the 'Use Column Default' Transformer to all Generated (non-Identity)columns."
-              body={handleAlertDescriptionBody()}
-              containerClassName="max-w-xl"
-              onConfirm={() => {
-                const formMappings = form.getValues('mappings');
-                const defaultTransformerValues =
-                  defaultTransformerForm.getValues();
-                formMappings.forEach((fm, idx) => {
-                  // skips setting the default transformer if the user has already set the transformer
-                  if (
-                    fm.transformer.source != 0 &&
-                    !defaultTransformerValues.overrideTransformers
-                  ) {
-                    return;
-                  } else {
-                    const colkey = {
-                      schema: fm.schema,
-                      table: fm.table,
-                      column: fm.column,
-                    };
-                    const isGenerated =
-                      constraintHandler.getIsGenerated(colkey);
-                    const identityType =
-                      constraintHandler.getIdentityType(colkey);
-                    const newJm =
-                      isGenerated && !identityType
-                        ? new JobMappingTransformer({
-                            source: TransformerSource.GENERATE_DEFAULT,
-                            config: new TransformerConfig({
-                              config: {
-                                case: 'generateDefaultConfig',
-                                value: new GenerateDefault(),
-                              },
-                            }),
-                          })
-                        : new JobMappingTransformer({
-                            source: TransformerSource.PASSTHROUGH,
-                            config: new TransformerConfig({
-                              config: {
-                                case: 'passthroughConfig',
-                                value: new Passthrough(),
-                              },
-                            }),
-                          });
+            <div className="flex flex-row items-center gap-2">
+              <ConfirmationDialog
+                trigger={
+                  <Button
+                    variant="outline"
+                    type="button"
+                    disabled={form.getValues('mappings').length == 0}
+                  >
+                    <div className="flex flex-row items-center gap-2">
+                      <AiOutlineImport />
+                      <ButtonText text="Import" />
+                    </div>
+                  </Button>
+                }
+                headerText="Apply Default Transformers?"
+                description="This setting will apply the 'Passthrough' Transformer to every column that is not Generated, while applying the 'Use Column Default' Transformer to all Generated (non-Identity)columns."
+                body={<ImportMappings />}
+                containerClassName="max-w-xl"
+                onConfirm={() => {}}
+                buttonText="Import"
+              />
+              <ConfirmationDialog
+                trigger={
+                  <Button
+                    variant="outline"
+                    type="button"
+                    disabled={form.getValues('mappings').length == 0}
+                  >
+                    <div className="flex flex-row items-center gap-2">
+                      <AiOutlineExport />
+                      <ButtonText text="Export" />
+                    </div>
+                  </Button>
+                }
+                headerText="Export Mappings"
+                description="Export your mappings to a JSON file"
+                body={
+                  <ExportMappings data={data.slice(0, 2)} progress={progress} />
+                }
+                containerClassName="max-w-xl"
+                onConfirm={async () => {
+                  try {
+                    setIsExporting(true);
+                    setProgress(0);
 
-                    form.setValue(
-                      `mappings.${idx}.transformer`,
-                      convertJobMappingTransformerToForm(newJm),
-                      {
-                        shouldDirty: true,
-                        shouldTouch: true,
-                        shouldValidate: false,
-                      }
-                    );
+                    // Process data in chunks
+                    const chunkSize = 100; // number of elements in array to chunk
+                    const totalChunks = Math.ceil(data.length / chunkSize);
+                    let processedChunks = 0;
+                    let chunks = [];
+
+                    // process chunks
+                    for (let i = 0; i < data.length; i += chunkSize) {
+                      const chunk = data.slice(i, i + chunkSize);
+                      // spread chunks into array
+                      chunks.push(...chunk);
+                      processedChunks++;
+                      setProgress(
+                        Math.round((processedChunks / totalChunks) * 100)
+                      );
+
+                      // update progress bar and UI
+                      await new Promise((resolve) => setTimeout(resolve, 0));
+                    }
+
+                    // create and download file
+                    const blob = new Blob([JSON.stringify(chunks)], {
+                      type: 'application/json',
+                    });
+
+                    // creates a url that the window will programmatically click to download the file
+                    const url = window.URL.createObjectURL(blob);
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.download = 'job_mapping';
+
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                    window.URL.revokeObjectURL(url);
+
+                    // Reset after a brief delay
+                    setTimeout(() => {
+                      setIsExporting(false);
+                      setProgress(0);
+                    }, 1000);
+                  } catch (error) {
+                    console.error('Export failed:', error);
+                    // TODO: handle on Error state
+                    // onError?.(error as Error);
+                    setIsExporting(false);
+                    setProgress(0);
                   }
-                });
-                form.trigger('mappings'); // trigger validation after bulk updating the selected form options
-              }}
-            />
+                }}
+                buttonText="Export"
+              />
+              <ConfirmationDialog
+                trigger={
+                  <Button
+                    variant="outline"
+                    type="button"
+                    disabled={form.getValues('mappings').length == 0}
+                  >
+                    <ButtonText text="Apply Default Transformers" />
+                  </Button>
+                }
+                headerText="Apply Default Transformers?"
+                description="This setting will apply the 'Passthrough' Transformer to every column that is not Generated, while applying the 'Use Column Default' Transformer to all Generated (non-Identity)columns."
+                body={handleAlertDescriptionBody()}
+                containerClassName="max-w-xl"
+                onConfirm={() => {
+                  const formMappings = form.getValues('mappings');
+                  const defaultTransformerValues =
+                    defaultTransformerForm.getValues();
+                  formMappings.forEach((fm, idx) => {
+                    // skips setting the default transformer if the user has already set the transformer
+                    if (
+                      fm.transformer.source != 0 &&
+                      !defaultTransformerValues.overrideTransformers
+                    ) {
+                      return;
+                    } else {
+                      const colkey = {
+                        schema: fm.schema,
+                        table: fm.table,
+                        column: fm.column,
+                      };
+                      const isGenerated =
+                        constraintHandler.getIsGenerated(colkey);
+                      const identityType =
+                        constraintHandler.getIdentityType(colkey);
+                      const newJm =
+                        isGenerated && !identityType
+                          ? new JobMappingTransformer({
+                              source: TransformerSource.GENERATE_DEFAULT,
+                              config: new TransformerConfig({
+                                config: {
+                                  case: 'generateDefaultConfig',
+                                  value: new GenerateDefault(),
+                                },
+                              }),
+                            })
+                          : new JobMappingTransformer({
+                              source: TransformerSource.PASSTHROUGH,
+                              config: new TransformerConfig({
+                                config: {
+                                  case: 'passthroughConfig',
+                                  value: new Passthrough(),
+                                },
+                              }),
+                            });
+
+                      form.setValue(
+                        `mappings.${idx}.transformer`,
+                        convertJobMappingTransformerToForm(newJm),
+                        {
+                          shouldDirty: true,
+                          shouldTouch: true,
+                          shouldValidate: false,
+                        }
+                      );
+                    }
+                  });
+                  form.trigger('mappings'); // trigger validation after bulk updating the selected form options
+                }}
+              />
+            </div>
           )}
           <SchemaTableViewOptions table={table} />
         </div>
@@ -399,4 +504,73 @@ function findCommonUserDefinedIds(
   }
 
   return commonElements;
+}
+
+interface ExportMappingsProps<TData> {
+  data: TData[];
+  progress: number;
+}
+
+function ExportMappings<TData>(
+  props: ExportMappingsProps<TData>
+): ReactElement {
+  const { data, progress } = props;
+
+  return (
+    <div className="flex flex-col gap-10 justify-start items-start">
+      {progress > 0 ? (
+        <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+          <div
+            className="h-full bg-blue-500 transition-all duration-300 ease-out"
+            style={{ width: `${progress}%` }}
+          />
+        </div>
+      ) : (
+        <ExportFilePreview sample={JSON.stringify(data, null, 2)} />
+      )}
+    </div>
+  );
+}
+interface ExportFilePreview {
+  sample: string;
+}
+
+function ExportFilePreview(props: ExportFilePreview): ReactElement {
+  const { sample } = props;
+
+  const { resolvedTheme } = useTheme();
+  return (
+    <div className="w-full flex flex-col gap-2">
+      <div className="text-sm">Preview</div>
+      <Editor
+        height="200px"
+        width="100%"
+        language="json"
+        value={sample}
+        className="border dark:border-gray-700 rounded-lg overflow-hidden"
+        theme={resolvedTheme === 'dark' ? 'vs-dark' : 'cobalt'}
+        options={{
+          minimap: { enabled: false },
+          readOnly: true,
+          wordWrap: 'on',
+          folding: true,
+          foldingHighlight: true,
+          lineNumbers: 'on',
+        }}
+      />
+    </div>
+  );
+}
+
+function ImportMappings(): ReactElement {
+  const sample = `{schema:'evis', table:'test',transformer:{config:'source'}}'`;
+  return (
+    <div className="flex flex-col gap-10 justify-start items-start">
+      <div className="flex flex-col gap-4">
+        <div className="text-sm">File name</div>
+        <Input value="" placeholder="<job>_<mapping>_export.json" />
+      </div>
+      <ExportFilePreview sample={sample} />
+    </div>
+  );
 }
