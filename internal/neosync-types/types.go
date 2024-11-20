a8745a6c4d1ab54228813692f31c8817d719b352
Alisha <alisha@nucleuscloud.com>
2024-11-20 15:25:10 -0800

    stash

--- internal/neosync-types/types.go
+++ internal/neosync-types/types.go
@@ -1,11 +1,17 @@
 package neosynctypes
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 // scan from source to neosync type
 // value from neosync type to destination
+
+// source is postgres write to S3 pull from S3 write to Postgres in cli sync
+// cli sync source is postgres convert to json bits stream to cli convert to pg types
 
 type NeosyncInterval struct {
 	Microseconds int64 `json:"microseconds"`
@@ -29,4 +35,19 @@ func (n NeosyncInterval) PgxValue() (*pgtype.Interval, error) {
 		Days:         n.Days,
 		Months:       n.Months,
 	}, nil
+}
+
+func (n NeosyncInterval) JsonScan(value any) error {
+	switch v := value.(type) {
+	case []byte:
+		return json.Unmarshal(v, &n)
+	case string:
+		return json.Unmarshal([]byte(v), &n)
+	default:
+		return fmt.Errorf("unsupported type for NeosyncInterval: %T", value)
+	}
+}
+
+func (n NeosyncInterval) JsonValue() ([]byte, error) {
+	return json.Marshal(n)
 }
