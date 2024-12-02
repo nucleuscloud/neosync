060066bde2fc36504126986e16b371133771476a
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-12-02 10:01:35 -0800

    adds session unit tests

--- internal/connection-manager/pool/providers/sql/sql-pool-provider_test.go
+++ internal/connection-manager/pool/providers/sql/sql-pool-provider_test.go
@@ -9,13 +9,3 @@ import (
 func Test_NewConnectionProvider(t *testing.T) {
 	assert.NotNil(t, NewConnectionProvider(nil, nil, nil, nil))
 }
-
-// func Test_newPoolProvider_GetDb(t *testing.T) {
-// 	provider := NewProvider(func(dsn string) (neosync_benthos_sql.SqlDbtx, error) {
-// 		return neosync_benthos_sql.NewMockSqlDbtx(t), nil
-// 	})
-// 	assert.NotNil(t, provider)
-// 	db, err := provider.GetDb("foo", "bar")
-// 	assert.NoError(t, err)
-// 	assert.NotNil(t, db)
-// }
