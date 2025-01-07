5435556d2f6c2b2f4a7f86f1e6f5c6f3f72dee42
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-01-07 13:17:11 -0800

    readds padding in memoized row

--- frontend/apps/web/components/FastTable/MemoizedRow.tsx
+++ frontend/apps/web/components/FastTable/MemoizedRow.tsx
@@ -24,7 +24,7 @@ function InnerRow<TData>(props: Props<TData>): ReactNode {
       {row.getVisibleCells().map((cell) => (
         <td
           key={cell.id}
-          // className={cn(cell.column.columnDef.id != 'isSelected' ? 'px-2' : '')}
+          className="py-2"
           style={{
             minWidth: cell.column.getSize(),
             width: cell.column.columnDef.id != 'isSelected' ? '187px' : '20px',
