d95135dd954b0e441e7f7ca14d8c7189a49c955e
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-01-06 16:28:24 -0800

    updates memoized cell and row to be more readable and typed

--- frontend/apps/web/components/FastTable/MemoizedCell.tsx
+++ frontend/apps/web/components/FastTable/MemoizedCell.tsx
@@ -1,25 +1,35 @@
 import { Cell, flexRender } from '@tanstack/react-table';
-import { memo } from 'react';
+import { memo, ReactNode } from 'react';
 
-const MemoizedCell = memo(
-  <TData,>({ cell }: { cell: Cell<TData, unknown> }) =>
-    flexRender(cell.column.columnDef.cell, cell.getContext()),
-  (prev, next) => {
-    const prevValue = prev.cell.getValue();
-    const nextValue = next.cell.getValue();
+interface Props<TData> {
+  cell: Cell<TData, unknown>;
+}
 
-    if (
-      prev.cell.column.id === 'isSelected' ||
-      prev.cell.column.id === 'actions'
-    ) {
-      // Always re-render checkbox cells as getIsSelected() is always the same for both
-      return false;
-    }
+function InnerCell<TData>(props: Props<TData>): ReactNode {
+  const { cell } = props;
+  return flexRender(cell.column.columnDef.cell, cell.getContext());
+}
 
-    // For other columns, just compare the values
-    return prevValue === nextValue;
+function shouldReRender<TData>(
+  prev: Props<TData>,
+  next: Props<TData>
+): boolean {
+  const prevValue = prev.cell.getValue();
+  const nextValue = next.cell.getValue();
+
+  if (
+    // todo: maybe these should be passed in as props so they are configurable by the caller
+    prev.cell.column.id === 'isSelected' ||
+    prev.cell.column.id === 'actions'
+  ) {
+    // Always re-render checkbox cells as getIsSelected() is always the same for both
+    return false;
   }
-);
-MemoizedCell.displayName = 'MemoizedCell';
 
+  // For other columns, just compare the values
+  return prevValue === nextValue;
+}
+
+const MemoizedCell = memo(InnerCell, shouldReRender);
+MemoizedCell.displayName = 'MemoizedCell';
 export default MemoizedCell;
