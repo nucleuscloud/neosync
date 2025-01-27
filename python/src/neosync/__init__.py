3417eabb9a8657ea6c53d7adbd97b0849beb0c50
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2025-01-27 14:03:26 -0800

    NEOS-1731: Fixes init types to properly handle python type hints (#3196)

--- python/src/neosync/__init__.py
+++ python/src/neosync/__init__.py
@@ -5,13 +5,13 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 # Exports root files so they are importable from just "neosync" without having to specify the file name
-from client import Neosync as Neosync
+from neosync.client import Neosync as Neosync
 
-from mgmt.v1alpha1.anonymization_pb2 import *
-from mgmt.v1alpha1.api_key_pb2 import *
-from mgmt.v1alpha1.connection_data_pb2 import *
-from mgmt.v1alpha1.connection_pb2 import *
-from mgmt.v1alpha1.job_pb2 import *
-from mgmt.v1alpha1.metrics_pb2 import *
-from mgmt.v1alpha1.transformer_pb2 import *
-from mgmt.v1alpha1.user_account_pb2 import *
+from neosync.mgmt.v1alpha1.anonymization_pb2 import *
+from neosync.mgmt.v1alpha1.api_key_pb2 import *
+from neosync.mgmt.v1alpha1.connection_data_pb2 import *
+from neosync.mgmt.v1alpha1.connection_pb2 import *
+from neosync.mgmt.v1alpha1.job_pb2 import *
+from neosync.mgmt.v1alpha1.metrics_pb2 import *
+from neosync.mgmt.v1alpha1.transformer_pb2 import *
+from neosync.mgmt.v1alpha1.user_account_pb2 import *
