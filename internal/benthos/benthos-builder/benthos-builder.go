cc596d275578ecf30fe3a20f01159c0e26486600
Alisha <alisha@nucleuscloud.com>
2025-02-13 12:51:00 -0800

    add proto validation-

--- internal/benthos/benthos-builder/benthos-builder.go
+++ internal/benthos/benthos-builder/benthos-builder.go
@@ -228,19 +228,15 @@ func NewWorkerBenthosConfigManager(
 	config *WorkerBenthosConfig,
 ) (*BenthosConfigManager, error) {
 	provider := NewBuilderProvider(config.Logger)
-	if config.SourceConnection == nil {
-		return nil, fmt.Errorf("job must have a source connection")
+	err := validateBenthosConfig(
+		config.Job,
+		config.SourceConnection,
+		config.DestinationConnections,
+	)
+	if err != nil {
+		return nil, err
 	}
-	if len(config.DestinationConnections) == 0 {
-		return nil, fmt.Errorf("job must have at least one destination connection")
-	}
-	if len(config.Job.GetDestinations()) == 0 {
-		return nil, fmt.Errorf("job must have at least one destination")
-	}
-	if config.Job.GetSource() == nil {
-		return nil, fmt.Errorf("job must have a source")
-	}
-	err := provider.registerStandardBuilders(
+	err = provider.registerStandardBuilders(
 		config.Job,
 		config.SourceConnection,
 		config.DestinationConnections,
@@ -267,6 +263,26 @@ func NewWorkerBenthosConfigManager(
 	}, nil
 }
 
+func validateBenthosConfig(
+	job *mgmtv1alpha1.Job,
+	sourceConnection *mgmtv1alpha1.Connection,
+	destinationConnections []*mgmtv1alpha1.Connection,
+) error {
+	if sourceConnection == nil {
+		return fmt.Errorf("data sync job must have a source connection")
+	}
+	if len(destinationConnections) == 0 {
+		return fmt.Errorf("data sync job must have at least one destination connection")
+	}
+	if len(job.GetDestinations()) == 0 {
+		return fmt.Errorf("data sync job must have at least one destination")
+	}
+	if job.GetSource() == nil {
+		return fmt.Errorf("data sync job must have a source")
+	}
+	return nil
+}
+
 // Manages all necessary configuration parameters for creating
 // a CLI-based Benthos configuration manager
 type CliBenthosConfig struct {
@@ -289,8 +305,16 @@ type CliBenthosConfig struct {
 func NewCliBenthosConfigManager(
 	config *CliBenthosConfig,
 ) (*BenthosConfigManager, error) {
+	err := validateBenthosConfig(
+		config.Job,
+		config.SourceConnection,
+		[]*mgmtv1alpha1.Connection{config.DestinationConnection},
+	)
+	if err != nil {
+		return nil, err
+	}
 	destinationProvider := NewBuilderProvider(config.Logger)
-	err := destinationProvider.registerStandardBuilders(
+	err = destinationProvider.registerStandardBuilders(
 		config.Job,
 		config.SourceConnection,
 		[]*mgmtv1alpha1.Connection{config.DestinationConnection},
