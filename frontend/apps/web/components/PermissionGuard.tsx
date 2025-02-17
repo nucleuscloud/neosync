5331447e8d4d2d1fbbb40a1ec1c88d3465217d18
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-02-16 16:23:39 -0800

    adds permission guard

---
+++ frontend/apps/web/components/PermissionGuard.tsx
@@ -0,0 +1,67 @@
+import { create } from '@bufbuild/protobuf';
+import { useQuery } from '@connectrpc/connect-query';
+import {
+  HasPermissionRequest_Permission,
+  HasPermissionRequestSchema,
+  UserAccountService,
+} from '@neosync/sdk';
+import { cloneElement, ReactElement, ReactNode } from 'react';
+import { Skeleton } from './ui/skeleton';
+
+interface BaseProps {
+  mode: 'disable' | 'hide';
+  accountId: string;
+  resourceId: string;
+  permission: HasPermissionRequest_Permission;
+}
+
+interface DisableProps extends BaseProps {
+  mode: 'disable';
+  children:
+    | ReactElement<ChildProps | any> // eslint-disable-line @typescript-eslint/no-explicit-any
+    | ((props: ChildProps) => ReactNode);
+}
+
+interface HideProps extends BaseProps {
+  mode: 'hide';
+  children: ReactNode;
+}
+
+type Props = DisableProps | HideProps;
+
+interface ChildProps {
+  isDisabled: boolean;
+}
+
+export default function PermissionGuard(props: Props): ReactNode {
+  const { mode, accountId, resourceId, permission, children } = props;
+
+  const { data, isLoading } = useQuery(
+    UserAccountService.method.hasPermission,
+    create(HasPermissionRequestSchema, {
+      accountId,
+      resourceId,
+      permission,
+    }),
+    {
+      enabled: !!accountId && !!resourceId && permission > 0,
+    }
+  );
+
+  if (isLoading) {
+    return <Skeleton className="h-4 w-full" />;
+  }
+
+  if (mode === 'disable') {
+    const isDisabled = !data?.hasPermission;
+    return typeof children === 'function'
+      ? children({
+          isDisabled,
+        })
+      : cloneElement(children, {
+          isDisabled,
+        });
+  }
+
+  return data?.hasPermission ? children : null;
+}
