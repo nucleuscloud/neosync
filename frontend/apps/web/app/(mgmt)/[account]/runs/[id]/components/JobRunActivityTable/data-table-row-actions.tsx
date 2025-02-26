c17673c96ecbc65ed5c4672944283a7a6f3d1656
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-02-25 20:19:38 -0800

    fixes buttons

--- frontend/apps/web/app/(mgmt)/[account]/runs/[id]/components/JobRunActivityTable/data-table-row-actions.tsx
+++ frontend/apps/web/app/(mgmt)/[account]/runs/[id]/components/JobRunActivityTable/data-table-row-actions.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Row } from '@tanstack/react-table';
 
+import { Button } from '@/components/ui/button';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 
 interface DataTableRowActionsProps<TData> {
@@ -20,8 +21,14 @@ export function DataTableRowActions<TData>({
 }: DataTableRowActionsProps<TData>) {
   return (
     <DropdownMenu modal={false}>
-      <DropdownMenuTrigger className="hover:bg-gray-100 dark:hover:bg-gray-800 py-1 px-2 rounded-lg">
-        <DotsHorizontalIcon className="h-4 w-4" />
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+        >
+          <DotsHorizontalIcon className="h-4 w-4" />
+          <span className="sr-only">Open menu</span>
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem
