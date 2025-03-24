6c477e6708999c8a123cc1c5db44e276d48564af
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-03-24 10:53:28 -0700

    adds scramble identity config to protos

--- backend/gen/go/protos/mgmt/v1alpha1/transformer.pb.json.go
+++ backend/gen/go/protos/mgmt/v1alpha1/transformer.pb.json.go
@@ -198,6 +198,16 @@ func (msg *TransformerConfig) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *ScrambleIdentity) MarshalJSON() ([]byte, error) {
+	return protojson.MarshalOptions{}.Marshal(msg)
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *ScrambleIdentity) UnmarshalJSON(b []byte) error {
+	return protojson.UnmarshalOptions{}.Unmarshal(b, msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *TransformPiiText) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{}.Marshal(msg)
 }
