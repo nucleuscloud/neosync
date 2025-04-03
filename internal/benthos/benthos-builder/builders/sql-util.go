6b8039f8a60a1e9b80c9b569ded33984647e8d31
Alisha <alisha@nucleuscloud.com>
2025-04-03 11:51:41 -0700

    forgot to do date

--- internal/benthos/benthos-builder/builders/sql-util.go
+++ internal/benthos/benthos-builder/builders/sql-util.go
@@ -799,8 +799,22 @@ func getJmTransformerByPostgresDataType(
 			},
 		}, nil
 	// case "bytea": // todo https://www.postgresql.org/docs/current/datatype-binary.html
-	// case "date":
-	// 	return &mgmtv1alpha1.JobMappingTransformer{}
+	case "date":
+		return &mgmtv1alpha1.JobMappingTransformer{
+			Config: &mgmtv1alpha1.TransformerConfig{
+				Config: &mgmtv1alpha1.TransformerConfig_GenerateJavascriptConfig{
+					GenerateJavascriptConfig: &mgmtv1alpha1.GenerateJavascript{
+						Code: `
+								const date = new Date();
+								const year = date.getFullYear();
+								const month = String(date.getMonth() + 1).padStart(2, '0');
+								const day = String(date.getDate()).padStart(2, '0');
+								return year + "-" + month + "-" + day;
+							`,
+					},
+				},
+			},
+		}, nil
 	case "time without time zone":
 		return &mgmtv1alpha1.JobMappingTransformer{
 			Config: &mgmtv1alpha1.TransformerConfig{
